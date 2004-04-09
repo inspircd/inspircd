eb0f039d0f66dc2fc920ed49cf0539ca186f6fe2
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2004-04-09 11:07:16 +0000

    Added security function that removes paths from filenames
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@463 e03df62e-2008-0410-955e-edbf42e46eb7

--- include/inspircd_util.h
+++ include/inspircd_util.h
@@ -1,7 +1,8 @@
 /*
+ *  InspIRCd utlity functions  
+ */
 
-   
-*/
+char* SafeStrncpy(char *, const char *, size_t );  
+char* CleanIpAddr(char *, const char *);
+char* CleanFilename(char* name);
 
-char * SafeStrncpy (char *, const char *, size_t );  
-char * CleanIpAddr (char *, const char *); 
