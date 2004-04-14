c854b6a0e182959223f1b602bdc0a745962b4d2b
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2004-04-14 20:14:56 +0000

    more authentication stuff
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@588 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/inspircd.cpp
+++ src/inspircd.cpp
@@ -6836,14 +6836,10 @@ void handle_link_packet(char* udp_msg, char* udp_host, serverrec *serv)
 
 		serverrec* source_server = NULL;
 
-		log(DEBUG,"Begin checking");
-
-		for (int j = 0; j < 255; j++)
+		for (int j = 0; j < 32; j++)
   		{
-			log(DEBUG,"Loop %d",j);
 			if (me[j] != NULL)
    			{
-				log(DEBUG,"Server %d not null",j);
 				for (int x = 0; x < me[j]->connectors.size(); x++)
     				{
     					log(DEBUG,"Servers are: '%s' '%s'",udp_host,me[j]->connectors[x].GetServerName().c_str());
