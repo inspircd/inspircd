cccf28594cc5bcbe5025adb0d6c66af3b664426f
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2004-04-16 14:29:52 +0000

    More SQUIT fixes
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@616 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/connection.cpp
+++ src/connection.cpp
@@ -354,7 +354,7 @@ bool connection::SendPacket(char *message, const char* host)
 			}
 			char buffer[MAXBUF];
 			snprintf(buffer,MAXBUF,"& %s",host);
-			NetSendToAllAlive(buffer);
+			NetSendToAllExcept(host,buffer);
 			log(DEBUG,"There are no routes to %s, we're gonna boot the server off!",host);
 			return false;
 		}
