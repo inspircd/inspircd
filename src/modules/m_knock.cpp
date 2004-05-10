37034b9f6e4837bbc2d89d70ead8d9965d04855c
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2004-05-10 15:12:29 +0000

    Added m_knock, +K and /KNOCK support
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@808 e03df62e-2008-0410-955e-edbf42e46eb7

---
+++ src/modules/m_knock.cpp
@@ -0,0 +1,101 @@
+// Globops and +g support module by C.J.Edwards
+
+#include <stdio.h>
+#include <string>
+#include "users.h"
+#include "channels.h"
+#include "modules.h"
+
+/* $ModDesc: Provides support for unreal-style GLOBOPS and umode +g */
+
+Server *Srv;
+	 
+void handle_globops(char **parameters, int pcnt, userrec *user)
+{
+	std::string line = "*** GLOBOPS - From " + std::string(user->nick) + ": ";
+	for (int i = 0; i < pcnt; i++)
+	{
+		line = line + std::string(parameters[i]) + " ";
+	}
+	Srv->SendToModeMask("og",WM_AND,line);
+}
+
+
+class ModuleGlobops : public Module
+{
+ public:
+	ModuleGlobops()
+	{
+		Srv = new Server;
+		
+		if (!Srv->AddExtendedMode('g',MT_CLIENT,true,0,0))
+		{
+			Srv->Log(DEFAULT,"*** m_globops: ERROR, failed to allocate user mode +g!");
+			printf("Could not claim usermode +g for this module!");
+			exit(0);
+		}
+		Srv->AddCommand("GLOBOPS",handle_globops,'o',1);
+	}
+	
+	virtual ~ModuleGlobops()
+	{
+		delete Srv;
+	}
+	
+	virtual Version GetVersion()
+	{
+		return Version(1,0,0,1);
+	}
+	
+	virtual int OnExtendedMode(userrec* user, void* target, char modechar, int type, bool mode_on, string_list &params)
+	{
+		// check if this is our mode character...
+		if ((modechar == 'g') && (type == MT_CLIENT))
+  		{
+			// we dont actually do anything with the mode in this module -
+   			// just tell the core its been claimed and is ok to give users.  			
+			return 1;
+		}
+		else
+		{
+			// this mode isn't ours, we have to bail and return 0 to not handle it.
+			return 0;
+		}
+	}
+
+	virtual void OnOper(userrec* user)
+	{
+		char* modes[2];			// only two parameters
+		modes[0] = user->nick;		// first parameter is the nick
+		modes[1] = "+g";		// second parameter is the mode
+		Srv->SendMode(modes,2,user);	// send these, forming the command "MODE <nick> +g"
+	}
+
+};
+
+// stuff down here is the module-factory stuff. For basic modules you can ignore this.
+
+class ModuleGlobopsFactory : public ModuleFactory
+{
+ public:
+	ModuleGlobopsFactory()
+	{
+	}
+	
+	~ModuleGlobopsFactory()
+	{
+	}
+	
+	virtual Module * CreateModule()
+	{
+		return new ModuleGlobops;
+	}
+	
+};
+
+
+extern "C" void * init_module( void )
+{
+	return new ModuleGlobopsFactory;
+}
+
