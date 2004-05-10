e5c42c2a136440942148d03abdc8db71e080e988
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2004-05-10 10:54:59 +0000

    Added m_blockcolor, implements unreal-style mode +c for color blocking
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@805 e03df62e-2008-0410-955e-edbf42e46eb7

---
+++ src/modules/m_blockcolor.cpp
@@ -0,0 +1,94 @@
+#include <stdio.h>
+
+#include "users.h"
+#include "channels.h"
+#include "modules.h"
+
+/* $ModDesc: Provides support for unreal-style channel mode +c */
+
+class ModuleBlockColor : public Module
+{
+	Server *Srv;
+	
+ public:
+ 
+	ModuleBlockColor()
+	{
+		Srv = new Server;
+		Srv->AddExtendedMode('c',MT_CHANNEL,false,0,0);
+	}
+	
+	virtual int OnUserPreMessage(userrec* user,void* dest,int target_type, std::string text)
+	{
+		if (target_type == TYPE_CHANNEL)
+		{
+			chanrec* c = (chanrec*)dest;
+			char ctext[MAXBUF];
+			snprintf(ctext,MAXBUF,"%s",text.c_str());
+			if (c->IsCustomModeSet('c'))
+			{
+				if (strchr(ctext,'\2')) || (strchr(ctext,'\3')) || (strchr(ctext,31))
+				{
+					WriteServ(user->fd,"404 %s %s :Can't send colors to channel (+c set)",user->nick, c->name);
+					return 1;
+				}
+			}
+		}
+		return 0;
+	}
+	
+	virtual int OnUserPreNotice(userrec* user,void* dest,int target_type, std::string text)
+	{
+		if (target_type == TYPE_CHANNEL)
+		{
+			chanrec* c = (chanrec*)dest;
+			char ctext[MAXBUF];
+			snprintf(ctext,MAXBUF,"%s",text.c_str());
+			if (c->IsCustomModeSet('c'))
+			{
+				if (strchr(ctext,'\2')) || (strchr(ctext,'\3')) || (strchr(ctext,31))
+				{
+					WriteServ(user->fd,"404 %s %s :Can't send colors to channel (+c set)",user->nick, c->name);
+					return 1;
+				}
+			}
+		}
+		return 0;
+	}
+	
+	virtual ~ModuleBlockColor()
+	{
+		delete Srv;
+	}
+	
+	virtual Version GetVersion()
+	{
+		return Version(1,0,0,0);
+	}
+};
+
+
+class ModuleBlockColorFactory : public ModuleFactory
+{
+ public:
+	ModuleBlockColorFactory()
+	{
+	}
+	
+	~ModuleBlockColorFactory()
+	{
+	}
+	
+	virtual Module * CreateModule()
+	{
+		return new ModuleBlockColor;
+	}
+	
+};
+
+
+extern "C" void * init_module( void )
+{
+	return new ModuleBlockColorFactory;
+}
+
