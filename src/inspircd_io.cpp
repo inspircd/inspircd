8946bc2a17d78481dfa7928f11c00dba12a1d120
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2005-05-18 19:38:29 +0000

    bzero removed for compatibility with solaris 10
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@1427 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/inspircd_io.cpp
+++ src/inspircd_io.cpp
@@ -619,7 +619,7 @@ int ConfValue(char* tag, char* var, int index, char *result,std::stringstream *c
 // This will bind a socket to a port. It works for UDP/TCP
 int BindSocket (int sockfd, struct sockaddr_in client, struct sockaddr_in server, int port, char* addr)
 {
-	bzero((char *)&server,sizeof(server));
+	memcpy((char *)&server,0,sizeof(server));
 	struct in_addr addy;
 	inet_aton(addr,&addy);
 	server.sin_family = AF_INET;
