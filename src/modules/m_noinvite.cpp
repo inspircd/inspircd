f7eb967d964cc089e5c7dc14079b03904e2f5a05
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2005-11-30 12:11:20 +0000

    Stylistic stuff: The evil infidelic spaces must DIE.
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@2045 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_noinvite.cpp
+++ src/modules/m_noinvite.cpp
@@ -1,15 +1,15 @@
-/*       +------------------------------------+
- *       | Inspire Internet Relay Chat Daemon |
- *       +------------------------------------+
+/*   +------------------------------------+
+ *   | Inspire Internet Relay Chat Daemon |
+ *   +------------------------------------+
  *
  *  Inspire is copyright (C) 2002-2004 ChatSpike-Dev.
- *                       E-mail:
- *                <brain@chatspike.net>
- *           	  <Craig@chatspike.net>
- *     
+ *   E-mail:
+ *<brain@chatspike.net>
+ *   	  <Craig@chatspike.net>
+ * 
  * Written by Craig Edwards, Craig McLure, and others.
  * This program is free but copyrighted software; see
- *            the file COPYING for details.
+ *the file COPYING for details.
  *
  * ---------------------------------------------------
  */
@@ -28,84 +28,85 @@ class ModuleNoInvite : public Module
 {
 	Server *Srv;
 	
- public:
+	public:
  
-	ModuleNoInvite()
-	{
-		Srv = new Server;
-		Srv->AddExtendedMode('V',MT_CHANNEL,false,0,0);
-	}
-
-        virtual void On005Numeric(std::string &output)
-        {
-                std::stringstream line(output);
-                std::string temp1, temp2;
-                while (!line.eof())
-                {
-                        line >> temp1;
-                        if (temp1.substr(0,10) == "CHANMODES=")
-                        {
-                                // append the chanmode to the end
-                                temp1 = temp1.substr(10,temp1.length());
-                                temp1 = "CHANMODES=" + temp1 + "V";
-                        }
-                        temp2 = temp2 + temp1 + " ";
-                }
-		if (temp2.length())
-	                output = temp2.substr(0,temp2.length()-1);
-        }
-
-
-	virtual int OnUserPreInvite(userrec* user,userrec* dest,chanrec* channel)
-	{
-		if (channel->IsCustomModeSet('V'))
+		ModuleNoInvite()
 		{
-			WriteServ(user->fd,"492 %s %s :Can't invite %s to channel (+V set)",user->nick, channel->name, dest->nick);
-			return 1;
+			Srv = new Server;
+			Srv->AddExtendedMode('V',MT_CHANNEL,false,0,0);
 		}
-		return 0;
-	}
-	
-	virtual int OnExtendedMode(userrec* user, void* target, char modechar, int type, bool mode_on, string_list &params)
-	{
-		// check if this is our mode character...
-		if ((modechar == 'V') && (type == MT_CHANNEL))
-  		{
-			return 1;
-		}
-		else
+
+		virtual void On005Numeric(std::string &output)
 		{
+			std::stringstream line(output);
+			std::string temp1, temp2;
+
+			while (!line.eof())
+			{
+				line >> temp1;
+				if (temp1.substr(0,10) == "CHANMODES=")
+				{
+					// append the chanmode to the end
+					temp1 = temp1.substr(10,temp1.length());
+					temp1 = "CHANMODES=" + temp1 + "V";
+				}
+				temp2 = temp2 + temp1 + " ";
+			}
+			if (temp2.length())
+				output = temp2.substr(0,temp2.length()-1);
+		}
+
+
+		virtual int OnUserPreInvite(userrec* user,userrec* dest,chanrec* channel)
+		{
+			if (channel->IsCustomModeSet('V'))
+			{
+				WriteServ(user->fd,"492 %s %s :Can't invite %s to channel (+V set)",user->nick, channel->name, dest->nick);
+				return 1;
+			}
 			return 0;
 		}
-	}
-
-	virtual ~ModuleNoInvite()
-	{
-		delete Srv;
-	}
 	
-	virtual Version GetVersion()
-	{
-		return Version(1,0,0,0,VF_STATIC|VF_VENDOR);
-	}
+		virtual int OnExtendedMode(userrec* user, void* target, char modechar, int type, bool mode_on, string_list &params)
+		{
+			// check if this is our mode character...
+			if ((modechar == 'V') && (type == MT_CHANNEL))
+  			{
+				return 1;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+
+		virtual ~ModuleNoInvite()
+		{
+			delete Srv;
+		}
+	
+		virtual Version GetVersion()
+		{
+			return Version(1,0,0,0,VF_STATIC|VF_VENDOR);
+		}
 };
 
 
 class ModuleNoInviteFactory : public ModuleFactory
 {
- public:
-	ModuleNoInviteFactory()
-	{
-	}
+	public:
+		ModuleNoInviteFactory()
+		{
+		}
 	
-	~ModuleNoInviteFactory()
-	{
-	}
+		~ModuleNoInviteFactory()
+		{
+		}
 	
-	virtual Module * CreateModule()
-	{
-		return new ModuleNoInvite;
-	}
+		virtual Module * CreateModule()
+		{
+			return new ModuleNoInvite;
+		}
 	
 };
 
