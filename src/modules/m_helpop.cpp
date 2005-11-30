2cfd118f5463ddcf55ebc9bdd0a80a29021c447c
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2005-11-30 08:20:49 +0000

    Fix stylistic things, remove an (incorrect) comment, clean up some stuff and mark stuff that will need to be looked at sometime.
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@2028 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_helpop.cpp
+++ src/modules/m_helpop.cpp
@@ -1,15 +1,15 @@
-/*       +------------------------------------+
- *       | Inspire Internet Relay Chat Daemon |
- *       +------------------------------------+
+/*   +------------------------------------+
+ *   | Inspire Internet Relay Chat Daemon |
+ *   +------------------------------------+
  *
  *  Inspire is copyright (C) 2002-2004 ChatSpike-Dev.
- *                       E-mail:
- *                <brain@chatspike.net>
- *           	  <Craig@chatspike.net>
- *     
+ *   E-mail:
+ *<brain@chatspike.net>
+ *   	  <Craig@chatspike.net>
+ * 
  * Written by Craig Edwards, Craig McLure, and others.
  * This program is free but copyrighted software; see
- *            the file COPYING for details.
+ *the file COPYING for details.
  *
  * ---------------------------------------------------
  */
@@ -22,6 +22,7 @@ using namespace std;
 #include "helperfuncs.h"
 
 // Global Vars
+/* XXX - should all this be marked static? clear the global namespace, etc. */
 ConfigReader *helpop;
 Server *Srv;
 
@@ -37,7 +38,8 @@ void handle_helpop(char **parameters, int pcnt, userrec *user)
 	char a[MAXBUF];
 	std::string output = " ";
 
-	if (pcnt < 1) {
+	if (pcnt < 1)
+	{
  		do_helpop(NULL,pcnt,user);
 		return;
    	}
@@ -46,31 +48,41 @@ void handle_helpop(char **parameters, int pcnt, userrec *user)
 	{
 		// Force send to all +h users
 		sendtohelpop(user, pcnt, parameters);
-	} else if (parameters[0][0] == '?') {
+	}
+	else if (parameters[0][0] == '?')
+	{
 		// Force to the helpop system with no forward if not found.
-                if (do_helpop(parameters, pcnt, user) == false) {
-                        // Not handled by the Database, Tell the user, and forward.
-                        for (int i = 1; output != ""; i++)
-                        {
-                                snprintf(a,MAXBUF,"line%d",i);
-                                output = helpop->ReadValue("nohelp", std::string(a), 0);
-				if(output != "") {
-	                                Srv->SendTo(NULL,user,"290 "+std::string(user->nick)+" :"+output);
+		if (do_helpop(parameters, pcnt, user) == false)
+		{
+			// Not handled by the Database, Tell the user, and bail.
+			for (int i = 1; output != ""; i++)
+			{
+				snprintf(a,MAXBUF,"line%d",i);
+				output = helpop->ReadValue("nohelp", std::string(a), 0);
+
+				if(output != "")
+				{
+					Srv->SendTo(NULL,user,"290 "+std::string(user->nick)+" :"+output);
 				}
-                        }
-                }
-	} else {
+			}
+		}
+	}
+	else
+	{
 		// Check with the helpop database, if not found send to +h
-		if (do_helpop(parameters, pcnt, user) == false) {
+		if (do_helpop(parameters, pcnt, user) == false)
+		{
 			// Not handled by the Database, Tell the user, and forward.
-		        for (int i = 1; output != ""; i++)
-      			{
-            			snprintf(a,MAXBUF,"line%d",i);
-               			output = helpop->ReadValue("nohelpo", std::string(a), 0);
-        			if (output != "") {        		
-	                		Srv->SendTo(NULL,user,"290 "+std::string(user->nick)+" :"+output);
+			for (int i = 1; output != ""; i++)
+  			{
+				snprintf(a,MAXBUF,"line%d",i);
+				/* XXX - "nohelpo" ? or "nohelp", as above */
+   				output = helpop->ReadValue("nohelpo", std::string(a), 0);
+				if (output != "")
+				{
+					Srv->SendTo(NULL,user,"290 "+std::string(user->nick)+" :"+output);
 				}
-      			}
+  			}
 			// Forward.
 			sendtohelpop(user, pcnt, parameters);
 		}
@@ -82,32 +94,35 @@ bool do_helpop(char **parameters, int pcnt, userrec *src)
 	char *search;
 	std::string output = " "; // a fix bought to you by brain :p
 	char a[MAXBUF];
+	char lower[MAXBUF];
+	int nlines = 0;
 
-	if (!parameters) {
+	if (!parameters)
+	{
  		search = "start";
   	}
-	else {
+	else
+	{
  		search = parameters[0];
    	}
 
-	if (search[0] == '?') {
+	if (search[0] == '?')
+	{
  		search++;
    	}
 
-        // FIX by brain: make the string lowercase, ConfigReader is
-        // case sensitive
-        char lower[MAXBUF];
-        strlcpy(lower,search,MAXBUF);
-        for (unsigned int t = 0; t < strlen(lower); t++)
-                lower[t] = tolower(lower[t]);
+	/* XXX - don't we have an strtolower()? if not, might pay to add one.. that works on char *, preferably.. */
+	strlcpy(lower, search, MAXBUF);
+	for (unsigned int t = 0; t < strlen(lower); t++)
+		lower[t] = tolower(lower[t]);
 
 
-	int nlines = 0;
 	for (int i = 1; output != ""; i++)
 	{
 		snprintf(a,MAXBUF,"line%d",i);
 		output = helpop->ReadValue(lower, a, 0);
-		if (output != "") {
+		if (output != "")
+		{
 			Srv->SendTo(NULL,src,"290 "+std::string(src->nick)+" :"+output);
 			nlines++;
 		}
@@ -120,7 +135,11 @@ bool do_helpop(char **parameters, int pcnt, userrec *src)
 void sendtohelpop(userrec *src, int pcnt, char **params)
 {
 	char* first = params[0];
-	if (first[0] == '!') { first++; }
+	if (*first == '!')
+	{
+		first++;
+	}
+
 	std::string line = "*** HELPOPS - From "+std::string(src->nick)+": "+std::string(first)+" ";
 	for (int i = 1; i < pcnt; i++)
 	{
@@ -131,84 +150,83 @@ void sendtohelpop(userrec *src, int pcnt, char **params)
 
 class ModuleHelpop : public Module
 {
- private:
-	ConfigReader *conf;
-	std::string  h_file;
+	private:
+		ConfigReader *conf;
+		std::string  h_file;
 
- public:
-	ModuleHelpop()
-	{
-		Srv  = new Server;
-
-		ReadConfig();
-		if (!Srv->AddExtendedMode('h',MT_CLIENT,true,0,0))
+	public:
+		ModuleHelpop()
 		{
-			Srv->Log(DEFAULT,"Unable to claim the +h usermode.");
-			return;
+			Srv  = new Server;
+
+			ReadConfig();
+			if (!Srv->AddExtendedMode('h',MT_CLIENT,true,0,0))
+			{
+				Srv->Log(DEFAULT,"Unable to claim the +h usermode.");
+				return;
+			}
+
+			Srv->AddCommand("HELPOP",handle_helpop,0,0,"m_helpop.so");
 		}
 
-		// Loads of comments, untill supported properly.
-		Srv->AddCommand("HELPOP",handle_helpop,0,0,"m_helpop.so");
-	}
-
-	virtual void ReadConfig()
-        {
-		conf = new ConfigReader;
-                h_file = conf->ReadValue("helpop", "file", 0);
-
-                if (h_file == "") {
-                        log(DEFAULT,"m_helpop: Helpop file not Specified.");
-                        return;
-                }
-
-                helpop = new ConfigReader(h_file);
-                if ((helpop->ReadValue("nohelp",  "line1", 0) == "") ||
-                    (helpop->ReadValue("nohelpo", "line1", 0) == "") ||
-                    (helpop->ReadValue("start",   "line1", 0) == ""))
-                {
-                        log(DEFAULT,"m_helpop: Helpop file is missing important entries. Please check the example conf.");
-                        return;
-                }
-        }
-
-
-	virtual void OnRehash()
-	{
-		delete conf;
-		delete helpop;
-
-		ReadConfig();
-
-	}
-
-	virtual int OnExtendedMode(userrec* user, void* target, char modechar, int type, bool mode_on, string_list &params)
-	{
-		if ((modechar == 'h') && (type == MT_CLIENT))
-  		{
-			return 1;
-		}
-		return 0;
-	}
-
-	virtual void OnWhois(userrec* src, userrec* dst)
-	{
-		if (strchr(dst->modes,'h'))
+		virtual void ReadConfig()
 		{
-			Srv->SendTo(NULL,src,"310 "+std::string(src->nick)+" "+std::string(dst->nick)+" :is available for help.");
-		}
-	}
+			conf = new ConfigReader;
+			h_file = conf->ReadValue("helpop", "file", 0);
 
-	virtual ~ModuleHelpop()
-	{
-		delete Srv;
-		delete conf;
-		delete helpop;
-	}
+			if (h_file == "")
+			{
+				log(DEFAULT,"m_helpop: Helpop file not Specified.");
+				return;
+			}
+
+			helpop = new ConfigReader(h_file);
+			if ((helpop->ReadValue("nohelp",  "line1", 0) == "") ||
+				(helpop->ReadValue("nohelpo", "line1", 0) == "") ||
+				(helpop->ReadValue("start",   "line1", 0) == ""))
+			{
+				log(DEFAULT,"m_helpop: Helpop file is missing important entries. Please check the example conf.");
+				return;
+			}
+		}
+
+
+		virtual void OnRehash()
+		{
+			delete conf;
+			delete helpop;
+
+			ReadConfig();
+		}
+
+		virtual int OnExtendedMode(userrec* user, void* target, char modechar, int type, bool mode_on, string_list &params)
+		{
+			if ((modechar == 'h') && (type == MT_CLIENT))
+	  		{
+				return 1;
+			}
+			return 0;
+		}
+
+		virtual void OnWhois(userrec* src, userrec* dst)
+		{
+			if (strchr(dst->modes,'h'))
+			{
+				Srv->SendTo(NULL,src,"310 "+std::string(src->nick)+" "+std::string(dst->nick)+" :is available for help.");
+			}
+		}
+
+		virtual ~ModuleHelpop()
+		{
+			delete Srv;
+			delete conf;
+			delete helpop;
+		}
 	
-	virtual Version GetVersion()
-	{
-		return Version(1,0,0,1,VF_STATIC|VF_VENDOR);
-	}
+		virtual Version GetVersion()
+		{
+			return Version(1,0,0,1,VF_STATIC|VF_VENDOR);
+		}
 };
 
 class ModuleHelpopFactory : public ModuleFactory
