0ae2cb132ba4a631edbc232f483f72724856f5fb
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2005-12-01 21:48:28 +0000

    *really* reverted is_uline change -- \!server returns false, \!*server returns true.
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@2087 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/commands.cpp
+++ src/commands.cpp
@@ -1526,8 +1526,9 @@ bool is_uline(const char* server)
 {
 	char ServName[MAXBUF];
 
-	/* fix, by w00t - per nenolod. I don't see how we can want '""' as a uline. */
-	if (!server || !(*server))
+	if (!server)
+		return false;
+	if (!(*server))
 		return true;
 
 	for (int i = 0; i < ConfValueEnum("uline",&config_f); i++)
