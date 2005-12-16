6c28b603d261a3f524a188ba8699995730251164
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2005-12-16 23:01:28 +0000

    Includes/externs tidyup
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@2544 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/cmd_gline.cpp
+++ src/cmd_gline.cpp
@@ -21,11 +21,6 @@ using namespace std;
 #include "inspircd_io.h"
 #include <time.h>
 #include <string>
-#ifdef GCC3
-#include <ext/hash_map>
-#else
-#include <hash_map>
-#endif
 #include <map>
 #include <sstream>
 #include <vector>
@@ -38,29 +33,17 @@ using namespace std;
 #include "wildcard.h"
 #include "message.h"
 #include "commands.h"
-#include "mode.h"
 #include "xline.h"
 #include "inspstring.h"
-#include "dnsqueue.h"
 #include "helperfuncs.h"
 #include "hashcomp.h"
-#include "socketengine.h"
 #include "typedefs.h"
 #include "command_parse.h"
-#include "cmd_gline.h"
+#include "cmd_eline.h"
 
-extern ServerConfig* Config;
-extern InspIRCd* ServerInstance;
 extern int MODCOUNT;
 extern std::vector<Module*> modules;
 extern std::vector<ircd_module*> factory;
-extern time_t TIME;
-extern user_hash clientlist;
-extern chan_hash chanlist;
-extern whowas_hash whowas;
-extern std::vector<userrec*> all_opers;
-extern std::vector<userrec*> local_users;
-extern userrec* fd_ref_table[65536];
 
 void cmd_gline::Handle (char **parameters, int pcnt, userrec *user)
 {
