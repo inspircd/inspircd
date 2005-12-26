336e8f1bb4be9440521d81a6f1c0be505be801de
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2005-12-26 21:33:34 +0000

    Fixed missing I_On005Numeric
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@2668 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_park.cpp
+++ src/modules/m_park.cpp
@@ -234,7 +234,7 @@ class ModulePark : public Module
 
 	void Implements(char* List)
 	{
-		List[I_OnRehash] = List[I_OnUserQuit] = List[I_OnUserPreMessage] = List[I_OnUserPreNick] = List[I_OnBackgroundTimer] = List[I_OnWhois] = 1;
+		List[I_On005Numeric] = List[I_OnRehash] = List[I_OnUserQuit] = List[I_OnUserPreMessage] = List[I_OnUserPreNick] = List[I_OnBackgroundTimer] = List[I_OnWhois] = 1;
 	}
 
 	virtual void OnRehash(std::string parameter)
