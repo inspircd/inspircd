b0cb12952a378d4d3717d3a5556325a15be1de0a
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2005-12-29 07:08:24 +0000

    Initial revision of /devoice.
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@2695 e03df62e-2008-0410-955e-edbf42e46eb7

---
+++ src/modules/m_devoice.cpp
@@ -0,0 +1,102 @@
+/*       +------------------------------------+
+ *       | Inspire Internet Relay Chat Daemon |
+ *       +------------------------------------+
+ *
+ *  Inspire is copyright (C) 2002-2004 ChatSpike-Dev.
+ *                       E-mail:
+ *                <brain@chatspike.net>
+ *           	  <Craig@chatspike.net>
+ *     
+ * Written by Craig Edwards, Craig McLure, and others.
+ * This program is free but copyrighted software; see
+ *            the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
+using namespace std;
+
+/*
+ * DEVOICE module for InspIRCd
+ *  Syntax: /DEVOICE <#chan>
+ */
+
+/* $ModDesc: Provides voiced users with the ability to devoice themselves. */
+
+#include <stdio.h>
+#include "users.h"
+#include "channels.h"
+#include "modules.h"
+
+Server *Srv;
+	 
+class cmd_devoice : public command_t
+{
+ public:
+	cmd_devoice () : command_t("DEVOICE", 'o', 2)
+	{
+		this->source = "m_devoice.so";
+	}
+
+	void Handle (char **parameters, int pcnt, userrec *user)
+	{
+		/*
+		 * NOTE: DO NOT CODE LIKE THIS!!! This has no checks and can send
+		 * invalid or plain confusing mode changes, code some checking!
+		 *
+		 * - I'm not aware what checking I need, so for now... be supreme evil.
+		 */
+                char* modes[3];
+                modes[0] = parameters[0];
+                modes[1] = "-v";
+                modes[2] = user->nick;
+
+                Srv->SendMode(modes,3,user);
+	}
+};
+
+class ModuleDeVoice : public Module
+{
+	cmd_devoice *mycommand;
+ public:
+	ModuleDeVoice(Server* Me) : Module::Module(Me)
+	{
+		Srv = Me;
+		mycommand = new cmd_devoice();
+		Srv->AddCommand(mycommand);
+	}
+	
+	virtual ~ModuleDeVoice()
+	{
+	}
+	
+	virtual Version GetVersion()
+	{
+		return Version(1, 0, 0, 0, VF_VENDOR);
+	}
+};
+
+
+class ModuleDeVoiceFactory : public ModuleFactory
+{
+ public:
+	ModuleDeVoiceFactory()
+	{
+	}
+	
+	~ModuleDeVoiceFactory()
+	{
+	}
+	
+	virtual Module * CreateModule(Server* Me)
+	{
+		return new ModuleDeVoice(Me);
+	}
+	
+};
+
+
+extern "C" void * init_module( void )
+{
+	return new ModuleDeVoiceFactory;
+}
