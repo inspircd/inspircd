79262c9d8751eca8cff50eca0b9f5b57f6730577
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-01-25 20:43:45 +0000

    Changed more to use InsertMode
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@2902 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_noctcp.cpp
+++ src/modules/m_noctcp.cpp
@@ -44,21 +44,7 @@ class ModuleNoCTCP : public Module
 
         virtual void On005Numeric(std::string &output)
         {
-                std::stringstream line(output);
-                std::string temp1, temp2;
-                while (!line.eof())
-                {
-                        line >> temp1;
-                        if (temp1.substr(0,10) == "CHANMODES=")
-                        {
-                                // append the chanmode to the end
-                                temp1 = temp1.substr(10,temp1.length());
-                                temp1 = "CHANMODES=" + temp1 + "C";
-                        }
-                        temp2 = temp2 + temp1 + " ";
-                }
-		if (temp2.length())
-	                output = temp2.substr(0,temp2.length()-1);
+		InsertMode(output,"C",4);
         }
 	
 	virtual int OnUserPreMessage(userrec* user,void* dest,int target_type, std::string &text)
