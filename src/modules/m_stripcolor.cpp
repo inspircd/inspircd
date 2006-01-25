1bc39e97ccdbfd84e533d6dc6bf266102a4060bd
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-01-25 20:47:22 +0000

    Tidied up to use InsertMode()
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@2903 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_stripcolor.cpp
+++ src/modules/m_stripcolor.cpp
@@ -59,21 +59,7 @@ class ModuleStripColor : public Module
 
         virtual void On005Numeric(std::string &output)
         {
-                std::stringstream line(output);
-                std::string temp1, temp2;
-                while (!line.eof())
-                {
-                        line >> temp1;
-                        if (temp1.substr(0,10) == "CHANMODES=")
-                        {
-                                // append the chanmode to the end
-                                temp1 = temp1.substr(10,temp1.length());
-                                temp1 = "CHANMODES=" + temp1 + "S";
-                        }
-                        temp2 = temp2 + temp1 + " ";
-                }
-		if (temp2.length())
-	                output = temp2.substr(0,temp2.length()-1);
+		InsertMode(output,"S",4);
         }
  	
 	virtual ~ModuleStripColor()
