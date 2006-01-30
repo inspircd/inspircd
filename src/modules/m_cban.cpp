2080de06813794dc52ee9d0a0809e3411fb9a43b
om <om@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-01-30 19:13:44 +0000

    Major updates to CBANs...like they're reasonably functional
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@2987 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_cban.cpp
+++ src/modules/m_cban.cpp
@@ -6,6 +6,7 @@
  *                       E-mail:
  *                <brain@chatspike.net>
  *           	  <Craig@chatspike.net>
+ *						<omster@gmail.com>
  *     
  * Written by Craig Edwards, Craig McLure, and others.
  * This program is free but copyrighted software; see
@@ -14,9 +15,10 @@
  * ---------------------------------------------------
  */
 
-using namespace std;
-
-#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
 #include "users.h"
 #include "channels.h"
 #include "modules.h"
@@ -24,41 +26,35 @@ using namespace std;
 
 /* $ModDesc: Gives /cban, aka C:lines. Think Q:lines, for channels. */
 
-extern time_t TIME;
-
 class CBan
 {
- private:
-	unsigned long expiry;
+public:
 	std::string chname;
+	std::string set_by;
+	time_t set_on;
+	long length;
 	std::string reason;
 
- public:
-	CBan(std::string cn, std::string rs, unsigned long ex)
+	CBan()
 	{
-		chname = cn;
-		reason = rs;
-		expiry = ex;
 	}
 
-	std::string GetName()
+	CBan(std::string cn, std::string sb, time_t so, long ln, std::string rs) : chname(cn), set_by(sb), set_on(so), length(ln), reason(rs)
 	{
-		return this->chname;
-	}
-
-	std::string GetReason()
-	{
-		return this->reason;
-	}
-
-	unsigned long GetExpiry()
-	{
-		return this->expiry;
 	}
 };
 
+std::string EncodeCBan(const CBan &ban);
+CBan DecodeCBan(const std::string &data);
+bool CBanComp(const CBan &ban1, const CBan &ban2);
+void ExpireBans();
+bool IsValidChan(const char* cname);
+
+extern time_t TIME;
+typedef std::vector<CBan> cbanlist;
+
 /* cbans is declared here, as our type is right above. Don't try move it. */
-vector<CBan> cbans;
+cbanlist cbans;
 
 class cmd_cban : public command_t
 {
@@ -66,7 +62,7 @@ class cmd_cban : public command_t
 	Server *Srv;
 
  public:
-	cmd_cban (Server* Me) : command_t("CBAN", 'o', 1)
+	cmd_cban(Server* Me) : command_t("CBAN", 'o', 1)
 	{
 		this->source = "m_cban.so";
 		this->Srv = Me;
@@ -76,19 +72,19 @@ class cmd_cban : public command_t
 	{
 		/* syntax: CBAN #channel time :reason goes here */
 		/* 'time' is a human-readable timestring, like 2d3h2s. */
+		
+		ExpireBans();
 
-		std::string chname;
-		std::string reason;
-		unsigned long expiry;
-
-		if (pcnt == 1)
+		if(pcnt == 1)
 		{
 			/* form: CBAN #channel removes a CBAN */
-			for (vector<CBan>::iterator myiter; myiter < cbans.end(); myiter++)
+			for (cbanlist::iterator iter = cbans.begin(); iter != cbans.end(); iter++)
 			{
-				if (parameters[0] == (*myiter).GetName())
+				if (parameters[0] == iter->chname)
 				{
-					cbans.erase(myiter);
+					unsigned long remaining = (iter->set_on + iter->length) - TIME;
+					WriteServ(user->fd, "386 %s %s :Removed CBAN with %lu seconds left before expiry (%s)", user->nick, iter->chname.c_str(), remaining, iter->reason.c_str());
+					cbans.erase(iter);
 					break;
 				}
 			}
@@ -96,13 +92,31 @@ class cmd_cban : public command_t
 		else if (pcnt >= 2)
 		{
 			/* full form to add a CBAN */
-			/* XXX - checking on chnames */
-			chname = parameters[0];
-			expiry = TIME + Srv->CalcDuration(parameters[1]);
-			reason = parameters[2];
-
-			CBan meow(chname, reason, expiry);
-			cbans.push_back(meow);
+			if(IsValidChan(parameters[0]))
+			{
+				// parameters[0] = #channel
+				// parameters[1] = 1h3m2s
+				// parameters[2] = Tortoise abuser
+				long length = Srv->CalcDuration(parameters[1]);
+				
+				cbans.push_back(CBan(parameters[0], user->nick, TIME, length, parameters[2]));
+				std::sort(cbans.begin(), cbans.end(), CBanComp);
+				
+				if(length > 0)
+				{
+					WriteServ(user->fd, "385 %s %s :Added %lu second channel ban (%s)", user->nick, parameters[0], length, parameters[2]);
+					WriteOpers("*** %s added %lu second channel ban on %s (%s)", user->nick, length, parameters[0], parameters[2]);
+				}
+				else
+				{
+					WriteServ(user->fd, "385 %s %s :Added permenant channel ban (%s)", user->nick, parameters[0], parameters[2]);
+					WriteOpers("*** %s added permenant channel ban on %s (%s)", user->nick, parameters[0], parameters[2]);
+				}
+			}
+			else
+			{
+				WriteServ(user->fd, "403 %s %s :No such channel", user->nick, parameters[0]);
+			}
 		}
 	}
 };
@@ -122,31 +136,55 @@ class ModuleCBan : public Module
 
 	void Implements(char* List)
 	{
-		List[I_OnUserPreJoin] = 1;
+		List[I_OnUserPreJoin] = List[I_OnSyncOtherMetaData] = List[I_OnDecodeMetaData] = List[I_OnStats] = 1;
+	}
+	
+	virtual int OnStats(char symbol, userrec* user)
+	{
+		ExpireBans();
+	
+		for(cbanlist::iterator iter = cbans.begin(); iter != cbans.end(); iter++)
+		{
+			unsigned long remaining = (iter->set_on + iter->length) - TIME;
+			WriteServ(user->fd, "210 %s %s %s %lu %lu %lu :%s", user->nick, iter->chname.c_str(), iter->set_by.c_str(), iter->set_on, iter->length, remaining, iter->reason.c_str());
+		}
+		
+		return 0;
 	}
 
 	virtual int OnUserPreJoin(userrec *user, chanrec *chan, const char *cname)
 	{
+		ExpireBans();
+	
 		/* check cbans in here, and apply as necessary. */
-		log(DEBUG,"In OnUserPreJoin cbans.size() == %d",cbans.size());
-
-		std::string chname = cname;
-
-		for (unsigned int a = 0; a < cbans.size(); a++)
+		for(cbanlist::iterator iter = cbans.begin(); iter != cbans.end(); iter++)
 		{
-			log(DEBUG,"m_cban: DEBUG: checking %s against %s in OnPreUserJoin()", chname.c_str(), cbans[a].GetName().c_str());
-			if (chname == cbans[a].GetName())
+			if(iter->chname == cname && !strchr(user->modes, 'o'))
 			{
-				/* matches CBAN */
-				WriteOpers("DENY join");
+				// Channel is banned.
+				WriteServ(user->fd, "384 %s %s :Cannot join channel, CBANed (%s)", user->nick, cname, iter->reason.c_str());
+				WriteOpers("*** %s tried to join %s which is CBANed (%s)", user->nick, cname, iter->reason.c_str());
 				return 1;
 			}
 		}
-
-		log(DEBUG,"DONE checking, allowed");
-
-		/* Allow the change. */
 		return 0;
+	}
+	
+	virtual void OnSyncOtherMetaData(Module* proto, void* opaque)
+	{
+		for(cbanlist::iterator iter = cbans.begin(); iter != cbans.end(); iter++)
+		{
+			proto->ProtoSendMetaData(opaque, TYPE_OTHER, NULL, "cban", EncodeCBan(*iter));
+		}
+	}
+	
+	virtual void OnDecodeMetaData(int target_type, void* target, std::string extname, std::string extdata)
+	{
+		if((target_type == TYPE_OTHER) && (extname == "cban"))
+		{
+			cbans.push_back(DecodeCBan(extdata));
+			std::sort(cbans.begin(), cbans.end(), CBanComp);
+		}
 	}
 
 	virtual ~ModuleCBan()
@@ -159,6 +197,57 @@ class ModuleCBan : public Module
 	}
 };
 
+std::string EncodeCBan(const CBan &ban)
+{
+	std::ostringstream stream;	
+	stream << ban.chname << " " << ban.set_by << " " << ban.set_on << " " << ban.length << " " << ban.reason;
+	return stream.str();	
+}
+
+CBan DecodeCBan(const std::string &data)
+{
+	CBan res;
+	std::istringstream stream;
+	stream >> res.chname;
+	stream >> res.set_by;
+	stream >> res.set_on;
+	stream >> res.length;
+	res.reason = stream.str();
+	
+	return res;
+}
+
+bool CBanComp(const CBan &ban1, const CBan &ban2)
+{
+	return ((ban1.set_on + ban1.length) < (ban2.set_on + ban2.length));
+}
+
+void ExpireBans()
+{
+	while(cbans.size() && ((cbans.begin()->set_on + cbans.begin()->length) <= TIME))
+	{
+		cbanlist::iterator iter = cbans.begin();
+		
+		log(DEBUG, "m_cban.so: Ban on %s expired, removing...", iter->chname.c_str());
+		WriteOpers("*** %li second CBAN on %s (%s) set %u seconds ago expired", iter->length, iter->chname.c_str(), iter->reason.c_str(), TIME - iter->set_on);
+		cbans.erase(iter);
+	}
+}
+
+bool IsValidChan(const char* cname)
+{
+	if(!cname)
+		return false;
+		
+	if(cname[0] != '#')
+		return false;
+		
+	for(unsigned int i = 0; i < strlen(cname); i++)
+		if((cname[i] == ' ') || (cname[i] == '\7') || (cname[i] == ','))
+			return false;
+			
+	return true;
+}
 
 class ModuleCBanFactory : public ModuleFactory
 {
@@ -183,4 +272,3 @@ extern "C" void * init_module( void )
 {
 	return new ModuleCBanFactory;
 }
-
