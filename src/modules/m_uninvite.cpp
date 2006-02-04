264e8c9e1ec51553a2e80dc2e86985b009ab7025
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-02-04 19:11:44 +0000

    First-revision UNINVITE, probably wont compile yet
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@3079 e03df62e-2008-0410-955e-edbf42e46eb7

---
+++ src/modules/m_uninvite.cpp
@@ -0,0 +1,130 @@
+/*       +------------------------------------+
+ *       | Inspire Internet Relay Chat Daemon |
+ *       +------------------------------------+
+ *
+ *  InspIRCd is copyright (C) 2002-2006 ChatSpike-Dev.
+ *                       E-mail:
+ *                <brain@chatspike.net>
+ *           	  <Craig@chatspike.net>
+ *     
+ * Written by Craig Edwards, Craig McLure, and others.
+ * This program is free but copyrighted software; see
+ *            the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
+using namespace std;
+
+/* $ModDesc: Provides the UNINVITE command which lets users un-invite other users from channels (!) */
+
+#include <stdio.h>
+#include "users.h"
+#include "channels.h"
+#include "modules.h"
+#include "helperfuncs.h"
+
+Server *Srv;
+	 
+class cmd_uninvite : public command_t
+{
+ public:
+	cmd_uninvite () : command_t("UNINVITE", 0, 2)
+	{
+		this->source = "m_uninvite.so";
+	}
+
+	void Handle (char **parameters, int pcnt, userrec *user)
+	{
+                userrec* u = Find(parameters[0]);
+                chanrec* c = FindChan(parameters[1]);
+                         
+                if ((!c) || (!u))
+                {        
+                        if (!c)
+                        {
+                                WriteServ(user->fd,"401 %s %s :No such nick/channel",user->nick, parameters[1]);
+                        }
+                        else
+                        {
+                                WriteServ(user->fd,"401 %s %s :No such nick/channel",user->nick, parameters[0]);
+                        }
+                                
+                        return; 
+                }        
+
+                if (c->binarymodes & CM_INVITEONLY)
+                {
+                        if (cstatus(user,c) < STATUS_HOP)
+                        {
+                                WriteServ(user->fd,"482 %s %s :You must be at least a half-operator to change modes on this channel",user->nick, c->name);
+                                return;
+                        }
+                }
+
+		irc::string xname(c->name);
+
+                if (!u->IsInvited(xname))
+                {
+                        WriteServ(user->fd,"491 %s %s %s :Is not invited to channel %s",user->nick,u->nick,c->name,c->name);
+                        return;
+                }
+                if (has_channel(user,c))
+                {
+                        WriteServ(user->fd,"492 %s %s :You're not on that channel!",user->nick, c->name);
+                        return;
+                }
+
+                u->RemoveInvite(xname);
+                WriteServ(user->fd,"NOTICE %s :*** Uninvited %s from %s",user->nick,u->nick,c->name);
+                WriteChannel(user,"NOTICE %s :*** %s uninvited %s.",c->name,user->nick,u->nick);
+        }
+};
+
+class ModuleUninvite : public Module
+{
+	cmd_uninvite *mycommand;
+
+ public:
+
+	ModuleUninvite(Server* Me) : Module::Module(Me)
+	{
+		Srv = Me;
+		mycommand = new cmd_uninvite();
+		Srv->AddCommand(mycommand);
+	}
+	
+	virtual ~ModuleUninvite()
+	{
+	}
+	
+	virtual Version GetVersion()
+	{
+		return Version(1, 0, 0, 0, VF_VENDOR);
+	}
+};
+
+
+class ModuleUninviteFactory : public ModuleFactory
+{
+ public:
+	ModuleUninviteFactory()
+	{
+	}
+	
+	~ModuleUninviteFactory()
+	{
+	}
+	
+	virtual Module * CreateModule(Server* Me)
+	{
+		return new ModuleUninvite(Me);
+	}
+	
+};
+
+
+extern "C" void * init_module( void )
+{
+	return new ModuleUninviteFactory;
+}
