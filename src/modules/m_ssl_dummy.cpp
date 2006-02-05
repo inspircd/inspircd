2f1e456630bb01373ca0a1c3b707ef09334e7226
om <om@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-02-05 02:41:38 +0000

    [Re ;\] add m_ssl_dummy :"
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@3089 e03df62e-2008-0410-955e-edbf42e46eb7

---
+++ src/modules/m_ssl_dummy.cpp
@@ -0,0 +1,109 @@
+/*   +------------------------------------+
+ *   | Inspire Internet Relay Chat Daemon |
+ *   +------------------------------------+
+ *
+ *  InspIRCd is copyright (C) 2002-2006 ChatSpike-Dev.
+ *   E-mail:
+ *	<brain@chatspike.net>
+ *	<Craig@chatspike.net>
+ * <omster@gmail.com>
+ * 
+ * Written by Craig Edwards, Craig McLure, and others.
+ * This program is free but copyrighted software; see
+ *the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
+#include "users.h"
+#include "modules.h"
+#include "helperfuncs.h"
+
+/* $ModDesc: Makes remote /whoises to SSL servers work on a non-ssl server */
+
+class ModuleSSLDummy : public Module
+{
+	Server* Srv;
+ public:
+	
+	ModuleSSLDummy(Server* Me)	: Module::Module(Me)
+	{
+		Srv = Me;
+	}
+	
+	virtual ~ModuleSSLDummy()
+	{
+	}
+		
+	virtual Version GetVersion()
+	{
+		return Version(1, 0, 0, 0, VF_VENDOR);
+	}
+
+	void Implements(char* List)
+	{
+		List[I_OnSyncUserMetaData] = List[I_OnDecodeMetaData] = List[I_OnWhois] = 1;
+	}
+
+	// :kenny.chatspike.net 320 Om Epy|AFK :is a Secure Connection
+	virtual void OnWhois(userrec* source, userrec* dest)
+	{
+		if(dest->GetExt("ssl"))
+		{
+			WriteServ(source->fd, "320 %s %s :is using a secure connection", source->nick, dest->nick);
+		}
+	}
+	
+	virtual void OnSyncUserMetaData(userrec* user, Module* proto, void* opaque, std::string extname)
+	{
+		// check if the linking module wants to know about OUR metadata
+		if(extname == "ssl")
+		{
+			// check if this user has an ssl field to send
+			if(user->GetExt(extname))
+			{
+				// call this function in the linking module, let it format the data how it
+				// sees fit, and send it on its way. We dont need or want to know how.
+				proto->ProtoSendMetaData(opaque, TYPE_USER, user, extname, "ON");
+			}
+		}
+	}
+	
+	virtual void OnDecodeMetaData(int target_type, void* target, std::string extname, std::string extdata)
+	{
+		// check if its our metadata key, and its associated with a user
+		if ((target_type == TYPE_USER) && (extname == "ssl"))
+		{
+			userrec* dest = (userrec*)target;
+			// if they dont already have an ssl flag, accept the remote server's
+			if (!dest->GetExt(extname))
+			{
+				dest->Extend(extname, "ON");
+			}
+		}
+	}
+};
+
+class ModuleSSLDummyFactory : public ModuleFactory
+{
+ public:
+	ModuleSSLDummyFactory()
+	{
+	}
+	
+	~ModuleSSLDummyFactory()
+	{
+	}
+	
+	virtual Module * CreateModule(Server* Me)
+	{
+		return new ModuleSSLDummy(Me);
+	}
+};
+
+
+extern "C" void * init_module( void )
+{
+	return new ModuleSSLDummyFactory;
+}
+
