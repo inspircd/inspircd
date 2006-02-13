628c4b1b6f9bb2e417bd69e6624b3804ed26ecbd
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-02-13 01:11:26 +0000

    m_join: Use IsValidChannelName()
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@3183 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/cmd_join.cpp
+++ src/cmd_join.cpp
@@ -50,7 +50,8 @@ void cmd_join::Handle (char **parameters, int pcnt, userrec *user)
 	
 	if (ServerInstance->Parser->LoopCall(this,parameters,pcnt,user,0,0,1))
 		return;
-	if (parameters[0][0] == '#')
+
+	if (IsValidChannelName(parameters[0]))
 	{
 		Ptr = add_channel(user,parameters[0],parameters[1],false);
 	}
