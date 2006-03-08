c35149012802a28034350aa1187b04af9351c509
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-03-08 11:38:31 +0000

    Changed TS check: Abort on +- 5 mins, warn on +- 60 secs!
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@3547 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_spanningtree.cpp
+++ src/modules/m_spanningtree.cpp
@@ -2273,11 +2273,15 @@ class TreeSocket : public InspSocket
 				{
 					time_t THEM = atoi(params[0].c_str());
 					long delta = THEM-time(NULL);
-					if ((delta < -600) || (delta > 600))
+					if ((delta < -300) || (delta > 300))
 					{
-						WriteOpers("*** \2ERROR\2: Your clocks are out by %d seconds (this is more than ten minutes). Link aborted, \2PLEASE SYNC YOUR CLOCKS!\2",abs(delta));
-						this->WriteLine("ERROR :Your clocks are out by "+ConvToStr(abs(delta))+" seconds (this is more than ten minutes). Link aborted, PLEASE SYNC YOUR CLOCKS!");
+						WriteOpers("*** \2ERROR\2: Your clocks are out by %d seconds (this is more than five minutes). Link aborted, \2PLEASE SYNC YOUR CLOCKS!\2",abs(delta));
+						this->WriteLine("ERROR :Your clocks are out by "+ConvToStr(abs(delta))+" seconds (this is more than five minutes). Link aborted, PLEASE SYNC YOUR CLOCKS!");
 						return false;
+					}
+					else if ((delta < -60) || (delta > 60))
+					{
+						WriteOpers("*** \2WARNING\2: Your clocks are out by %d seconds, please consider synching your clocks.",abs(delta));
 					}
 					this->LinkState = CONNECTED;
 					Node = new TreeServer(InboundServerName,InboundDescription,TreeRoot,this);
