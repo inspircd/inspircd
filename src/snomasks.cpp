05d6973c72d019def3e89ac68c19b4fab94d3369
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-07-10 14:33:43 +0000

    Add headers and header comment
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@4271 e03df62e-2008-0410-955e-edbf42e46eb7

---
+++ src/snomasks.cpp
@@ -0,0 +1,31 @@
+/*       +------------------------------------+
+ *       | Inspire Internet Relay Chat Daemon |
+ *       +------------------------------------+
+ *
+ *  InspIRCd is copyright (C) 2002-2006 ChatSpike-Dev.
+ *                       E-mail:
+ *                <brain@chatspike.net>
+ *                <Craig@chatspike.net>
+ *
+ * Written by Craig Edwards, Craig McLure, and others.
+ * This program is free but copyrighted software; see
+ *            the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
+#include <string>
+#include <vector>
+#include "configreader.h"
+#include "users.h"
+#include "modules.h"
+#include "commands.h"
+#include "xline.h"
+#include "helperfuncs.h"
+#include "snomasks.h"
+
+extern ServerConfig* Config;
+extern int MODCOUNT;
+extern std::vector<Module*> modules;
+extern std::vector<ircd_module*> factory;
+
