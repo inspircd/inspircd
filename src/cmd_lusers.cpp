7d8bca01e0bb5e4e7ea0a6bca29b42584ca6d6ae
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-07-25 11:20:53 +0000

    Exclude LUSERS lines with zero counts (NO WE WONT CHANGE THE SPELLING OF connection(s)!)
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@4543 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/cmd_lusers.cpp
+++ src/cmd_lusers.cpp
@@ -24,8 +24,12 @@ void cmd_lusers::Handle (const char** parameters, int pcnt, userrec *user)
 	// this lusers command shows one server at all times because
 	// a protocol module must override it to show those stats.
 	WriteServ(user->fd,"251 %s :There are %d users and %d invisible on 1 server",user->nick,usercnt()-usercount_invisible(),usercount_invisible());
-	WriteServ(user->fd,"252 %s %d :operator(s) online",user->nick,usercount_opers());
-	WriteServ(user->fd,"253 %s %d :unknown connections",user->nick,usercount_unknown());
-	WriteServ(user->fd,"254 %s %d :channels formed",user->nick,chancount());
-	WriteServ(user->fd,"254 %s :I have %d clients and 0 servers",user->nick,local_count());
+	if (usercount_opers())
+		WriteServ(user->fd,"252 %s %d :operator(s) online",user->nick,usercount_opers());
+	if (usercount_unknown())
+		WriteServ(user->fd,"253 %s %d :unknown connections",user->nick,usercount_unknown());
+	if (chancount())
+		WriteServ(user->fd,"254 %s %d :channels formed",user->nick,chancount());
+	if (local_count())
+		WriteServ(user->fd,"254 %s :I have %d clients and 0 servers",user->nick,local_count());
 }
