7639c4bf92149ff65be6e09d6eca5db68debc760
om <om@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-08-09 15:00:06 +0000

    Fix for when we hit PgSQL 9.x
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@4814 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/extra/pgsql_config.pl
+++ src/modules/extra/pgsql_config.pl
@@ -3,4 +3,4 @@
 my $v = substr(`pg_config --version`, 11);
 my($a, $b, $c) = split(/\./, $v);
 
-print "-D PGSQL_HAS_ESCAPECONN" if(($a >= 8) and ($b >= 1) and ($c >= 4));
+print "-D PGSQL_HAS_ESCAPECONN" if((($a = 8) and ($b >= 1) and ($c >= 4)) or ($a > 8));
