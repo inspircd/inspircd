4c233483f0a0f93127ee519662cafd001c7a5d6e
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-08-30 19:57:51 +0000

    In this instance find is faster than rfind
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@5086 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/mode.cpp
+++ src/mode.cpp
@@ -410,7 +410,7 @@ void ModeParser::Process(const char** parameters, int pcnt, userrec *user, bool 
 									parameter = parameters[parameter_counter++];
 
 									/* Yerk, invalid! */
-									if ((parameter.rfind(':') == 0) || (parameter.rfind(' ') != std::string::npos))
+									if ((parameter.find(':') == 0) || (parameter.rfind(' ') != std::string::npos))
 										parameter = "";
 								}
 								else
