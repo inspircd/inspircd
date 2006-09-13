b4a34260192496f1adbf042575dbd13d28079129
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-09-13 22:06:08 +0000

    Add jamie to code contributors in /info
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@5239 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/cmd_info.cpp
+++ src/cmd_info.cpp
@@ -41,6 +41,7 @@ CmdResult cmd_info::Handle (const char** parameters, int pcnt, userrec *user)
 	user->WriteServ( "371 %s :        typobox43", user->nick);
 	user->WriteServ( "371 %s :        Jazza", user->nick);
 	user->WriteServ( "371 %s :        pippijn", user->nick);
+	user->WriteServ( "371 %s :        jamie", user->nick);
 	user->WriteServ( "371 %s : ", user->nick);
 	user->WriteServ( "371 %s :Testers:", user->nick);
 	user->WriteServ( "371 %s :        CC", user->nick);
