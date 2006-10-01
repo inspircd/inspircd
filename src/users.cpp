db8d1d68f8c1bad721969e0eb71bea1d5ad8c5d7
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-10-01 16:42:40 +0000

    userrec::CountGlobalClones malfunctioning, spotted by jamie, fixes bug#157 effecting only beta2
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@5369 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/users.cpp
+++ src/users.cpp
@@ -983,18 +983,16 @@ void userrec::AddClient(InspIRCd* Instance, int socket, int port, bool iscached,
 
 long userrec::GlobalCloneCount()
 {
-	char u1[128] = {0};
-	char u2[128] = {0};
+	char u1[128];
+	char u2[128];
 	long x = 0;
-	
-	this->GetIPString(u2);
-	
+
 	for (user_hash::const_iterator a = ServerInstance->clientlist.begin(); a != ServerInstance->clientlist.end(); a++)
 	{
 		/* We have to match ip's as strings - we don't know what protocol
 		 * a remote user may be using
 		 */
-		if (strcmp(a->second->GetIPString(u1), u2) == 0)
+		if (strcmp(a->second->GetIPString(u1), this->GetIPString(u2)) == 0)
 			x++;
 	}
 	
