d32c5c0090c10c0ebfc221c0d1f3d25912808fb7
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-10-16 13:06:13 +0000

    More crashbug fixage, half way through indent fix
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@5475 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_dccallow.cpp
+++ src/modules/m_dccallow.cpp
@@ -17,21 +17,21 @@ static ConfigReader *Conf;
 class BannedFileList
 {
  public:
-std::string filemask;
-std::string action;
+	std::string filemask;
+	std::string action;
 };
 
 class DCCAllow
 {
  public:
-std::string nickname;
-std::string hostmask;
-time_t set_on;
-long length;
+	std::string nickname;
+	std::string hostmask;
+	time_t set_on;
+	long length;
 
-DCCAllow() { }
+	DCCAllow() { }
 
-DCCAllow(std::string nick, std::string hm, time_t so, long ln) : nickname(nick), hostmask(hm), set_on(so), length(ln) { }
+	DCCAllow(std::string nick, std::string hm, time_t so, long ln) : nickname(nick), hostmask(hm), set_on(so), length(ln) { }
 };
 
 typedef std::vector<userrec *> userlist;
@@ -44,154 +44,152 @@ bannedfilelist bfl;
 class cmd_dccallow : public command_t
 {
  public:
-cmd_dccallow(InspIRCd* Me) : command_t(Me, "DCCALLOW", 0, 0)
-{
-	this->source = "m_dccallow.so";
-	syntax = "{[+|-]<nick> <time>}";
-}
-
-CmdResult Handle(const char **parameters, int pcnt, userrec *user)
-{
-	/* syntax: DCCALLOW [+|-]<nick> (<time>) */
-	
-	if (!pcnt)
+	cmd_dccallow(InspIRCd* Me) : command_t(Me, "DCCALLOW", 0, 0)
 	{
-	// display current DCCALLOW list
-	DisplayDCCAllowList(user);
-
-	return CMD_SUCCESS;
+		this->source = "m_dccallow.so";
+		syntax = "{[+|-]<nick> <time>}";
 	}
-	else if (pcnt > 0)
+
+	CmdResult Handle(const char **parameters, int pcnt, userrec *user)
 	{
-	char action = *parameters[0];
-	
-	// if they didn't specify an action, this is probably a command
-	if (action != '+' && action != '-')
-	{
-		if (!strcasecmp(parameters[0], "LIST"))
+		/* syntax: DCCALLOW [+|-]<nick> (<time>) */
+		if (!pcnt)
 		{
-		// list current DCCALLOW list
-		DisplayDCCAllowList(user);
-		return CMD_SUCCESS;
-		} 
-		else if (!strcasecmp(parameters[0], "HELP"))
-		{
-		// display help
-		DisplayHelp(user);
-		return CMD_SUCCESS;
+			// display current DCCALLOW list
+			DisplayDCCAllowList(user);
+			return CMD_SUCCESS;
 		}
-	}
+		else if (pcnt > 0)
+		{
+			char action = *parameters[0];
 		
-	std::string nick = parameters[0] + 1;
-	userrec *target = ServerInstance->FindNick(nick);
-
-	if (target)
-	{
-		ServerInstance->Log(DEBUG, "m_dccallow.so: got target %s and action %c", target->nick, action);
-			
-		if (action == '-')
-		{
-		user->GetExt("dccallow_list", dl);
-		// check if it contains any entries
-		if (dl)
-		{
-			if (dl->size())
+			// if they didn't specify an action, this is probably a command
+			if (action != '+' && action != '-')
 			{
-			for (dccallowlist::iterator i = dl->begin(); i != dl->end(); ++i)
-			{
-				// search through list
-				if (i->nickname == target->nick)
+				if (!strcasecmp(parameters[0], "LIST"))
 				{
-				dl->erase(i);
-				user->WriteServ("995 %s %s :Removed %s from your DCCALLOW list", user->nick, user->nick, target->nick);
-				break;
+					// list current DCCALLOW list
+					DisplayDCCAllowList(user);
+					return CMD_SUCCESS;
+				} 
+				else if (!strcasecmp(parameters[0], "HELP"))
+				{
+					// display help
+					DisplayHelp(user);
+					return CMD_SUCCESS;
 				}
 			}
-			}
-		}
-		else
-		{
-			DELETE(dl);
-			user->Shrink("dccallow_list");
 			
-			// remove from userlist
-			for (userlist::iterator j = ul.begin(); j != ul.end(); ++j)
+			std::string nick = parameters[0] + 1;
+			userrec *target = ServerInstance->FindNick(nick);
+	
+			if (target)
 			{
-			userrec* u = (userrec*)(*j);
-			if (u->nick == user->nick)
+				ServerInstance->Log(DEBUG, "m_dccallow.so: got target %s and action %c", target->nick, action);
+				
+				if (action == '-')
+				{
+					user->GetExt("dccallow_list", dl);
+					// check if it contains any entries
+					if (dl)
+					{
+						if (dl->size())
+						{
+							for (dccallowlist::iterator i = dl->begin(); i != dl->end(); ++i)
+							{
+								// search through list
+								if (i->nickname == target->nick)
+								{
+									dl->erase(i);
+									user->WriteServ("995 %s %s :Removed %s from your DCCALLOW list", user->nick, user->nick, target->nick);
+									break;
+								}
+							}
+						}
+					}
+					else
+					{
+						DELETE(dl);
+						user->Shrink("dccallow_list");
+				
+						// remove from userlist
+						for (userlist::iterator j = ul.begin(); j != ul.end(); ++j)
+						{
+							userrec* u = (userrec*)(*j);
+							if (u->nick == user->nick)
+							{
+								ul.erase(j);
+								break;
+							}
+						}
+					}
+				}
+				else if (action == '+')
+				{
+					// fetch current DCCALLOW list
+					user->GetExt("dccallow_list", dl);
+					// they don't have one, create it
+					if (!dl)
+					{
+						dl = new dccallowlist;
+						user->Extend(std::string("dccallow_list"), dl);
+						// add this user to the userlist
+						ul.push_back(user);
+					}
+					for (dccallowlist::const_iterator k = dl->begin(); k != dl->end(); ++k)
+					{
+						if (k->nickname == target->nick)
+						{
+							user->WriteServ("996 %s %s :%s is already on your DCCALLOW list", user->nick, user->nick, target->nick);
+							return CMD_SUCCESS;
+						}
+					}
+				
+					std::string mask = std::string(target->nick)+"!"+std::string(target->ident)+"@"+std::string(target->dhost);
+					std::string default_length = Conf->ReadValue("dccallow", "length", 0).c_str();
+		
+					long length;
+					if (pcnt == 1 || ServerInstance->Duration(parameters[1]) < 1)
+					{
+						length = ServerInstance->Duration(default_length.c_str());
+					} 
+					else if (parameters[1] == 0)
+					{
+						length = 0;
+					}
+					else
+					{
+						length = ServerInstance->Duration(parameters[1]);
+					}
+	
+					if (!ServerInstance->IsValidMask(mask.c_str()))
+					{
+						return CMD_FAILURE;
+					}
+			
+					dl->push_back(DCCAllow(target->nick, mask, ServerInstance->Time(), length));
+			
+					if (length > 0)
+					{
+						user->WriteServ("993 %s %s :Added %s to DCCALLOW list for %d seconds", user->nick, user->nick, target->nick, length);
+					}
+					else
+					{
+						user->WriteServ("994 %s %s :Added %s to DCCALLOW list for this session", user->nick, user->nick, target->nick);
+					}
+				
+					return CMD_SUCCESS;
+				}
+			}
+			else
 			{
-				ul.erase(j);
-				break;
-			}
-			}
-		}
-		}
-		else if (action == '+')
-		{
-		// fetch current DCCALLOW list
-		user->GetExt("dccallow_list", dl);
-		// they don't have one, create it
-		if (!dl)
-		{
-			dl = new dccallowlist;
-			user->Extend(std::string("dccallow_list"), dl);
-			// add this user to the userlist
-			ul.push_back(user);
-		}
-		for (dccallowlist::const_iterator k = dl->begin(); k != dl->end(); ++k)
-		{
-			if (k->nickname == target->nick)
-			{
-			user->WriteServ("996 %s %s :%s is already on your DCCALLOW list", user->nick, user->nick, target->nick);
-			return CMD_SUCCESS;
+				// nick doesn't exist
+				user->WriteServ("401 %s %s :No such nick/channel", user->nick, nick.c_str());
+				return CMD_FAILURE;
 			}
 		}
-		
-		std::string mask = std::string(target->nick)+"!"+std::string(target->ident)+"@"+std::string(target->dhost);
-		const char* default_length = Conf->ReadValue("dccallow", "length", 0).c_str();
-
-		long length;
-		if (pcnt == 1 || ServerInstance->Duration(parameters[1]) < 1)
-		{
-			length = ServerInstance->Duration(default_length);
-		} 
-		else if (parameters[1] == 0)
-		{
-			length = 0;
-		}
-		else
-		{
-			length = ServerInstance->Duration(parameters[1]);
-		}
-
-		if (!ServerInstance->IsValidMask(mask.c_str()))
-		{
-			return CMD_FAILURE;
-		}
-		
-		dl->push_back(DCCAllow(target->nick, mask, ServerInstance->Time(), length));
-
-		if (length > 0)
-		{
-			user->WriteServ("993 %s %s :Added %s to DCCALLOW list for %d seconds", user->nick, user->nick, target->nick, length);
-		}
-		else
-		{
-			user->WriteServ("994 %s %s :Added %s to DCCALLOW list for this session", user->nick, user->nick, target->nick);
-		}
-		
 		return CMD_SUCCESS;
-		}
 	}
-	else
-	{
-		// nick doesn't exist
-		user->WriteServ("401 %s %s :No such nick/channel", user->nick, nick.c_str());
-		return CMD_FAILURE;
-	}
-	}
-return CMD_SUCCESS;
-}
 
 void DisplayHelp(userrec* user)
 {
@@ -225,10 +223,10 @@ void DisplayDCCAllowList(userrec* user)
 	
 	if (dl)
 	{
-	for (dccallowlist::const_iterator c = dl->begin(); c != dl->end(); ++c)
-	{
-		user->WriteServ("991 %s %s :%s (%s)", user->nick, user->nick, c->nickname.c_str(), c->hostmask.c_str());
-	}
+		for (dccallowlist::const_iterator c = dl->begin(); c != dl->end(); ++c)
+		{
+			user->WriteServ("991 %s %s :%s (%s)", user->nick, user->nick, c->nickname.c_str(), c->hostmask.c_str());
+		}
 	}
 	
 	user->WriteServ("992 %s :End of DCCALLOW list", user->nick);
@@ -238,11 +236,10 @@ void DisplayDCCAllowList(userrec* user)
 	
 class ModuleDCCAllow : public Module
 {
-cmd_dccallow* mycommand;
-
+	cmd_dccallow* mycommand;
  public:
-ModuleDCCAllow(InspIRCd* Me)
-	: Module::Module(Me)
+	ModuleDCCAllow(InspIRCd* Me)
+		: Module::Module(Me)
 {
 	Conf = new ConfigReader(ServerInstance);
 	mycommand = new cmd_dccallow(ServerInstance);
@@ -257,7 +254,7 @@ void Implements(char* List)
 
 virtual void OnRehash(const std::string &parameter)
 {
-		delete Conf;
+	delete Conf;
 	Conf = new ConfigReader(ServerInstance);
 }
 
@@ -269,9 +266,9 @@ virtual void OnUserQuit(userrec* user, const std::string &reason)
 	user->GetExt("dccallow_list", dl);
 	if (dl)
 	{
-	DELETE(dl);
-	user->Shrink("dccallow_list");
-	RemoveFromUserlist(user);
+		DELETE(dl);
+		user->Shrink("dccallow_list");
+		RemoveFromUserlist(user);
 	}
 	
 	// remove them from any DCCALLOW lists
@@ -297,84 +294,84 @@ virtual int OnUserPreNotice(userrec* user, void* dest, int target_type, std::str
 
 	if (target_type == TYPE_USER)
 	{
-	userrec* u = (userrec*)dest;
+		userrec* u = (userrec*)dest;
 	
-	if ((text.length()) && (text[0] == '\1'))
-	{
-		// :jamie!jamie@test-D4457903BA652E0F.silverdream.org PRIVMSG eimaj :DCC SEND m_dnsbl.cpp 3232235786 52650 9676
-		// :jamie!jamie@test-D4457903BA652E0F.silverdream.org PRIVMSG eimaj :VERSION
-			
-		if (strncmp(text.c_str(), "\1DCC ", 5) == 0)
+		if ((text.length()) && (text[0] == '\1'))
 		{
-		u->GetExt("dccallow_list", dl);
-
-		if (dl)
-		{
-			if (dl->size())
+			// :jamie!jamie@test-D4457903BA652E0F.silverdream.org PRIVMSG eimaj :DCC SEND m_dnsbl.cpp 3232235786 52650 9676
+			// :jamie!jamie@test-D4457903BA652E0F.silverdream.org PRIVMSG eimaj :VERSION
+				
+			if (strncmp(text.c_str(), "\1DCC ", 5) == 0)
 			{
-			for (dccallowlist::const_iterator iter = dl->begin(); iter != dl->end(); ++iter)
-			{
-				if (ServerInstance->MatchText(user->GetFullHost(), iter->hostmask))
+				u->GetExt("dccallow_list", dl);
+	
+				if (dl)
 				{
-				return 0;
+					if (dl->size())
+					{
+						for (dccallowlist::const_iterator iter = dl->begin(); iter != dl->end(); ++iter)
+						{
+							if (ServerInstance->MatchText(user->GetFullHost(), iter->hostmask))
+							{
+								return 0;
+							}
+						}
+					}
 				}
-			}
-			}
-		}
-
-		// tokenize
-		stringstream ss(text);
-		std::string buf;
-		vector<string> tokens;
-
-		while (ss >> buf)
-			tokens.push_back(buf);
-
-		irc::string type = tokens[1].c_str();
-		ServerInstance->Log(DEBUG, "m_dccallow.so: got DCC type %s", type.c_str());
-
-		bool blockchat = Conf->ReadFlag("dccallow", "blockchat", 0);
-
-		if (type == "SEND")
-		{
-			std::string defaultaction = Conf->ReadValue("dccallow", "action", 0);
-			std::string filename = tokens[2];
-			
-			if (defaultaction == "allow") 
-			{
-			return 0;
-			}
-			
-			for (unsigned int i = 0; i < bfl.size(); i++)
-			{
-			if (ServerInstance->MatchText(filename, bfl[i].filemask))
-			{
-				if (strcmp(bfl[i].action.c_str(), "allow") == 0)
+	
+				// tokenize
+				stringstream ss(text);
+				std::string buf;
+				vector<string> tokens;
+	
+				while (ss >> buf)
+					tokens.push_back(buf);
+	
+				irc::string type = tokens[1].c_str();
+				ServerInstance->Log(DEBUG, "m_dccallow.so: got DCC type %s", type.c_str());
+	
+				bool blockchat = Conf->ReadFlag("dccallow", "blockchat", 0);
+	
+				if (type == "SEND")
 				{
-				return 0;
+					std::string defaultaction = Conf->ReadValue("dccallow", "action", 0);
+					std::string filename = tokens[2];
+				
+					if (defaultaction == "allow") 
+					{
+						return 0;
+					}
+			
+					for (unsigned int i = 0; i < bfl.size(); i++)
+					{
+						if (ServerInstance->MatchText(filename, bfl[i].filemask))
+						{
+							if (strcmp(bfl[i].action.c_str(), "allow") == 0)
+							{
+								return 0;
+							}
+						}
+						else
+						{
+							if (defaultaction == "allow")
+							{
+								return 0;
+							}
+						}
+						user->WriteServ("NOTICE %s :The user %s is not accepting DCC SENDs from you. Your file %s was not sent.", user->nick, u->nick, filename.c_str());
+						u->WriteServ("NOTICE %s :%s (%s@%s) attempted to send you a file named %s, which was blocked.", u->nick, user->nick, user->ident, user->dhost, filename.c_str());
+						u->WriteServ("NOTICE %s :If you trust %s and were expecting this, you can type /DCCALLOW HELP for information on the DCCALLOW system.", u->nick, user->nick);
+					}
 				}
-			}
-			else
-			{
-				if (defaultaction == "allow")
+				else if ((type == "CHAT") && (blockchat))
 				{
-				return 0;
+					user->WriteServ("NOTICE %s :The user %s is not accepting DCC CHAT requests from you.", user->nick, u->nick);
+					u->WriteServ("NOTICE %s :%s (%s@%s) attempted to initiate a DCC CHAT session, which was blocked.", u->nick, user->nick, user->ident, user->dhost);
+					u->WriteServ("NOTICE %s :If you trust %s and were expecting this, you can type /DCCALLOW HELP for information on the DCCALLOW system.", u->nick, user->nick);
 				}
-			}
-			user->WriteServ("NOTICE %s :The user %s is not accepting DCC SENDs from you. Your file %s was not sent.", user->nick, u->nick, filename.c_str());
-			u->WriteServ("NOTICE %s :%s (%s@%s) attempted to send you a file named %s, which was blocked.", u->nick, user->nick, user->ident, user->dhost, filename.c_str());
-			u->WriteServ("NOTICE %s :If you trust %s and were expecting this, you can type /DCCALLOW HELP for information on the DCCALLOW system.", u->nick, user->nick);
+				return 1;
 			}
 		}
-		else if ((type == "CHAT") && (blockchat))
-		{
-			user->WriteServ("NOTICE %s :The user %s is not accepting DCC CHAT requests from you.", user->nick, u->nick);
-			u->WriteServ("NOTICE %s :%s (%s@%s) attempted to initiate a DCC CHAT session, which was blocked.", u->nick, user->nick, user->ident, user->dhost);
-			u->WriteServ("NOTICE %s :If you trust %s and were expecting this, you can type /DCCALLOW HELP for information on the DCCALLOW system.", u->nick, user->nick);
-		}
-		return 1;
-		}
-	}
 	}
 	return 0;
 }
@@ -383,35 +380,35 @@ void Expire()
 {
 	for (userlist::iterator iter = ul.begin(); iter != ul.end(); ++iter)
 	{
-	userrec* u = (userrec*)(*iter);
-	u->GetExt("dccallow_list", dl);
+		userrec* u = (userrec*)(*iter);
+		u->GetExt("dccallow_list", dl);
 
-	if (dl)
-	{
-		if (dl->size())
+		if (dl)
 		{
-		dccallowlist::iterator iter = dl->begin();
-		while (iter != dl->end())
-		{
-			if ((iter->set_on + iter->length) <= ServerInstance->Time())
+			if (dl->size())
 			{
-			u->WriteServ("997 %s %s :DCCALLOW entry for %s has expired", u->nick, u->nick, iter->nickname.c_str());
-			iter = dl->erase(iter);
-			}
-			else
-			{
-			++iter;
+				dccallowlist::iterator iter = dl->begin();
+				while (iter != dl->end())
+				{
+					if ((iter->set_on + iter->length) <= ServerInstance->Time())
+					{
+						u->WriteServ("997 %s %s :DCCALLOW entry for %s has expired", u->nick, u->nick, iter->nickname.c_str());
+						iter = dl->erase(iter);
+					}
+					else
+					{
+						++iter;
+					}
+				}
 			}
 		}
+		else
+		{
+			DELETE(dl);
+			u->Shrink("dccallow_list");
+			RemoveFromUserlist(u);
+			ServerInstance->Log(DEBUG, "m_dccallow.so: UH OH! Couldn't get DCCALLOW list for %s", u->nick);
 		}
-	}
-	else
-	{
-		DELETE(dl);
-		u->Shrink("dccallow_list");
-		RemoveFromUserlist(u);
-		ServerInstance->Log(DEBUG, "m_dccallow.so: UH OH! Couldn't get DCCALLOW list for %s", u->nick);
-	}
 	}
 }
 
@@ -420,89 +417,89 @@ void RemoveNick(userrec* user)
 	/* Iterate through all DCCALLOW lists and remove user */
 	for (userlist::iterator iter = ul.begin(); iter != ul.end(); ++iter)
 	{
-	userrec *u = (userrec*)(*iter);
-	u->GetExt("dccallow_list", dl);
+		userrec *u = (userrec*)(*iter);
+		u->GetExt("dccallow_list", dl);
 
-	if (dl)
-	{
-		if (dl->size())
+		if (dl)
 		{
-		for (dccallowlist::iterator i = dl->begin(); i != dl->end(); ++i)
-		{
-			if (i->nickname == user->nick)
+			if (dl->size())
 			{
-			
-			u->WriteServ("NOTICE %s :%s left the network or changed their nickname and has been removed from your DCCALLOW list", u->nick, i->nickname.c_str());
-			u->WriteServ("995 %s %s :Removed %s from your DCCALLOW list", u->nick, u->nick, i->nickname.c_str());
-			dl->erase(i);
-			break;
+				for (dccallowlist::iterator i = dl->begin(); i != dl->end(); ++i)
+				{
+					if (i->nickname == user->nick)
+					{
+				
+						u->WriteServ("NOTICE %s :%s left the network or changed their nickname and has been removed from your DCCALLOW list", u->nick, i->nickname.c_str());
+						u->WriteServ("995 %s %s :Removed %s from your DCCALLOW list", u->nick, u->nick, i->nickname.c_str());
+						dl->erase(i);
+						break;
+					}
+				}
 			}
 		}
+		else
+		{
+			DELETE(dl);
+			u->Shrink("dccallow_list");
+			RemoveFromUserlist(u);
 		}
 	}
-	else
+}
+
+	void RemoveFromUserlist(userrec *user)
 	{
-		DELETE(dl);
-		u->Shrink("dccallow_list");
-		RemoveFromUserlist(u);
+		// remove user from userlist
+		for (userlist::iterator j = ul.begin(); j != ul.end(); ++j)
+		{
+			userrec* u = (userrec*)(*j);
+			if (u->nick == user->nick)
+			{
+					ul.erase(j);
+				break;
+			}
+		}
 	}
-	}
-}
 
-void RemoveFromUserlist(userrec *user)
-{
-	// remove user from userlist
-	for (userlist::iterator j = ul.begin(); j != ul.end(); ++j)
+	void ReadFileConf()
 	{
-	userrec* u = (userrec*)(*j);
-	if (u->nick == user->nick)
+		bfl.clear();
+		for (int i = 0; i < Conf->Enumerate("banfile"); i++)
+		{
+			BannedFileList bf;
+			std::string fileglob = Conf->ReadValue("banfile", "pattern", i);
+			std::string action = Conf->ReadValue("banfile", "action", i);
+			bf.filemask = fileglob;
+			bf.action = action;
+			bfl.push_back(bf);
+		}
+	
+	}
+
+	virtual ~ModuleDCCAllow()
 	{
-		ul.erase(j);
-		break;
 	}
-	}
-}
 
-void ReadFileConf()
-{
-	bfl.clear();
-	for (int i = 0; i < Conf->Enumerate("banfile"); i++)
+	virtual Version GetVersion()
 	{
-	BannedFileList bf;
-	std::string fileglob = Conf->ReadValue("banfile", "pattern", i);
-	std::string action = Conf->ReadValue("banfile", "action", i);
-	bf.filemask = fileglob;
-	bf.action = action;
-	bfl.push_back(bf);
+		return Version(1,0,0,0,VF_COMMON,API_VERSION);
 	}
-
-}
-
-virtual ~ModuleDCCAllow()
-{
-}
-
-virtual Version GetVersion()
-{
-	return Version(1,0,0,0,VF_COMMON,API_VERSION);
-}
 };
 
 class ModuleDCCAllowFactory : public ModuleFactory
 {
  public:
-ModuleDCCAllowFactory()
-{
-}
+	ModuleDCCAllowFactory()
+	{
+	}
 
-~ModuleDCCAllowFactory()
-{
-}
+	~ModuleDCCAllowFactory()
+	{
+	}
 
-virtual Module * CreateModule(InspIRCd* Me)
-{
-	return new ModuleDCCAllow(Me);
-}
+	virtual Module * CreateModule(InspIRCd* Me)
+	{
+		return new ModuleDCCAllow(Me);
+	}
 
 };
 
