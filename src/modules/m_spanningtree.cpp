23dba98a52ec5c666dc8fd57460588bd205bf1db
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-10-17 20:49:55 +0000

    Move the declaration of class Link up top
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@5494 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_spanningtree.cpp
+++ src/modules/m_spanningtree.cpp
@@ -80,6 +80,28 @@ class ModuleSpanningTree;
  */
 typedef nspace::hash_map<std::string, TreeServer*, nspace::hash<string>, irc::StrHashComp> server_hash;
 
+
+/** The Link class might as well be a struct,
+ * but this is C++ and we don't believe in structs (!).
+ * It holds the entire information of one <link>
+ * tag from the main config file. We maintain a list
+ * of them, and populate the list on rehash/load.
+ */
+class Link : public classbase
+{
+ public:
+	irc::string Name;
+	std::string IPAddr;
+	int Port;
+	std::string SendPass;
+	std::string RecvPass;
+	unsigned long AutoConnect;
+	time_t NextConnectTime;
+	std::string EncryptionKey;
+	bool HiddenFromStats;
+	std::string FailOver;
+};
+
 /** Contains helper functions and variables for this module,
  * and keeps them out of the global namespace
  */
@@ -489,27 +511,6 @@ class TreeServer : public classbase
 		/* We'd better tidy up after ourselves, eh? */
 		this->DelHashEntry();
 	}
-};
-
-/** The Link class might as well be a struct,
- * but this is C++ and we don't believe in structs (!).
- * It holds the entire information of one <link>
- * tag from the main config file. We maintain a list
- * of them, and populate the list on rehash/load.
- */
-class Link : public classbase
-{
- public:
-	irc::string Name;
-	std::string IPAddr;
-	int Port;
-	std::string SendPass;
-	std::string RecvPass;
-	unsigned long AutoConnect;
-	time_t NextConnectTime;
-	std::string EncryptionKey;
-	bool HiddenFromStats;
-	irc::string FailOver;
 };
 
 /** Yay for fast searches!
