87b531bf29e52d2f0f42b244604715f9af15b48c
om <om@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-11-19 16:19:46 +0000

    Also return for 7.x series > 7.3.15, this still wants testing :p (which versions actually have the functions, I'm pretty happy with the script working properly)
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@5778 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/extra/pgsql_config.pl
+++ src/modules/extra/pgsql_config.pl
@@ -1,4 +1,7 @@
 #!/usr/bin/perl
 
-`pg_config --version` =~ /^.*?(\d+)\.(\d+)\.(\d+).*?$/;
-print "-D PGSQL_HAS_ESCAPECONN" if(hex(sprintf("0x%02x%02x%02x", $1, $2, $3)) >= 0x080104);
+my $s = `pg_config --version`;
+$s = shift;
+$s =~ /^.*?(\d+)\.(\d+)\.(\d+).*?$/;
+my $v = hex(sprintf("0x%02x%02x%02x", $1, $2, $3));
+print "-D PGSQL_HAS_ESCAPECONN" if(($v >= 0x080104) or (($v >= 0x07030F) and ($v < 0x080000)));
