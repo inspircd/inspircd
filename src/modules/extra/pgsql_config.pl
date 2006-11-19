ca24e71f7576591dfa5d405e16e2475aba6d2386
om <om@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-11-19 16:13:28 +0000

    Hopefully uncraq this script enough to actually work, should return for versions > 8.1.4
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@5777 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/extra/pgsql_config.pl
+++ src/modules/extra/pgsql_config.pl
@@ -1,6 +1,4 @@
 #!/usr/bin/perl
 
-my $v = substr(`pg_config --version`, 11);
-my($a, $b, $c) = split(/\./, $v);
-
-print "-D PGSQL_HAS_ESCAPECONN" if(hex(sprintf("%02x", $a) . sprintf("%02x", $b) . sprintf("%02x", $c)) >= 0x080000);
+`pg_config --version` =~ /^.*?(\d+)\.(\d+)\.(\d+).*?$/;
+print "-D PGSQL_HAS_ESCAPECONN" if(hex(sprintf("0x%02x%02x%02x", $1, $2, $3)) >= 0x080104);
