ae0f4c9d021bfdfa547bbae6818027eec36db8a8
om <om@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-11-19 16:20:25 +0000

    Ooops, remove my debug line
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@5779 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/extra/pgsql_config.pl
+++ src/modules/extra/pgsql_config.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl
 
 my $s = `pg_config --version`;
-$s = shift;
 $s =~ /^.*?(\d+)\.(\d+)\.(\d+).*?$/;
 my $v = hex(sprintf("0x%02x%02x%02x", $1, $2, $3));
 print "-D PGSQL_HAS_ESCAPECONN" if(($v >= 0x080104) or (($v >= 0x07030F) and ($v < 0x080000)));
