55b4bfe5e75838d9701f22af012a0028e7f77279
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-11-19 16:41:41 +0000

    Remove space
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@5780 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/extra/pgsql_config.pl
+++ src/modules/extra/pgsql_config.pl
@@ -3,4 +3,4 @@
 my $s = `pg_config --version`;
 $s =~ /^.*?(\d+)\.(\d+)\.(\d+).*?$/;
 my $v = hex(sprintf("0x%02x%02x%02x", $1, $2, $3));
-print "-D PGSQL_HAS_ESCAPECONN" if(($v >= 0x080104) or (($v >= 0x07030F) and ($v < 0x080000)));
+print "-DPGSQL_HAS_ESCAPECONN" if(($v >= 0x080104) or (($v >= 0x07030F) and ($v < 0x080000)));
