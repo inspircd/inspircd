6529381dde6382993d041ae8511117e205b0ebc7
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-12-11 21:40:25 +0000

    Someone forgot to add the copyright headers here :p
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@5943 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/extra/m_ssl_openssl.cpp
+++ src/modules/extra/m_ssl_openssl.cpp
@@ -1,3 +1,19 @@
+/*       +------------------------------------+
+ *       | Inspire Internet Relay Chat Daemon |
+ *       +------------------------------------+
+ *
+ *  InspIRCd is copyright (C) 2002-2006 ChatSpike-Dev.
+ *                       E-mail:
+ *                <brain@chatspike.net>
+ *                <Craig@chatspike.net>
+ *     
+ * Written by Craig Edwards, Craig McLure, and others.
+ * This program is free but copyrighted software; see
+ *            the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
 #include <string>
 #include <vector>
 
