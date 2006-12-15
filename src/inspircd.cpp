96a89aef2d59e16bd8e850180ec8b7a592491b75
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-12-15 22:41:53 +0000

    s/ChatSpike/InspIRCd/ development team
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@5999 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/inspircd.cpp
+++ src/inspircd.cpp
@@ -69,10 +69,9 @@ void InspIRCd::Exit(int status)
 void InspIRCd::Start()
 {
 	printf("\033[1;32mInspire Internet Relay Chat Server, compiled %s at %s\n",__DATE__,__TIME__);
-	printf("(C) ChatSpike Development team.\033[0m\n\n");
+	printf("(C) InspIRCd Development Team.\033[0m\n\n");
 	printf("Developers:\t\t\033[1;32mBrain, FrostyCoolSlug, w00t, Om, Special, pippijn, peavey\033[0m\n");
 	printf("Others:\t\t\t\033[1;32mSee /INFO Output\033[0m\n");
-	printf("Name concept:\t\t\033[1;32mLord_Zathras\033[0m\n\n");
 }
 
 void InspIRCd::Rehash(int status)
