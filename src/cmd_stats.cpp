a6bb2c203986c53583f64f3fc5c7c0923b30f31e
peavey <peavey@e03df62e-2008-0410-955e-edbf42e46eb7>
2006-12-25 13:54:06 +0000

    Cosmetic fix to stats z output.
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@6111 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/cmd_stats.cpp
+++ src/cmd_stats.cpp
@@ -167,7 +167,6 @@ void DoStats(InspIRCd* ServerInstance, char statschar, userrec* user, string_lis
 		/* stats z (debug and memory info) */
 		case 'z':
 		{
-
 			rusage R;
 			results.push_back(sn+" 240 "+user->nick+" :InspIRCd(CLASS) "+ConvToStr(sizeof(InspIRCd))+" bytes");
 			results.push_back(sn+" 249 "+user->nick+" :Users(HASH_MAP) "+ConvToStr(ServerInstance->clientlist->size())+" ("+ConvToStr(ServerInstance->clientlist->size()*sizeof(userrec))+" bytes, "+ConvToStr(ServerInstance->clientlist->bucket_count())+" buckets)");
@@ -192,8 +191,8 @@ void DoStats(InspIRCd* ServerInstance, char statschar, userrec* user, string_lis
 			}
 
 			results.push_back(sn+" 249 "+user->nick+" :MOTD(VECTOR) "+ConvToStr(ServerInstance->Config->MOTD.size())+", RULES(VECTOR) "+ConvToStr(ServerInstance->Config->RULES.size()));
-			results.push_back(sn+" 249 "+user->nick+" :Modules(VECTOR) "+ConvToStr(ServerInstance->modules.size())+" ("+ConvToStr(ServerInstance->modules.size()*sizeof(Module))+") bytes");
-			results.push_back(sn+" 249 "+user->nick+" :ClassFactories(VECTOR) "+ConvToStr(ServerInstance->factory.size())+" ("+ConvToStr(ServerInstance->factory.size()*sizeof(ircd_module))+") bytes");
+			results.push_back(sn+" 249 "+user->nick+" :Modules(VECTOR) "+ConvToStr(ServerInstance->modules.size())+" ("+ConvToStr(ServerInstance->modules.size()*sizeof(Module))+" bytes)");
+			results.push_back(sn+" 249 "+user->nick+" :ClassFactories(VECTOR) "+ConvToStr(ServerInstance->factory.size())+" ("+ConvToStr(ServerInstance->factory.size()*sizeof(ircd_module))+" bytes)");
 
 			if (!getrusage(0,&R))	/* RUSAGE_SELF */
 			{
