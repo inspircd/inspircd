55f4198a3528a2f80bc0723046e9946c6f8445fe
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-01-14 00:49:16 +0000

    Formatting glitch
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@6313 e03df62e-2008-0410-955e-edbf42e46eb7

--- make/utilities.pm
+++ make/utilities.pm
@@ -151,7 +151,7 @@ sub translate_functions($$)
 			{
 				$tmpfile = tmpnam();
 			} until sysopen(TF, $tmpfile, O_RDWR|O_CREAT|O_EXCL, 0700);
-			print "(Created and executed \033[1;32m$tmpfile)\033[0m\n";
+			print "(Created and executed \033[1;32m$tmpfile\033[0m)\n";
 			print TF $1;
 			close TF;
 			my $replace = `perl $tmpfile`;
