772dc6c47406b0ca6be5e49914dd739c8d580bd5
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-04-08 18:08:20 +0000

    Add support for solaris 10 IO Completion Ports, contributed by nenolod
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@6763 e03df62e-2008-0410-955e-edbf42e46eb7

---
+++ include/socketengine_ports.h
@@ -0,0 +1,68 @@
+/*       +------------------------------------+
+ *       | Inspire Internet Relay Chat Daemon |
+ *       +------------------------------------+
+ *
+ *  InspIRCd: (C) 2002-2007 InspIRCd Development Team
+ * See: http://www.inspircd.org/wiki/index.php/Credits
+ *
+ * This program is free but copyrighted software; see
+ *            the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
+#ifndef __SOCKETENGINE_PORTS__
+#define __SOCKETENGINE_PORTS__
+
+#ifndef __sun
+# error You need Solaris 10 or later to make use of this code.
+#endif
+
+#include <vector>
+#include <string>
+#include <map>
+#include "inspircd_config.h"
+#include "globals.h"
+#include "inspircd.h"
+#include "socketengine.h"
+#include <port.h>
+#define EP_DELAY 5
+
+class InspIRCd;
+
+/** A specialisation of the SocketEngine class, designed to use linux 2.6 epoll().
+ */
+class PortsEngine : public SocketEngine
+{
+private:
+	/** These are used by epoll() to hold socket events
+	 */
+	port_event_t events[MAX_DESCRIPTORS];
+public:
+	/** Create a new EPollEngine
+	 * @param Instance The creator of this object
+	 */
+	PortsEngine(InspIRCd* Instance);
+	/** Delete an EPollEngine
+	 */
+	virtual ~PortsEngine();
+	virtual bool AddFd(EventHandler* eh);
+	virtual int GetMaxFds();
+	virtual int GetRemainingFds();
+	virtual bool DelFd(EventHandler* eh, bool force = false);
+	virtual int DispatchEvents();
+	virtual std::string GetName();
+	virtual void WantWrite(EventHandler* eh);
+};
+
+/** Creates a SocketEngine
+ */
+class SocketEngineFactory
+{
+public:
+	/** Create a new instance of SocketEngine based on PortsEngine
+	 */
+	SocketEngine* Create(InspIRCd* Instance) { return new PortsEngine(Instance); }
+};
+
+#endif
