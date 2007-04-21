cbd52f4c3dc086a95a6361f1d48b8f2b71b23290
peavey <peavey@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-04-21 18:11:41 +0000

    Add *** to +s SNOTICEs
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@6820 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_sanick.cpp
+++ src/modules/m_sanick.cpp
@@ -46,13 +46,13 @@ class cmd_sanick : public command_t
 			{
 				if (source->ForceNickChange(parameters[1]))
 				{
-					ServerInstance->WriteOpers(oldnick+" used SANICK to change "+std::string(parameters[0])+" to "+parameters[1]);
+					ServerInstance->WriteOpers("*** " + oldnick+" used SANICK to change "+std::string(parameters[0])+" to "+parameters[1]);
 					return CMD_SUCCESS;
 				}
 				else
 				{
 					/* We couldnt change the nick */
-					ServerInstance->WriteOpers(oldnick+" failed SANICK (from "+std::string(parameters[0])+" to "+parameters[1]+")");
+					ServerInstance->WriteOpers("*** " + oldnick+" failed SANICK (from "+std::string(parameters[0])+" to "+parameters[1]+")");
 					return CMD_FAILURE;
 				}
 			}
