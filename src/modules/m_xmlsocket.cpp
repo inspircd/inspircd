3c362be4a28886a761b9d8d2d651dc04d78a0a2d
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-05-08 20:18:16 +0000

    Tidy up some lollercode
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@6917 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_xmlsocket.cpp
+++ src/modules/m_xmlsocket.cpp
@@ -57,7 +57,7 @@ class ModuleXMLSocket : public Module
 		for (int i = 0; i < Conf->Enumerate("bind"); i++)
 		{
 			// For each <bind> tag
-			if (((Conf->ReadValue("bind", "type", i) == "") || (Conf->ReadValue("bind", "type", i) == "clients")) && (Conf->ReadValue("bind", "xmlsocket", i) == "yes"))
+			if (((Conf->ReadValue("bind", "type", i) == "") || (Conf->ReadValue("bind", "type", i) == "clients")) && (Conf->ReadFlag("bind", "xmlsocket", i)))
 			{
 				// Get the port we're meant to be listening on with SSL
 				std::string port = Conf->ReadValue("bind", "port", i);
