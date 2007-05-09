221cd997be917025266716ee98f04dafdc87307b
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-05-09 22:03:57 +0000

    Add a module which only lets identified users and opers create new channels, as per bug #279 reported by Casey
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@6932 e03df62e-2008-0410-955e-edbf42e46eb7

---
+++ src/modules/m_regonlycreate.cpp
@@ -0,0 +1,85 @@
+/*       +------------------------------------+
+ *       | Inspire Internet Relay Chat Daemon |
+ *       +------------------------------------+
+ *
+ *  InspIRCd: (C) 2002-2007 InspIRCd Development Team
+ * See: http://www.inspircd.org/wiki/index.php/Credits
+ *
+ * This program is free but copyrighted software; see
+ *            the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
+#include "users.h"
+#include "channels.h"
+#include "modules.h"
+#include "inspircd.h"
+
+/* $ModDesc: Prevents users who's nicks are not registered from creating new channels */
+
+class ModuleRegOnlyCreate : public Module
+{
+ public:
+	ModuleRegOnlyCreate(InspIRCd* Me)
+		: Module::Module(Me)
+	{
+	}
+
+	void Implements(char* List)
+	{
+		List[I_OnUserPreJoin] = 1;
+	}
+
+	virtual int OnUserPreJoin(userrec* user, chanrec* chan, const char* cname, std::string &privs)
+	{
+		if (chan)
+			return 0;
+
+		if (IS_OPER(user))
+			return 0;
+
+		if ((!user->IsModeSet('r')) && (!user->GetExt("accountname")))
+		{
+			user->WriteServ("482 %s %s :You must have a registered nickname to create a new channel",user->nick, chan->name);
+			return 1;
+		}
+
+		return 0;
+	}
+	
+    	virtual ~ModuleRegOnlyCreate()
+	{
+	}
+	
+	virtual Version GetVersion()
+	{
+		return Version(1, 1, 0, 0, VF_VENDOR, API_VERSION);
+	}
+};
+
+
+class ModuleRegOnlyCreateFactory : public ModuleFactory
+{
+ public:
+	ModuleRegOnlyCreateFactory()
+	{
+	}
+	
+	~ModuleRegOnlyCreateFactory()
+	{
+	}
+	
+	virtual Module * CreateModule(InspIRCd* Me)
+	{
+		return new ModuleRegOnlyCreate(Me);
+	}
+	
+};
+
+
+extern "C" void * init_module( void )
+{
+	return new ModuleRegOnlyCreateFactory;
+}
+
