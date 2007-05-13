f45f5c17f03e2303e26f93083eb97ec08bef7d78
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-05-13 17:35:32 +0000

    Better logging to troubleshoot silentbursts
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@7013 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_spanningtree/treesocket1.cpp
+++ src/modules/m_spanningtree/treesocket1.cpp
@@ -962,9 +962,12 @@ bool TreeSocket::IntroduceClient(const std::string &source, std::deque<std::stri
 
 	Instance->AddGlobalClone(_new);
 
-	bool send = !(((this->Utils->quiet_bursts) && (this->bursting || Utils->FindRemoteBurstServer(remoteserver))) || (this->Instance->SilentULine(_new->server)));
+	bool dosend = !(((this->Utils->quiet_bursts) && (this->bursting || Utils->FindRemoteBurstServer(remoteserver))) || (this->Instance->SilentULine(_new->server)));
+
+	Instance->Log(DEBUG,"SNOMASK send state: dosend=%d, quiet_bursts = %d, this->bursting = %d, FRBS=%d, silentuline=%d", 
+			dosend, this->Utils->quiet_bursts, this->bursting, Utils->FindRemoteBurstServer(remoteserver) != NULL, this->Instance->SilentULine(_new->server));
 	
-	if (send)
+	if (dosend)
 		this->Instance->SNO->WriteToSnoMask('C',"Client connecting at %s: %s!%s@%s [%s] [%s]",_new->server,_new->nick,_new->ident,_new->host, _new->GetIPString(), _new->fullname);
 
 	params[7] = ":" + params[7];
