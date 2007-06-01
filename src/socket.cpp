858ea80c87d7946cec7d680a48ad326025c70af1
peavey <peavey@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-06-01 23:00:18 +0000

    re-apply ipv6 ifdef tidyup that got pulled in last revert.
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@7205 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/socket.cpp
+++ src/socket.cpp
@@ -47,10 +47,8 @@ ListenSocket::ListenSocket(InspIRCd* Instance, int port, char* addr) : ServerIns
 		if ((!*addr) || (strchr(addr,':')))
 			this->family = AF_INET6;
 		else
-			this->family = AF_INET;
-#else
-		this->family = AF_INET;
 #endif
+		this->family = AF_INET;
 		Instance->SE->AddFd(this);
 	}
 }
@@ -81,14 +79,12 @@ void ListenSocket::HandleEvent(EventType et, int errornum)
 		length = sizeof(sockaddr_in6);
 	}
 	else
+#endif
 	{
 		uslen = sizeof(sockaddr_in);
 		length = sizeof(sockaddr_in);
 	}
-#else
-	uslen = sizeof(sockaddr_in);
-	length = sizeof(sockaddr_in);
-#endif
+
 	incomingSockfd = _accept (this->GetFd(), (sockaddr*)client, &length);
 
 	if ((incomingSockfd > -1) && (!_getsockname(incomingSockfd, sock_us, &uslen)))
@@ -101,14 +97,12 @@ void ListenSocket::HandleEvent(EventType et, int errornum)
 			in_port = ntohs(((sockaddr_in6*)sock_us)->sin6_port);
 		}
 		else
+#endif
 		{
 			inet_ntop(AF_INET, &((const sockaddr_in*)client)->sin_addr, buf, sizeof(buf));
 			in_port = ntohs(((sockaddr_in*)sock_us)->sin_port);
 		}
-#else
-		inet_ntop(AF_INET, &((const sockaddr_in*)client)->sin_addr, buf, sizeof(buf));
-		in_port = ntohs(((sockaddr_in*)sock_us)->sin_port);
-#endif
+
 		NonBlocking(incomingSockfd);
 		if (ServerInstance->Config->GetIOHook(in_port))
 		{
