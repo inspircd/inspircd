72948525ec3ef03fe46553349c1892cafa5ac18c
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-06-02 15:05:49 +0000

    Add comments
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@7212 e03df62e-2008-0410-955e-edbf42e46eb7

--- win/inspircd_memory_functions.cpp
+++ win/inspircd_memory_functions.cpp
@@ -1,14 +1,24 @@
-// Use the global heap for this process for all allocate/free operations.
 #include "inspircd_win32wrapper.h"
 #include <exception>
 #include <new>
 #include <new.h>
 
+/** On windows, all dll files and executables have their own private heap,
+ * whereas on POSIX systems, shared objects loaded into an executable share
+ * the executable's heap. This means that if we pass an arbitrary pointer to
+ * a windows DLL which is not allocated in that dll, without some form of
+ * marshalling, we get a page fault. To fix this, these overrided operators
+ * new and delete use the windows HeapAlloc and HeapFree functions to claim
+ * memory from the windows global heap. This makes windows 'act like' POSIX
+ * when it comes to memory usage between dlls and exes.
+ */
+
 void * ::operator new(size_t iSize)
 {
-	void* ptr = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, iSize);		// zero memory for unix compatibility
+	void* ptr = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, iSize);		/* zero memory for unix compatibility */
 	/* This is the correct behaviour according to C++ standards for out of memory,
-	 * not returning null -- Brain*/
+	 * not returning null -- Brain
+	 */
 	if (!ptr)
 		throw std::bad_alloc();
 	else
