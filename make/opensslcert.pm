ed7912c90fad5ba4a83946af9bf46a9bd3656f23
special <special@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-06-26 05:22:03 +0000

    Fixed the order of fields in openssl certificate generation; this would make really broken certificates. Thanks to yell0w
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@7410 e03df62e-2008-0410-955e-edbf42e46eb7

--- make/opensslcert.pm
+++ make/opensslcert.pm
@@ -14,14 +14,15 @@ sub make_openssl_cert()
 	my $country = promptstring_s("Please enter your country (two letter code)", "US");
 	my $city = promptstring_s("Please enter your city", "Factory Town");
 	my $email = promptstring_s("Please enter a contact email address", "oompa\@loompa.com");
+	my $commonname = promptstring_s("Please enter the common name (domain name) of the irc server", "example.inspircd.org");
 	print FH <<__END__;
 $country
 $state
 $city
 $org
 $unit
+$commonname
 $email
-firstname.lastname@yourcompany.com
 __END__
 close(FH);
 system("cat openssl.template | openssl req -x509 -nodes -newkey rsa:1024 -keyout key.pem -out cert.pem 2>/dev/null");
