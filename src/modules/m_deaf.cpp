f12b7717f16168c87b9f50c5b1e221e917120b33
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-08-06 07:59:58 +0000

    Backport r7676
    git-svn-id: http://svn.inspircd.org/repository/branches/1_1_stable@7677 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_deaf.cpp
+++ src/modules/m_deaf.cpp
@@ -108,7 +108,7 @@ class ModuleDeaf : public Module
 
 	virtual int PreText(userrec* user,void* dest,int target_type, std::string &text, char status, CUList &exempt_list)
 	{
-		if ((target_type == TYPE_CHANNEL) & (IS_LOCAL(user)))
+		if (target_type == TYPE_CHANNEL)
 		{
 			chanrec* chan = (chanrec*)dest;
 			if (chan)
