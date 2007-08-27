c125dd1decee96ddbde3cfd5dc72fe2206c87376
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-08-27 12:43:47 +0000

    Fix for bug #401 reported by DarkStorm
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@7879 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/server.cpp
+++ src/server.cpp
@@ -86,7 +86,7 @@ void InspIRCd::BuildISupport()
 {
 	// the neatest way to construct the initial 005 numeric, considering the number of configure constants to go in it...
 	std::stringstream v;
-	v << "WALLCHOPS WALLVOICES MODES=" << MAXMODES-1 << " CHANTYPES=# PREFIX=" << this->Modes->BuildPrefixes() << " MAP MAXCHANNELS=" << Config->MaxChans << " MAXBANS=60 VBANLIST NICKLEN=" << NICKMAX-1;
+	v << "WALLCHOPS WALLVOICES MODES=" << MAXMODES << " CHANTYPES=# PREFIX=" << this->Modes->BuildPrefixes() << " MAP MAXCHANNELS=" << Config->MaxChans << " MAXBANS=60 VBANLIST NICKLEN=" << NICKMAX-1;
 	v << " CASEMAPPING=rfc1459 STATUSMSG=@%+ CHARSET=ascii TOPICLEN=" << MAXTOPIC << " KICKLEN=" << MAXKICK << " MAXTARGETS=" << Config->MaxTargets << " AWAYLEN=";
 	v << MAXAWAY << " CHANMODES=" << this->Modes->ChanModes() << " FNC NETWORK=" << Config->Network << " MAXPARA=32 ELIST=MU";
 	Config->data005 = v.str();
