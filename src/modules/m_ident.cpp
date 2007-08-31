46a4409b754ffa05dcebfe7381813a51ad479c1f
special <special@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-08-31 07:42:09 +0000

    Fixed a double free in m_ident. Found by AnMaster
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@8005 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_ident.cpp
+++ src/modules/m_ident.cpp
@@ -282,6 +282,7 @@ class ModuleIdent : public Module
 			if (user->GetExt("IDENT", identstr))
 			{
 				delete identstr;
+				user->Shrink("IDENT");
 			}
 		}
 	}
@@ -306,6 +307,7 @@ class ModuleIdent : public Module
 		if (user->GetExt("IDENT", identstr))
 		{
 			delete identstr;
+			user->Shrink("IDENT");
 		}
 	}
 
