6304c3747312e70d670f70b1dfda0ba10870c3bc
special <special@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-09-27 00:11:25 +0000

    Forgot part of the fix for bug #404
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@8086 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_spanningtree/treesocket2.cpp
+++ src/modules/m_spanningtree/treesocket2.cpp
@@ -237,8 +237,9 @@ bool TreeSocket::OperType(const std::string &prefix, std::deque<std::string> &pa
 	userrec* u = this->Instance->FindNick(prefix);
 	if (u)
 	{
+		if (!u->IsModeSet('o'))
+			this->Instance->all_opers.push_back(u);
 		u->modes[UM_OPERATOR] = 1;
-		this->Instance->all_opers.push_back(u);
 		strlcpy(u->oper,opertype.c_str(),NICKMAX-1);
 		Utils->DoOneToAllButSender(u->nick,"OPERTYPE",params,u->server);
 
