85684d458072b3a8b7c81ff976a76654194c58e4
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-10-13 23:52:24 +0000

    Move some stuff around, this will move a crash to a different place and maybe we'll catch it
    git-svn-id: http://svn.inspircd.org/repository/branches/1_1_stable@8172 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/users.cpp
+++ src/users.cpp
@@ -832,6 +832,7 @@ void userrec::UnOper()
 
 void userrec::QuitUser(InspIRCd* Instance, userrec *user, const std::string &quitreason, const char* operreason)
 {
+	user->Write("ERROR :Closing link (%s@%s) [%s]", user->ident, user->host, operreason);
 	user->muted = true;
 	Instance->GlobalCulls.AddItem(user, quitreason.c_str(), operreason);
 }
