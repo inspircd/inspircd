773afbfe3bb6ae0df1776016a080185e6094c8fb
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-10-21 14:26:25 +0000

    Set cmd_nick to have a penalty of 3
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@8253 e03df62e-2008-0410-955e-edbf42e46eb7

--- include/commands/cmd_nick.h
+++ include/commands/cmd_nick.h
@@ -30,7 +30,7 @@ class cmd_nick : public Command
  public:
 	/** Constructor for nick.
 	 */
-	cmd_nick (InspIRCd* Instance) : Command(Instance,"NICK",0,1,true), allowinvalid(false) { syntax = "<newnick>"; }
+	cmd_nick (InspIRCd* Instance) : Command(Instance,"NICK", 0, 1, true, 3), allowinvalid(false) { syntax = "<newnick>"; }
 	/** Handle command.
 	 * @param parameters The parameters to the comamnd
 	 * @param pcnt The number of parameters passed to teh command
