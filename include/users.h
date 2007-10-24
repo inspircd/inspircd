3c4730c34f86ed6ccc4120c8b61061e5a0dedc4c
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-10-24 16:08:29 +0000

    Some constructors dont initialize the refcount, namely, the one thats usually called :P
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@8346 e03df62e-2008-0410-955e-edbf42e46eb7

--- include/users.h
+++ include/users.h
@@ -181,17 +181,15 @@ public:
 		registration_timeout(source->registration_timeout), flood(source->flood), host(source->host),
 		pingtime(source->pingtime), pass(source->pass), threshold(source->threshold), sendqmax(source->sendqmax),
 		recvqmax(source->recvqmax), maxlocal(source->maxlocal), maxglobal(source->maxglobal), maxchans(source->maxchans),
-		port(source->port)
+		port(source->port), RefCount(0)
 	{
-		this->RefCount = 0;
 	}
 
 	/** Create a new connect class with no settings.
 	 */
 	ConnectClass() : type(CC_DENY), name("unnamed"), registration_timeout(0), flood(0), host(""), pingtime(0), pass(""),
-			threshold(0), sendqmax(0), recvqmax(0), maxlocal(0), maxglobal(0)
+			threshold(0), sendqmax(0), recvqmax(0), maxlocal(0), maxglobal(0), RefCount(0)
 	{
-		this->RefCount = 0;
 	}
 
 	/** Create a new connect class to ALLOW connections.
@@ -211,16 +209,15 @@ public:
 			const std::string &pas, unsigned int thres, unsigned long sendq, unsigned long recvq,
 			unsigned long maxl, unsigned long maxg, unsigned int maxc, int p = 0) :
 			type(CC_ALLOW), name(thename), registration_timeout(timeout), flood(fld), host(hst), pingtime(ping), pass(pas),
-			threshold(thres), sendqmax(sendq), recvqmax(recvq), maxlocal(maxl), maxglobal(maxg), maxchans(maxc), port(p) { }
+			threshold(thres), sendqmax(sendq), recvqmax(recvq), maxlocal(maxl), maxglobal(maxg), maxchans(maxc), port(p), RefCount(0) { }
 
 	/** Create a new connect class to DENY connections
 	 * @param thename Name of the connect class
 	 * @param hst The IP mask to deny
 	 */
 	ConnectClass(const std::string &thename, const std::string &hst) : type(CC_DENY), name(thename), registration_timeout(0),
-			flood(0), host(hst), pingtime(0), pass(""), threshold(0), sendqmax(0), recvqmax(0), maxlocal(0), maxglobal(0), maxchans(0), port(0)
+			flood(0), host(hst), pingtime(0), pass(""), threshold(0), sendqmax(0), recvqmax(0), maxlocal(0), maxglobal(0), maxchans(0), port(0), RefCount(0)
 	{
-		this->RefCount = 0;
 	}
 
 	/* Create a new connect class based on another class
@@ -231,9 +228,8 @@ public:
 				registration_timeout(source->registration_timeout), flood(source->flood), host(source->host),
 				pingtime(source->pingtime), pass(source->pass), threshold(source->threshold), sendqmax(source->sendqmax),
 				recvqmax(source->recvqmax), maxlocal(source->maxlocal), maxglobal(source->maxglobal), maxchans(source->maxchans),
-				port(source->port)
+				port(source->port), RefCount(0)
 	{
-		this->RefCount = 0;
 	}
 
 	/* Update an existing entry with new values
@@ -373,29 +369,6 @@ public:
 	unsigned long GetMaxGlobal()
 	{
 		return maxglobal;
-	}
-
-	bool operator== (ConnectClass* other)
-	{
-		return (other->GetName() == name);
-	}
-
-	void operator=(const ConnectClass* other)
-	{
-		type = other->type;
-		name = other->name;
-		registration_timeout = other->registration_timeout;
-		flood = other->flood;
-		host = other->host;
-		pingtime = other->pingtime;
-		pass = other->pass;
-		threshold = other->threshold;
-		sendqmax = other->sendqmax;
-		recvqmax = other->recvqmax;
-		maxlocal = other->maxlocal;
-		maxglobal = other->maxglobal;
-		maxchans = other->maxchans;
-		port = other->port;
 	}
 };
 
