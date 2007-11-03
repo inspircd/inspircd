841dc636d191bafd714fc72e5e517bf4d64d64b6
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-11-03 21:19:39 +0000

    Add bancache.coo/h. Nothing in them, yet.
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@8483 e03df62e-2008-0410-955e-edbf42e46eb7

---
+++ src/bancache.cpp
@@ -0,0 +1,16 @@
+/*       +------------------------------------+
+ *       | Inspire Internet Relay Chat Daemon |
+ *       +------------------------------------+
+ *
+ *  InspIRCd: (C) 2002-2007 InspIRCd Development Team
+ * See: http://www.inspircd.org/wiki/index.php/Credits
+ *
+ * This program is free but copyrighted software; see
+ *	    the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
+/* $Core: libIRCDbancache */
+
+
