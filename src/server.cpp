e525fb7b740aceccaf83576d330f95568c68ba02
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2007-11-11 18:57:57 +0000

    Take out some printf gunk
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@8572 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/server.cpp
+++ src/server.cpp
@@ -33,7 +33,6 @@ void InspIRCd::SignalHandler(int signal)
 
 void InspIRCd::Exit(int status)
 {
-	printf("exit with status %d\n", status);
 #ifdef WINDOWS
 	delete WindowsIPC;
 #endif
@@ -42,7 +41,6 @@ void InspIRCd::Exit(int status)
 		this->SendError("Exiting with status " + ConvToStr(status) + " (" + std::string(ExitCodes[status]) + ")");
 		this->Cleanup();
 	}
-	printf("Exit done.\n");
 	exit (status);
 }
 
