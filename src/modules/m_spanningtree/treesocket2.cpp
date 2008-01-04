32790543e1e5050e1d2c4c71e9465c2f065c700c
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-01-04 18:06:20 +0000

    Forward port r8629: Fix for bug #458 - servers going missing on collision etc
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@8630 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_spanningtree/treesocket2.cpp
+++ src/modules/m_spanningtree/treesocket2.cpp
@@ -1641,7 +1641,8 @@ void TreeSocket::OnTimeout()
 
 void TreeSocket::OnClose()
 {
-	if (this->LinkState == LISTENER)
+	// Test fix for big fuckup
+	if (this->LinkState != CONNECTED)
 		return;
 
 	// Connection closed.
