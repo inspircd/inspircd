bf77b312c8e15da249991cff43432c21b44807d7
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-01-17 12:02:22 +0000

    Add logger.(cpp|h), will eventually hold implementation of logging manager
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@8725 e03df62e-2008-0410-955e-edbf42e46eb7

---
+++ include/logger.h
@@ -0,0 +1,13 @@
+/*       +------------------------------------+
+ *       | Inspire Internet Relay Chat Daemon |
+ *       +------------------------------------+
+ *
+ *  InspIRCd: (C) 2002-2008 InspIRCd Development Team
+ * See: http://www.inspircd.org/wiki/index.php/Credits
+ *
+ * This program is free but copyrighted software; see
+ *            the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
