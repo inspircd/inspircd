5773bcf973708ee2d9b0eca42fd0f8076a93f4c9
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-01-26 21:53:59 +0000

    ANNOUNCENENT -> ANNOUNCEMENT
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@8754 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/snomasks.cpp
+++ src/snomasks.cpp
@@ -95,7 +95,7 @@ void SnomaskManager::SetupDefaults()
 	this->EnableSnomask('K',"REMOTEKILL");			/* Remote kill notices */
 	this->EnableSnomask('l',"LINK");			/* Link notices */
 	this->EnableSnomask('o',"OPER");			/* Oper up/down notices */
-	this->EnableSnomask('A',"ANNOUNCENENT");		/* formerly WriteOpers() - generic notices to all opers */
+	this->EnableSnomask('A',"ANNOUNCEMENT");		/* formerly WriteOpers() - generic notices to all opers */
 	this->EnableSnomask('d',"DEBUG");			/* Debug notices */
 	this->EnableSnomask('x',"XLINE");			/* Xline notice (g/z/q/k/e) */
 	this->EnableSnomask('t',"STATS");			/* Local or remote stats request */
