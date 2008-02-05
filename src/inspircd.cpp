ada79c95bdfaa00dfec752403769d6e3be3aaad4
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-02-05 13:47:48 +0000

    Add new developers to list at startup
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@8834 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/inspircd.cpp
+++ src/inspircd.cpp
@@ -421,7 +421,9 @@ InspIRCd::InspIRCd(int argc, char** argv)
 
 	printf_c("\033[1;32mInspire Internet Relay Chat Server, compiled %s at %s\n",__DATE__,__TIME__);
 	printf_c("(C) InspIRCd Development Team.\033[0m\n\n");
-	printf_c("Developers:\t\t\033[1;32mBrain, FrostyCoolSlug, w00t, Om, Special, pippijn, peavey, Burlex\033[0m\n");
+	printf_c("Developers:\n");
+	printf_c("\t\033[1;32mBrain, FrostyCoolSlug, w00t, Om, Special\n");
+	printf_c("\t\033[1;32mpippijn, peavey, aquanight, fez\033[0m\n\n");
 	printf_c("Others:\t\t\t\033[1;32mSee /INFO Output\033[0m\n");
 
 	/* Set the finished argument values */
