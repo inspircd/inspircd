60f3d89d5907049a2627d134b652c675b052602b
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-02-11 10:35:31 +0000

    Remove many unneeded headers from spanningtree files
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@8895 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_spanningtree/fhost.cpp
+++ src/modules/m_spanningtree/fhost.cpp
@@ -12,23 +12,14 @@
  */
 
 #include "inspircd.h"
-#include "commands/cmd_whois.h"
-#include "commands/cmd_stats.h"
-#include "socket.h"
-#include "wildcard.h"
 #include "xline.h"
-#include "transport.h"
-#include "socketengine.h"
 
-#include "m_spanningtree/main.h"
-#include "m_spanningtree/utils.h"
-#include "m_spanningtree/treeserver.h"
-#include "m_spanningtree/link.h"
 #include "m_spanningtree/treesocket.h"
-#include "m_spanningtree/resolvers.h"
-#include "m_spanningtree/handshaketimer.h"
+#include "m_spanningtree/treeserver.h"
+#include "m_spanningtree/utils.h"
 
-/* $ModDep: m_spanningtree/timesynctimer.h m_spanningtree/resolvers.h m_spanningtree/main.h m_spanningtree/utils.h m_spanningtree/treeserver.h m_spanningtree/link.h m_spanningtree/treesocket.h */
+/* $ModDep: m_spanningtree/utils.h m_spanningtree/treeserver.h m_spanningtree/treesocket.h */
+
 
 bool TreeSocket::ChangeHost(const std::string &prefix, std::deque<std::string> &params)
 {
--- src/modules/m_spanningtree/ftopic.cpp
+++ src/modules/m_spanningtree/ftopic.cpp
@@ -12,24 +12,14 @@
  */
 
 #include "inspircd.h"
-#include "commands/cmd_whois.h"
-#include "commands/cmd_stats.h"
-#include "socket.h"
-#include "wildcard.h"
 #include "xline.h"
-#include "transport.h"
-#include "m_hash.h"
-#include "socketengine.h"
 
-#include "m_spanningtree/main.h"
-#include "m_spanningtree/utils.h"
-#include "m_spanningtree/treeserver.h"
-#include "m_spanningtree/link.h"
 #include "m_spanningtree/treesocket.h"
-#include "m_spanningtree/resolvers.h"
-#include "m_spanningtree/handshaketimer.h"
+#include "m_spanningtree/treeserver.h"
+#include "m_spanningtree/utils.h"
 
-/* $ModDep: m_spanningtree/timesynctimer.h m_spanningtree/resolvers.h m_spanningtree/main.h m_spanningtree/utils.h m_spanningtree/treeserver.h m_spanningtree/link.h m_spanningtree/treesocket.h m_hash.h */
+/* $ModDep: m_spanningtree/utils.h m_spanningtree/treeserver.h m_spanningtree/treesocket.h */
+
 
 /** FTOPIC command */
 bool TreeSocket::ForceTopic(const std::string &source, std::deque<std::string> &params)
