85bb56eb97866f0783067459cc4428a74382779d
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-03-02 13:48:26 +0000

    Make this use the environment variable defined %TEMP% dir, rather than hard coding D:\temp (i no longer have a d:\temp after resizing and reallocating my partitions)
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@9055 e03df62e-2008-0410-955e-edbf42e46eb7

--- win/makeinstaller.bat
+++ win/makeinstaller.bat
@@ -1,7 +1,10 @@
-@mkdir d:\temp\
-move ..\bin\release\modules\m_ssl_gnutls.so d:\temp\
-move ..\bin\release\modules\m_sslinfo.so d:\temp\
-move ..\bin\release\modules\m_ssl_oper_cert.so d:\temp\
-move ..\bin\release\modules\m_filter_pcre.so d:\temp
+move ..\bin\release\modules\m_ssl_gnutls.so %TEMP%
+move ..\bin\release\modules\m_sslinfo.so %TEMP%
+move ..\bin\release\modules\m_ssl_oper_cert.so %TEMP%
+move ..\bin\release\modules\m_filter_pcre.so %TEMP%
 "C:\Program Files\NSIS\makensisw.exe" "inspircd.nsi"
-move d:\temp\*.so ..\bin\release\modules\
+move %TEMP%\m_ssl_gnutls.so ..\bin\release\modules
+move %TEMP%\m_sslinfo.so ..\bin\release\modules
+move %TEMP%\m_ssl_oper_cert.so ..\bin\release\modules
+move %TEMP%\m_filter_pcre.so ..\bin\release\modules
+
