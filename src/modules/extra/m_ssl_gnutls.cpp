f8e07df6d6fe0250044929f3c200f2f2666d8f5c
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-03-24 13:35:49 +0000

    Wheeeeee
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@9184 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/extra/m_ssl_gnutls.cpp
+++ src/modules/extra/m_ssl_gnutls.cpp
@@ -322,7 +322,7 @@ class ModuleSSLGnuTLS : public Module
 
 	virtual Version GetVersion()
 	{
-		return Version(1, 1, 0, 0, VF_VENDOR, API_VERSION);
+		return Version(1, 2, 0, 0, VF_VENDOR, API_VERSION);
 	}
 
 
--- src/modules/extra/m_testclient.cpp
+++ src/modules/extra/m_testclient.cpp
@@ -34,7 +34,7 @@ public:
 		
 	virtual Version GetVersion()
 	{
-		return Version(1, 1, 0, 0, VF_VENDOR, API_VERSION);
+		return Version(1, 2, 0, 0, VF_VENDOR, API_VERSION);
 	}
 	
 	virtual void OnBackgroundTimer(time_t)
