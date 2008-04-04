2029b53b523193dc80cf244c7b13c2edf230bc6d
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-04-04 22:26:47 +0000

    m_quietban: implements snomask +b q:
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@9340 e03df62e-2008-0410-955e-edbf42e46eb7

---
+++ src/modules/m_quietban.cpp
@@ -0,0 +1,68 @@
+/*       +------------------------------------+
+ *       | Inspire Internet Relay Chat Daemon |
+ *       +------------------------------------+
+ *
+ *  InspIRCd: (C) 2002-2008 InspIRCd Development Team
+ * See: http://www.inspircd.org/wiki/index.php/Credits
+ *
+ * This program is free but copyrighted software; see
+ *            the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
+#include "inspircd.h"
+
+/* $ModDesc: Implements extban +b q: - quiet bans */
+
+class ModuleQuietBan : public Module
+{
+ private:
+ public:
+	ModuleQuietBan(InspIRCd* Me) : Module(Me)
+	{
+		Implementation eventlist[] = { I_OnUserPreMessage, I_OnUserPreNotice };
+		ServerInstance->Modules->Attach(eventlist, this, 2);
+	}
+	
+	virtual ~ModuleQuietBan()
+	{
+	}
+	
+	virtual Version GetVersion()
+	{
+		return Version(1,2,0,0,VF_VENDOR,API_VERSION);
+	}
+
+	virtual int OnUserPreMessage(User *user, void *dest, int target_type, std::string &text, char status, CUList &exempt_list)
+	{
+		if (target_type == TYPE_CHANNEL)
+		{
+			if (((Channel *)dest)->IsExtBanned(user, 'q'))
+			{
+				user->WriteServ("NOTICE "+std::string(user->nick)+" :*** Cannot send to " + ((Channel *)dest)->name + ", as you are muted");
+				return 1;
+			}
+		}
+		
+		return 0;
+	}
+
+	virtual int OnUserPreNotice(User *user, void *dest, int target_type, std::string &text, char status, CUList &exempt_list)
+	{
+		if (target_type == TYPE_CHANNEL)
+		{
+			if (((Channel *)dest)->IsExtBanned(user, 'q'))
+			{
+				user->WriteServ("NOTICE "+std::string(user->nick)+" :*** Cannot send to " + ((Channel *)dest)->name + ", as you are muted");
+				return 1;
+			}
+		}
+
+		return 0;
+	}
+};
+
+
+MODULE_INIT(ModuleQuietBan)
+
