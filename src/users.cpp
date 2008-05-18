a14e9ff3d194001952c2ed9a296c88840908bb46
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-05-18 18:33:33 +0000

    Show count of total num of classes, for Jason
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@9740 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/users.cpp
+++ src/users.cpp
@@ -1715,6 +1715,8 @@ ConnectClass* User::SetClass(const std::string &explicit_name)
 	}
 	else
 	{
+		ServerInstance->Logs->Log("CONNECTCLASS", DEBUG, "Total classes: %d", ServerInstance->Config->Classes.size());
+
 		for (ClassVector::iterator i = ServerInstance->Config->Classes.begin(); i != ServerInstance->Config->Classes.end(); i++)
 		{
 			ConnectClass* c = *i;
