ad2a7f64c43b1aea521b79750392fde499d9b6d5
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-05-25 17:38:17 +0000

    More compile fixes
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@9805 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_sajoin.cpp
+++ src/modules/m_sajoin.cpp
@@ -37,7 +37,7 @@ class CommandSajoin : public Command
 				user->WriteNumeric(990, "%s :Cannot use an SA command on a u-lined client",user->nick.c_str());
 				return CMD_FAILURE;
 			}
-			if (IS_LOCAL(source) && !ServerInstance->IsChannel(parameters[1].c_str(), ServerInstance->Config->Limits.ChanMax))
+			if (IS_LOCAL(user) && !ServerInstance->IsChannel(parameters[1].c_str(), ServerInstance->Config->Limits.ChanMax))
 			{
 				/* we didn't need to check this for each character ;) */
 				user->WriteServ("NOTICE "+std::string(user->nick)+" :*** Invalid characters in channel name or name too long");
