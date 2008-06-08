0c4ef6e9055e245eff2fad33f5e509d4113ee4e8
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-06-08 16:44:14 +0000

    Add module that allows hiding of MAP and LINKS as per ircu, e.g. ":server.name NOTICE nick :The /MAP command has been disabled, visit: url" URL is configurable in the config file, blocks LINKS and MAP for non-opers.
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@9870 e03df62e-2008-0410-955e-edbf42e46eb7

---
+++ src/modules/m_maphide.cpp
@@ -0,0 +1,59 @@
+/*       +------------------------------------+
+ *       | Inspire Internet Relay Chat Daemon |
+ *       +------------------------------------+
+ *
+ *  InspIRCd: (C) 2002-2008 InspIRCd Development Team
+ * See: http://www.inspircd.org/wiki/index.php/Credits
+ *
+ * This program is free but copyrighted software; see
+ *            the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
+#include "inspircd.h"
+
+/* $ModDesc: Hide /MAP and /LINKS in the same form as ircu (mostly useless) */
+
+class ModuleMapHide : public Module
+{
+	std::string url;
+ public:
+	ModuleMapHide(InspIRCd* Me)
+		: Module(Me)
+	{
+		// Create a new command
+		ServerInstance->Modules->Attach(I_OnPreCommand, this);
+		ServerInstance->Modules->Attach(I_OnRehash, this);
+		OnRehash(NULL, "");
+	}
+
+	void OnRehash(User* user, const std::string &parameter)
+	{
+		ConfigReader MyConf(ServerInstance);
+		url = MyConf.ReadValue("security", "maphide", 0);
+	}
+
+	int OnPreCommand(std::string &command, std::vector<std::string> &parameters, User *user, bool validated, const std::string &original_line)
+	{
+		if (!IS_OPER(user) && !url.empty() && (command == "MAP" || command == "LINKS"))
+		{
+			user->WriteServ("NOTICE %s :/%s has been disabled; visit %s", user->nick.c_str(), command.c_str(), url.c_str());
+			return 1;
+		}
+		else
+			return 0;
+	}
+
+	virtual ~ModuleMapHide()
+	{
+	}
+
+	virtual Version GetVersion()
+	{
+		return Version(1, 2, 0, 0, VF_VENDOR, API_VERSION);
+	}
+};
+
+MODULE_INIT(ModuleMapHide)
+
