aee3d9df449b6348d4e7fbb6d1c46e9525f7e613
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-06-26 17:34:38 +0000

    Merge in patch from nenolod to make channelmode +z not count ulined clients when determining if all users on a channel are ssl before setting the mode. Thanks nenolod.
    git-svn-id: http://svn.inspircd.org/repository/branches/1_1_stable@9937 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_sslmodes.cpp
+++ src/modules/m_sslmodes.cpp
@@ -38,7 +38,7 @@ class SSLMode : public ModeHandler
 					CUList* userlist = channel->GetUsers();
 					for(CUList::iterator i = userlist->begin(); i != userlist->end(); i++)
 					{
-						if(!i->first->GetExt("ssl", dummy))
+						if(!i->first->GetExt("ssl", dummy) && !ServerInstance->ULine(i->first->server))
 						{
 							source->WriteServ("490 %s %s :all members of the channel must be connected via SSL", source->nick, channel->name);
 							return MODEACTION_DENY;
