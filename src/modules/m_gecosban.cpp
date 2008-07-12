432bfa9dfab698d35c618e648340567bdd17610c
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-07-12 21:10:15 +0000

    Add m_gecosban, implements extban +b r: - hostname bans.
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@9989 e03df62e-2008-0410-955e-edbf42e46eb7

---
+++ src/modules/m_gecosban.cpp
@@ -0,0 +1,66 @@
+/*       +------------------------------------+
+ *       | Inspire Internet Relay Chat Daemon |
+ *       +------------------------------------+
+ *
+ *  InspIRCd: (C) 2002-2008 InspIRCd Development Team
+ * See: http://www.inspircd.org/wiki/index.php/Credits
+ *
+ * This program is free but copyrighted software; see
+ *            the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
+#include "inspircd.h"
+
+/* $ModDesc: Implements extban +b r: - realname (gecos) bans */
+
+class ModuleNickBan : public Module
+{
+ private:
+ public:
+	ModuleNickBan(InspIRCd* Me) : Module(Me)
+	{
+		Implementation eventlist[] = { I_OnUserPreJoin, I_On005Numeric };
+		ServerInstance->Modules->Attach(eventlist, this, 2);
+	}
+
+	virtual ~ModuleNickBan()
+	{
+	}
+
+	virtual Version GetVersion()
+	{
+		return Version(1,2,0,0,VF_VENDOR,API_VERSION);
+	}
+
+	virtual int OnUserPreJoin(User *user, Channel *c, const char *cname, std::string &privs, const std::string &key)
+	{
+		if (!IS_LOCAL(user))
+			return 0;
+
+		if (!c)
+			return 0;
+
+		if (c->IsExtBanned(user->fullname, 'r'))
+		{
+			// XXX: send a numeric here
+			user->WriteServ("NOTICE "+std::string(user->nick)+" :*** Cannot join " + c->name + ", as you match a ban");
+			return 1;
+		}
+
+		return 0;
+	}
+
+	virtual void On005Numeric(std::string &output)
+	{
+		if (output.find(" EXTBAN=:") == std::string::npos)
+			output.append(" EXTBAN=:r");
+		else
+			output.insert(output.find(" EXTBAN=:") + 9, "r");
+	}
+};
+
+
+MODULE_INIT(ModuleNickBan)
+
