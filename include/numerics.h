7009fbf66e908c97e6204333c4ba8bd6bcc9a7a1
brain <brain@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-07-13 20:40:09 +0000

    Fix indentation to look right Fix 008 and 042 to be correct (not invalid octal) Change documentation of 501
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@10012 e03df62e-2008-0410-955e-edbf42e46eb7

--- include/numerics.h
+++ include/numerics.h
@@ -26,33 +26,33 @@ enum Numerics
 	/*
 	 * Reply range of numerics.
 	 */
-	RPL_WELCOME						=	001, // not RFC, extremely common though
+	RPL_WELCOME					=	001, // not RFC, extremely common though
 	RPL_YOURHOSTIS					=	002, // not RFC, extremely common though
 	RPL_SERVERCREATED				=	003, // not RFC, extremely common though
 	RPL_SERVERVERSION				=	004, // not RFC, extremely common though
 	RPL_ISUPPORT					=	005, // not RFC, extremely common though
 
-	RPL_SNOMASKIS					=	008, // unrealircd
+	RPL_SNOMASKIS					=	8, // unrealircd - cant put 008 as it will be interpreted as octal
 
-	RPL_YOURUUID					=	042, // taken from ircnet
+	RPL_YOURUUID					=	42, // taken from ircnet - see above
 
-	RPL_UMODEIS						=	221,
-	RPL_RULES						=	232, // unrealircd
+	RPL_UMODEIS					=	221,
+	RPL_RULES					=	232, // unrealircd
 
 	RPL_RULESTART					=	308, // unrealircd
 	RPL_RULESEND					=	309, // unrealircd
 	RPL_CHANNELMODEIS				=	324,
 	RPL_CHANNELCREATED				=	329, // ???
-	RPL_TOPIC						=	332,
+	RPL_TOPIC					=	332,
 	RPL_TOPICTIME					=	333, // not RFC, extremely common though
 	RPL_NAMREPLY					=	353,
 	RPL_ENDOFNAMES					=	366,
 
-	RPL_MOTD						=	372,
+	RPL_MOTD					=	372,
 	RPL_MOTDSTART					=	375,
 	RPL_ENDOFMOTD					=	376,
 
-	RPL_YOURDISPLAYEDHOST			=	396, // from charybdis/etc, common convention
+	RPL_YOURDISPLAYEDHOST				=	396, // from charybdis/etc, common convention
 
 	/*
 	 * Error range of numerics.
@@ -60,9 +60,9 @@ enum Numerics
 	ERR_NOSUCHNICK					=	401,
 	ERR_TOOMANYCHANNELS				=	405,
 	ERR_UNKNOWNCOMMAND				=	421,
-	ERR_NOMOTD						=	422,
-	ERR_NORULES						=	434, // unrealircd
-	ERR_USERNOTINCHANNEL			=	441,
+	ERR_NOMOTD					=	422,
+	ERR_NORULES					=	434, // unrealircd
+	ERR_USERNOTINCHANNEL				=	441,
 	ERR_NOTREGISTERED				=	451,
 	ERR_NEEDMOREPARAMS				=	461,
 
@@ -88,12 +88,12 @@ enum Numerics
 	ERR_BANNEDFROMCHAN				=	474,
 
 	ERR_NOPRIVILEGES				=	481, // rfc, beware though, we use this for other things opers may not do also
-	ERR_CHANOPRIVSNEEDED			=	482, // rfc, beware though, we use this for other things like trying to kick a uline
+	ERR_CHANOPRIVSNEEDED				=	482, // rfc, beware though, we use this for other things like trying to kick a uline
 
-	ERR_UNKNOWNSNOMASK				=	501, // not rfc. unrealircd?
+	ERR_UNKNOWNSNOMASK				=	501, // insp-specific
 	ERR_USERSDONTMATCH				=	502,
 
-	ERR_CANTUNLOADMODULE			=	972, // insp-specific
+	ERR_CANTUNLOADMODULE				=	972, // insp-specific
 	RPL_UNLOADEDMODULE				=	973, // insp-specific
 	ERR_CANTLOADMODULE				=	974, // insp-specific
 	RPL_LOADEDMODULE				=	975 // insp-specific
