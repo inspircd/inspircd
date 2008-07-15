403d16068410909b8d352588dc83f6d255a018b7
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-07-15 13:32:28 +0000

    Send ERR_NOTREGISTERED to clients that send SERVER on a client port with a text that tells them what they are doing wrong. Should help them debug it themselves.
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@10023 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/commands/cmd_server.cpp
+++ src/commands/cmd_server.cpp
@@ -23,6 +23,13 @@ extern "C" DllExport Command* init_command(InspIRCd* Instance)
 
 CmdResult CommandServer::Handle (const std::vector<std::string>&, User *user)
 {
-	user->WriteNumeric(ERR_ALREADYREGISTERED, "%s :You are already registered. (Perhaps your IRC client does not have a /SERVER command).",user->nick.c_str());
+	if (user->registered == REG_ALL)
+	{
+		user->WriteNumeric(ERR_ALREADYREGISTERED, "%s :You are already registered. (Perhaps your IRC client does not have a /SERVER command).",user->nick.c_str());
+	}
+	else
+	{
+		user->WriteNumeric(ERR_NOTREGISTERED, "%s :You may not register as a server (servers have seperate ports from clients, change your config)",command.c_str());
+	}
 	return CMD_FAILURE;
 }
