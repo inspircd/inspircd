62a971acc29bcee901ac343b257d31ecb87ecef7
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-07-22 13:44:34 +0000

    Fix bug #575, taxonomy finally works properly
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@10065 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_taxonomy.cpp
+++ src/modules/m_taxonomy.cpp
@@ -73,7 +73,7 @@ class ModuleTaxonomy : public Module
 	{
 		if (target_type == TYPE_USER)
 		{
-			User* spoolto = (User*)target;
+			User* spoolto = (User*)opaque;
 			std::string taxstr = "304 " + std::string(spoolto->nick) + ":TAXONOMY METADATA "+extname+" = "+extdata;
 			spoolto->WriteServ(taxstr);
 			claimed = true;
