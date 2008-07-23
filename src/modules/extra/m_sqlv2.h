c440833393dde325fa45e295753cbe86683baea6
peavey <peavey@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-07-23 08:37:45 +0000

    fix a little typo I found while checking some other stuff
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@10068 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/extra/m_sqlv2.h
+++ src/modules/extra/m_sqlv2.h
@@ -459,13 +459,13 @@ class SQLhost
  */
 bool operator== (const SQLhost& l, const SQLhost& r)
 {
-	return (l.id == r.id && l.host == r.host && l.port == r.port && l.name == r.name && l.user == l.user && l.pass == r.pass && l.ssl == r.ssl);
+	return (l.id == r.id && l.host == r.host && l.port == r.port && l.name == r.name && l.user == r.user && l.pass == r.pass && l.ssl == r.ssl);
 }
 /** Overload operator!= for two SQLhost objects for easy comparison.
  */
 bool operator!= (const SQLhost& l, const SQLhost& r)
 {
-	return (l.id != r.id || l.host != r.host || l.port != r.port || l.name != r.name || l.user != l.user || l.pass != r.pass || l.ssl != r.ssl);
+	return (l.id != r.id || l.host != r.host || l.port != r.port || l.name != r.name || l.user != r.user || l.pass != r.pass || l.ssl != r.ssl);
 }
 
 
