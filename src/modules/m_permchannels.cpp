8eae84e3ba8ec6d025592b9406193f73929bbf77
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-08-25 13:22:57 +0000

    Add and document <permchannels> block for m_permchannels, which creates a channel on startup. Fixes bug #511.
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@10271 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_permchannels.cpp
+++ src/modules/m_permchannels.cpp
@@ -64,12 +64,68 @@ public:
 		}
 		Implementation eventlist[] = { I_OnChannelPreDelete };
 		ServerInstance->Modules->Attach(eventlist, this, 1);
+
+		OnRehash(NULL, "");
 	}
 
 	virtual ~ModulePermanentChannels()
 	{
 		ServerInstance->Modes->DelMode(p);
 		delete p;
+	}
+
+	virtual void OnRehash(User *user, const std::string &parameter)
+	{
+		/*
+		 * Process config-defined list of permanent channels.
+		 * -- w00t
+		 */
+		ConfigReader MyConf(ServerInstance);
+		for (int i = 0; i < MyConf.Enumerate("permchannels"); i++)
+		{
+			std::string channel = MyConf.ReadValue("permchannels", "channel", i);
+			std::string topic = MyConf.ReadValue("permchannels", "topic", i);
+			std::string modes = MyConf.ReadValue("permchannels", "modes", i);
+
+			if (channel.empty() || topic.empty())
+			{
+				ServerInstance->Logs->Log("blah", DEBUG, "Malformed permchannels tag with empty topic or channel name.");
+				continue;
+			}
+
+			Channel *c = ServerInstance->FindChan(channel);
+
+			if (!c)
+			{
+				c = new Channel(ServerInstance, channel, ServerInstance->Time());
+				c->SetTopic(NULL, topic, true);
+				ServerInstance->Logs->Log("blah", DEBUG, "Added %s with topic %s", channel.c_str(), topic.c_str());
+
+				if (modes.empty())
+					continue;
+
+				irc::spacesepstream list(modes);
+				std::string modeseq;
+				std::string par;
+
+				list.GetToken(modeseq);
+
+				// XXX bleh, should we pass this to the mode parser instead? ugly. --w00t
+				for (std::string::iterator n = modeseq.begin(); n != modeseq.end(); ++n)
+				{
+					ModeHandler* mode = ServerInstance->Modes->FindMode(*n, MODETYPE_CHANNEL);
+					if (mode)
+					{
+						if (mode->GetNumParams(true))
+							list.GetToken(par);
+						else
+							par.clear();
+
+						mode->OnModeChange(ServerInstance->FakeClient, ServerInstance->FakeClient, c, par, true);
+					}
+				}
+			}
+		}
 	}
 
 	virtual Version GetVersion()
