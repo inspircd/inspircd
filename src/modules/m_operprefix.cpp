8547cb30c17e44dc3238a0a7a5912c589d4e8190
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-11-24 14:12:24 +0000

    Add m_operprefix, more simplified version of module written by Phoenix (thanks!). Adds cmode +y, set on-join which grants opers an optional prefix status on the channel.
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@10822 e03df62e-2008-0410-955e-edbf42e46eb7

---
+++ src/modules/m_operprefix.cpp
@@ -0,0 +1,223 @@
+/*       +------------------------------------+
+ *       | Inspire Internet Relay Chat Daemon |
+ *       +------------------------------------+
+ *
+ *  InspIRCd: (C) 2002-2008 InspIRCd Development Team
+ * See: http://www.inspircd.org/wiki/index.php/Credits
+ *
+ * This program is free but copyrighted software; see
+ *            the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
+/* 
+   by Chernov-Phoenix Alexey (Phoenix@RusNet) mailto:phoenix /email address separator/ pravmail.ru */
+
+#include "inspircd.h"
+
+static char prefixchar;
+
+std::set<std::string>* SetupExt(User* user)
+{
+	std::set<std::string>* ext;
+	if (!user->GetExt("m_operprefix",ext))
+	{
+		ext=new std::set<std::string>;
+		ext->clear();
+		user->Extend("m_operprefix",ext);
+	}
+	return ext;
+}
+
+
+void DelPrefixChan(User* user, Channel* channel)
+{
+	std::set<std::string>* chans = SetupExt(user);
+	chans->erase(channel->name);
+}
+
+
+void AddPrefixChan(User* user, Channel* channel)
+{
+	std::set<std::string>* chans = SetupExt(user);
+	chans->insert(channel->name);
+}
+
+
+class OperPrefixMode : public ModeHandler
+{
+	public:
+		OperPrefixMode(InspIRCd* Instance) : ModeHandler(Instance, 'y', 1, 1, true, MODETYPE_CHANNEL, false, prefixchar) { }
+
+		ModeAction OnModeChange(User* source, User* dest, Channel* channel, std::string &parameter, bool adding, bool servermode)
+		{
+			if (servermode || (source && ServerInstance->ULine(source->server)))
+				return MODEACTION_ALLOW;
+			else
+			{
+				if (source && channel)
+					source->WriteNumeric(ERR_CHANOPRIVSNEEDED, "%s %s :Only servers are permitted to change channel mode '%c'", source->nick.c_str(), channel->name.c_str(), 'y');
+				return MODEACTION_DENY;
+			}
+		}
+
+		ModePair ModeSet(User* source, User* dest, Channel* channel, const std::string &parameter)
+		{
+			User* x = ServerInstance->FindNick(parameter);
+			if (x)
+			{
+				if (!channel->HasUser(x))
+				{
+					return std::make_pair(false, parameter);
+				}
+				else
+				{
+					std::set<std::string>* ext;
+					if (x->GetExt("m_operprefix",ext))
+					{
+						if (ext->find(channel->name)!=ext->end())
+						{
+							return std::make_pair(true, x->nick);
+						}
+						else
+							return std::make_pair(false, parameter);
+					}
+					else
+					{
+						return std::make_pair(false, parameter);
+					}
+				}
+			}
+			return std::make_pair(false, parameter);
+		}
+
+		bool NeedsOper() { return true; }
+};
+
+class ModuleOperPrefixMode : public Module
+{
+ private:
+	OperPrefixMode* opm;
+ public:
+	ModuleOperPrefixMode(InspIRCd* Me) : Module(Me)
+	{
+		ConfigReader Conf(ServerInstance);
+		std::string tmp;
+		tmp = Conf.ReadValue("operprefix", "prefix", "!", 0, false);
+		strlcpy(&prefixchar,tmp.c_str(),2);
+
+		opm = new OperPrefixMode(ServerInstance);
+		if ((!ServerInstance->Modes->AddMode(opm)))
+			throw ModuleException("Could not add a new mode!");
+			
+		Implementation eventlist[] = { I_OnPostJoin, I_OnCleanup, I_OnUserQuit, I_OnUserKick, I_OnUserPart };
+		ServerInstance->Modules->Attach(eventlist, this, 6);
+	}
+
+	virtual void PushChanMode(Channel* channel, User* user, bool negate = false)
+	{
+		if (negate)
+			DelPrefixChan(user, channel);
+		else
+			AddPrefixChan(user, channel);
+		char modeline[]="+y";
+		if (negate)
+			modeline [0]='-';
+		std::vector<std::string> modechange;
+		modechange.push_back(channel->name);
+		modechange.push_back(modeline);
+		modechange.push_back(user->nick);
+		ServerInstance->SendMode(modechange,this->ServerInstance->FakeClient);
+	}
+
+	virtual void OnPostJoin(User *user, Channel *channel)
+	{
+		// This may look wrong, but I don't think it is.. PushChanMode will send FMODE which should sort it all out.
+		if (!IS_LOCAL(user))
+			return;
+
+		if (user && IS_OPER(user))
+		{
+			if (user->IsModeSet('H'))
+			{
+				/* we respect your wish to be invisible */
+				return;
+			}
+			PushChanMode(channel, user);
+		}
+	}
+
+	// XXX: is there a better way to do this?
+	virtual int OnRawMode(User* user, Channel* chan, const char mode, const std::string &param, bool adding, int pcnt, bool servermode)
+	{
+		/* force event propagation to its ModeHandler */
+		if (!servermode && chan && (mode == 'y'))
+			return ACR_ALLOW;
+		return 0;
+	}
+
+	virtual ~ModuleOperPrefixMode()
+	{
+		ServerInstance->Modes->DelMode(opm);
+		delete opm;
+	}
+
+	virtual void CleanUser(User* user, bool quitting=false)
+	{
+		if (!IS_LOCAL(user))
+			return;
+
+		std::set<std::string>* ext;
+		if (user->GetExt("m_operprefix",ext))
+		{
+			// Don't want to announce -mode when they're quitting anyway..
+			if (!quitting)
+			{
+				for (UCListIter v = user->chans.begin(); v != user->chans.end(); v++)
+				{
+					ModePair ms=opm->ModeSet(NULL, NULL , v->first, user->nick);
+					if (ms.first)
+					{
+						PushChanMode(v->first, user, true);
+					}
+				}
+			}
+			ext->clear();
+			delete ext;
+			user->Shrink("m_operprefix");
+		}
+	}
+
+	virtual void OnCleanup(int target_type, void* item)
+	{
+		if (target_type == TYPE_USER)
+		{
+			User* user = (User*)item;
+			CleanUser(user);
+		}
+	}
+
+	virtual void OnUserQuit(User* user, const std::string &reason, const std::string &oper_message)
+	{
+		CleanUser(user,true);
+	}
+
+	virtual void OnUserKick(User* source, User* user, Channel* chan, const std::string &reason, bool &silent)
+	{
+		DelPrefixChan(user, chan);
+	}
+
+	virtual void OnUserPart(User* user, Channel* channel, std::string &partreason, bool &silent)
+	{
+		DelPrefixChan(user, channel);
+	}
+
+	virtual Version GetVersion()
+	{
+		return Version("$Id$", VF_COMMON, API_VERSION);
+	}
+};
+
+MODULE_INIT(ModuleOperPrefixMode)
+
