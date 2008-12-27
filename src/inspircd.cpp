bc05569e2de011206bf57ca715c6d342156aee46
w00t <w00t@e03df62e-2008-0410-955e-edbf42e46eb7>
2008-12-27 02:45:29 +0000

    Remove silly comment from r10,000.. mostly just testing vim..
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@10915 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/inspircd.cpp
+++ src/inspircd.cpp
@@ -11,8 +11,6 @@
  * ---------------------------------------------------
  */
 
-/* w00t was here. ;p */
-
 /* $Install: src/inspircd $(BINPATH) */
 #include "inspircd.h"
 #include <signal.h>
@@ -525,7 +523,7 @@ InspIRCd::InspIRCd(int argc, char** argv)
 		/* Windows can (and defaults to) hide file extensions, so let's play a bit nice for windows users. */
 		std::string txtconf = this->ConfigFileName;
 		txtconf.append(".txt");
-
+    i
 		if (ServerConfig::FileExists(txtconf.c_str()))
 		{
 			strlcat(this->ConfigFileName, ".txt", MAXBUF);
