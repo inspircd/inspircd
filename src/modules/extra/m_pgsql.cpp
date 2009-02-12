b5e82511c4810d8267799bae24a2662b804b689e
peavey <peavey@e03df62e-2008-0410-955e-edbf42e46eb7>
2009-02-12 18:05:05 +0000

    Nuke an unused var
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@11091 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/extra/m_pgsql.cpp
+++ src/modules/extra/m_pgsql.cpp
@@ -587,9 +587,6 @@ class SQLConn : public EventHandler
 				/* Total length of the unescaped parameters */
 				unsigned long maxparamlen, paramcount;
 
-				/* Total length of query, used for binary-safety */
-				unsigned long querylength = 0;
-
 				/* The length of the longest parameter */
 				maxparamlen = 0;
 
