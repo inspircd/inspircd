00fa6d592ed2640fcdf74444786de555c1c3da25
danieldg <danieldg@e03df62e-2008-0410-955e-edbf42e46eb7>
2009-02-27 02:44:36 +0000

    [Taros] Add m_sakick.so
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@11156 e03df62e-2008-0410-955e-edbf42e46eb7

---
+++ src/modules/m_sakick.cpp
@@ -0,0 +1,124 @@
+/*       +------------------------------------+
+ *       | Inspire Internet Relay Chat Daemon |
+ *       +------------------------------------+
+ *
+ *  InspIRCd: (C) 2002-2009 InspIRCd Development Team
+ * See: http://www.inspircd.org/wiki/index.php/Credits
+ *
+ * This program is free but copyrighted software; see
+ *            the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
+#include "inspircd.h"
+
+/* $ModDesc: Provides a SAKICK command */
+
+/** Handle /SAKICK
+ */
+class CommandSakick : public Command
+{
+ public:
+	CommandSakick (InspIRCd* Instance) : Command(Instance,"SAKICK", "o", 2, 3, false, 0)
+	{
+		this->source = "m_sakick.so";
+		syntax = "<channel> <nick> [reason]";
+		TRANSLATE4(TR_NICK, TR_TEXT, TR_TEXT, TR_END);
+	}
+
+	CmdResult Handle (const std::vector<std::string>& parameters, User *user)
+	{
+		User* dest = ServerInstance->FindNick(parameters[1]);
+		Channel* channel = ServerInstance->FindChan(parameters[0]);
+		const char* reason = "";
+		const char* servername = NULL;
+
+		if (dest && channel)
+		{
+			if (parameters.size() > 2)
+			{
+				reason = parameters[2].c_str();
+			}
+			else
+			{
+				reason = dest->nick.c_str();
+			}
+
+			if (ServerInstance->ULine(dest->server))
+			{
+				user->WriteNumeric(ERR_NOPRIVILEGES, "%s :Cannot use an SA command on a u-lined client",user->nick.c_str());
+				return CMD_FAILURE;
+			}
+
+			/* For local clients, directly kick them. For remote clients,
+			 * just return CMD_SUCCESS knowing the protocol module will route the SAKICK to the user's
+			 * local server and that will kick them instead.
+			 */
+			if (IS_LOCAL(dest))
+			{
+				if (!channel->ServerKickUser(dest, reason, true, servername))
+					delete channel;
+
+				Channel* n = ServerInstance->FindChan(parameters[1]);
+				if (!n)
+				{
+					ServerInstance->SNO->WriteToSnoMask('A', std::string(user->nick)+" SAKICKed "+dest->nick+" on "+parameters[0]);
+					return CMD_SUCCESS;
+				}
+				else
+				{
+					if (!n->HasUser(dest))
+					{
+						ServerInstance->SNO->WriteToSnoMask('A', std::string(user->nick)+" SAKICKed "+dest->nick+" on "+parameters[0]);
+						return CMD_SUCCESS;
+					}
+					else
+					{
+						user->WriteServ("NOTICE %s :*** Unable to kick %s from %s",user->nick.c_str(), dest->nick.c_str(), parameters[0].c_str());
+						return CMD_FAILURE;
+					}
+				}
+			}
+			else
+			{
+				ServerInstance->SNO->WriteToSnoMask('A', std::string(user->nick)+" sent remote SAKICK to kick "+dest->nick+" from "+parameters[0]);
+			}
+
+			return CMD_SUCCESS;
+		}
+		else
+		{
+			user->WriteServ("NOTICE %s :*** Invalid nickname or channel", user->nick.c_str());
+		}
+
+		return CMD_FAILURE;
+	}
+};
+
+class ModuleSakick : public Module
+{
+	CommandSakick*	mycommand;
+ public:
+	ModuleSakick(InspIRCd* Me)
+		: Module(Me)
+	{
+
+		mycommand = new CommandSakick(ServerInstance);
+		ServerInstance->AddCommand(mycommand);
+
+	}
+
+	virtual ~ModuleSakick()
+	{
+	}
+
+	virtual Version GetVersion()
+	{
+		return Version("$Id$", VF_VENDOR, API_VERSION);
+	}
+
+};
+
+MODULE_INIT(ModuleSakick)
+
