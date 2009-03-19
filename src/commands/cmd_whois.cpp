ea4f54a4f09d8443bc8688cb34e0d62aa59e9432
danieldg <danieldg@e03df62e-2008-0410-955e-edbf42e46eb7>
2009-03-19 16:58:00 +0000

    Show users their own server name in "/whois myself"
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@11236 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/commands/cmd_whois.cpp
+++ src/commands/cmd_whois.cpp
@@ -17,86 +17,78 @@
 
 void do_whois(InspIRCd* ServerInstance, User* user, User* dest,unsigned long signon, unsigned long idle, const char* nick)
 {
-	if (dest->Visibility && !dest->Visibility->VisibleTo(user))
+	if ((dest->Visibility && !dest->Visibility->VisibleTo(user)) || dest->registered != REG_ALL)
 	{
 		ServerInstance->SendWhoisLine(user, dest, 401, "%s %s :No such nick/channel",user->nick.c_str(), *nick ? nick : "*");
 		ServerInstance->SendWhoisLine(user, dest, 318, "%s %s :End of /WHOIS list.",user->nick.c_str(), *nick ? nick : "*");
 		return;
 	}
 
-	if (dest->registered == REG_ALL)
+	ServerInstance->SendWhoisLine(user, dest, 311, "%s %s %s %s * :%s",user->nick.c_str(), dest->nick.c_str(), dest->ident.c_str(), dest->dhost.c_str(), dest->fullname.c_str());
+	if (user == dest || user->HasPrivPermission("users/auspex"))
 	{
-		ServerInstance->SendWhoisLine(user, dest, 311, "%s %s %s %s * :%s",user->nick.c_str(), dest->nick.c_str(), dest->ident.c_str(), dest->dhost.c_str(), dest->fullname.c_str());
-		if (user == dest || user->HasPrivPermission("users/auspex"))
-		{
-			ServerInstance->SendWhoisLine(user, dest, 378, "%s %s :is connecting from %s@%s %s", user->nick.c_str(), dest->nick.c_str(), dest->ident.c_str(), dest->host.c_str(), dest->GetIPString());
-		}
+		ServerInstance->SendWhoisLine(user, dest, 378, "%s %s :is connecting from %s@%s %s", user->nick.c_str(), dest->nick.c_str(), dest->ident.c_str(), dest->host.c_str(), dest->GetIPString());
+	}
 
-		std::string cl = dest->ChannelList(user);
+	std::string cl = dest->ChannelList(user);
 
-		if (cl.length())
+	if (cl.length())
+	{
+		if (cl.length() > 400)
 		{
-			if (cl.length() > 400)
-			{
-				user->SplitChanList(dest,cl);
-			}
-			else
-			{
-				ServerInstance->SendWhoisLine(user, dest, 319, "%s %s :%s",user->nick.c_str(), dest->nick.c_str(), cl.c_str());
-			}
-		}
-		if (*ServerInstance->Config->HideWhoisServer && !user->HasPrivPermission("servers/auspex"))
-		{
-			ServerInstance->SendWhoisLine(user, dest, 312, "%s %s %s :%s",user->nick.c_str(), dest->nick.c_str(), ServerInstance->Config->HideWhoisServer, ServerInstance->Config->Network);
+			user->SplitChanList(dest,cl);
 		}
 		else
 		{
-			ServerInstance->SendWhoisLine(user, dest, 312, "%s %s %s :%s",user->nick.c_str(), dest->nick.c_str(), dest->server, ServerInstance->GetServerDescription(dest->server).c_str());
+			ServerInstance->SendWhoisLine(user, dest, 319, "%s %s :%s",user->nick.c_str(), dest->nick.c_str(), cl.c_str());
 		}
-
-		if (IS_AWAY(dest))
-		{
-			ServerInstance->SendWhoisLine(user, dest, 301, "%s %s :%s",user->nick.c_str(), dest->nick.c_str(), dest->awaymsg.c_str());
-		}
-
-		if (IS_OPER(dest))
-		{
-			if (ServerInstance->Config->GenericOper)
-				ServerInstance->SendWhoisLine(user, dest, 313, "%s %s :is an IRC operator",user->nick.c_str(), dest->nick.c_str());
-			else
-				ServerInstance->SendWhoisLine(user, dest, 313, "%s %s :is %s %s on %s",user->nick.c_str(), dest->nick.c_str(), (strchr("AEIOUaeiou",dest->oper[0]) ? "an" : "a"),irc::Spacify(dest->oper.c_str()), ServerInstance->Config->Network);
-		}
-
-		if (user == dest || user->HasPrivPermission("users/auspex"))
-		{
-			if (dest->IsModeSet('s') != 0)
-			{
-				ServerInstance->SendWhoisLine(user, dest, 379, "%s %s :is using modes +%s +%s", user->nick.c_str(), dest->nick.c_str(), dest->FormatModes(), dest->FormatNoticeMasks());
-			}
-			else
-			{
-				ServerInstance->SendWhoisLine(user, dest, 379, "%s %s :is using modes +%s", user->nick.c_str(), dest->nick.c_str(), dest->FormatModes());
-			}
-		}
-
-		FOREACH_MOD(I_OnWhois,OnWhois(user,dest));
-
-		/*
-		 * We only send these if we've been provided them. That is, if hidewhois is turned off, and user is local, or
-		 * if remote whois is queried, too. This is to keep the user hidden, and also since you can't reliably tell remote time. -- w00t
-		 */
-		if ((idle) || (signon))
-		{
-			ServerInstance->SendWhoisLine(user, dest, 317, "%s %s %lu %lu :seconds idle, signon time",user->nick.c_str(), dest->nick.c_str(), idle, signon);
-		}
-
-		ServerInstance->SendWhoisLine(user, dest, 318, "%s %s :End of /WHOIS list.",user->nick.c_str(), dest->nick.c_str());
+	}
+	if (user != dest && *ServerInstance->Config->HideWhoisServer && !user->HasPrivPermission("servers/auspex"))
+	{
+		ServerInstance->SendWhoisLine(user, dest, 312, "%s %s %s :%s",user->nick.c_str(), dest->nick.c_str(), ServerInstance->Config->HideWhoisServer, ServerInstance->Config->Network);
 	}
 	else
 	{
-		ServerInstance->SendWhoisLine(user, dest, 401, "%s %s :No such nick/channel",user->nick.c_str(), *nick ? nick : "*");
-		ServerInstance->SendWhoisLine(user, dest, 318, "%s %s :End of /WHOIS list.",user->nick.c_str(), *nick ? nick : "*");
+		ServerInstance->SendWhoisLine(user, dest, 312, "%s %s %s :%s",user->nick.c_str(), dest->nick.c_str(), dest->server, ServerInstance->GetServerDescription(dest->server).c_str());
 	}
+
+	if (IS_AWAY(dest))
+	{
+		ServerInstance->SendWhoisLine(user, dest, 301, "%s %s :%s",user->nick.c_str(), dest->nick.c_str(), dest->awaymsg.c_str());
+	}
+
+	if (IS_OPER(dest))
+	{
+		if (ServerInstance->Config->GenericOper)
+			ServerInstance->SendWhoisLine(user, dest, 313, "%s %s :is an IRC operator",user->nick.c_str(), dest->nick.c_str());
+		else
+			ServerInstance->SendWhoisLine(user, dest, 313, "%s %s :is %s %s on %s",user->nick.c_str(), dest->nick.c_str(), (strchr("AEIOUaeiou",dest->oper[0]) ? "an" : "a"),irc::Spacify(dest->oper.c_str()), ServerInstance->Config->Network);
+	}
+
+	if (user == dest || user->HasPrivPermission("users/auspex"))
+	{
+		if (dest->IsModeSet('s') != 0)
+		{
+			ServerInstance->SendWhoisLine(user, dest, 379, "%s %s :is using modes +%s +%s", user->nick.c_str(), dest->nick.c_str(), dest->FormatModes(), dest->FormatNoticeMasks());
+		}
+		else
+		{
+			ServerInstance->SendWhoisLine(user, dest, 379, "%s %s :is using modes +%s", user->nick.c_str(), dest->nick.c_str(), dest->FormatModes());
+		}
+	}
+
+	FOREACH_MOD(I_OnWhois,OnWhois(user,dest));
+
+	/*
+	 * We only send these if we've been provided them. That is, if hidewhois is turned off, and user is local, or
+	 * if remote whois is queried, too. This is to keep the user hidden, and also since you can't reliably tell remote time. -- w00t
+	 */
+	if ((idle) || (signon))
+	{
+		ServerInstance->SendWhoisLine(user, dest, 317, "%s %s %lu %lu :seconds idle, signon time",user->nick.c_str(), dest->nick.c_str(), idle, signon);
+	}
+
+	ServerInstance->SendWhoisLine(user, dest, 318, "%s %s :End of /WHOIS list.",user->nick.c_str(), dest->nick.c_str());
 }
 
 
