inspircd/inspircd

commit 2bb40c5b237e91ce8d3095aa81879b10cdca5bc8

more fixes. inet_pton is now in error, this is because the windows 6.0 sdk defines it, we need some kind of ifdef or something around this to not define our own if we have the 6.0 SDK. Again i will check this if i have chance, otherwise... :p
git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@11407 e03df62e-2008-0410-955e-edbf42e46eb7

win/inspircd_win32wrapper.h
@@ -37,6 +37,9 @@
 
 #define FD_SETSIZE 24000
 
+#define uint32_t DWORD
+#define uint16_t WORD
+
 /* Make builds smaller, leaner and faster */
 #define VC_EXTRALEAN
 #define WIN32_LEAN_AND_MEAN
@@ -77,8 +80,6 @@
 
 /* Say we're building on windows 2000. Anyone running something older than this
  * reeeeeeeally needs to upgrade! */
-
-#define _WIN32_WINNT 0x500
 
 /* Normal windows (platform-specific) includes */
 #include <winsock2.h>

commit 2f1466ee943f870c157bb065edd92037c79e4b22

Since Windows SDK6.0a inet_pton and inet_ntop is defined if (NTDDI_VERSION >= NTDDI_LONGHORN). I found a similar fix in pythons socketcode and thus think this is fine.
Fixes the breakage Brain mentions in r11407.

git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@11459 e03df62e-2008-0410-955e-edbf42e46eb7

win/inspircd_win32wrapper.cpp
@@ -46,6 +46,7 @@ int inet_aton(const char *cp, struct in_addr *addr)
 	return (addr->s_addr == INADDR_NONE) ? 0 : 1;
 }
 
+#if !defined(NTDDI_VERSION) || (NTDDI_VERSION < NTDDI_LONGHORN)
 const char *inet_ntop(int af, const void *src, char *dst, socklen_t cnt)
 {
 
@@ -69,12 +70,14 @@ const char *inet_ntop(int af, const void *src, char *dst, socklen_t cnt)
 	}
 	return NULL;
 }
+#endif
 
 int geteuid()
 {
 	return 1;
 }
 
+#if !defined(NTDDI_VERSION) || (NTDDI_VERSION < NTDDI_LONGHORN)
 int inet_pton(int af, const char *src, void *dst)
 {
 	sockaddr_in sa;
@@ -90,6 +93,7 @@ int inet_pton(int af, const char *src, void *dst)
 	memcpy(dst, &sa.sin_addr, sizeof(struct in_addr));
 	return rv;
 }
+#endif
 
 void setcolor(int color_code)
 {

win/inspircd_win32wrapper.h
@@ -110,11 +110,13 @@
 #define EINPROGRESS WSAEWOULDBLOCK
 #define EADDRNOTAVAIL WSAEADDRNOTAVAIL
 
+#if !defined(NTDDI_VERSION) || (NTDDI_VERSION < NTDDI_LONGHORN)
 /* Convert formatted (xxx.xxx.xxx.xxx) string to in_addr struct */
 CoreExport int inet_pton(int af, const char * src, void * dst);
 
 /* Convert struct to formatted (xxx.xxx.xxx.xxx) string */
 CoreExport const char * inet_ntop(int af, const void * src, char * dst, socklen_t cnt);
+#endif
 
 /* Safe printf functions aren't defined in VC2003 */
 #define snprintf _snprintf