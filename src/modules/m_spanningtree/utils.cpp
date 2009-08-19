inspircd/inspircd

commit fcb51634669e6ed588e42f2072c6f910e267126f

Fix UID collision on reload of m_spanningtree.so
git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@11386 e03df62e-2008-0410-955e-edbf42e46eb7

src/modules/m_spanningtree/utils.cpp
@@ -177,6 +177,7 @@ SpanningTreeUtilities::~SpanningTreeUtilities()
 		}
 	}
 	delete TreeRoot;
+	ServerUser->uuid = TreeRoot->GetID();
 	delete ServerUser;
 	ServerInstance->BufferedSocketCull();
 }

commit 01dbd2658f357b4978f6ac7494704a24548228db

Fix a regression from fcb51634669e6ed588e42f2072c6f910e267126f ("fix uid collision on spanningtree reload") caused by access-after-free. Thanks to transnet for the report.
git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@11533 e03df62e-2008-0410-955e-edbf42e46eb7

src/modules/m_spanningtree/utils.cpp
@@ -176,8 +176,10 @@ SpanningTreeUtilities::~SpanningTreeUtilities()
 			sock->Close();
 		}
 	}
-	delete TreeRoot;
+	
+	// This avoids a collision on reload
 	ServerUser->uuid = TreeRoot->GetID();
+	delete TreeRoot;
 	delete ServerUser;
 	ServerInstance->BufferedSocketCull();
 }