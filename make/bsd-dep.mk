e9c90126360fbe6115eacb38ca807e10aeb9ff58
danieldg <danieldg@e03df62e-2008-0410-955e-edbf42e46eb7>
2009-09-01 15:06:25 +0000

    Move bsd-dep.mk/gnu-dep.mk to top-level Makefile
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@11569 e03df62e-2008-0410-955e-edbf42e46eb7

--- make/bsd-dep.mk
+++
@@ -1,5 +0,0 @@
-DFILES != perl -e 'print join " ", <*.cpp>, <commands/*.cpp>, <modes/*.cpp>, <modules/*.cpp>, <modules/m_*/*.cpp>'
-DFILES += socketengines/$(SOCKETENGINE).cpp threadengines/threadengine_pthread.cpp
-
-alldep:
-	../make/calcdep.pl $(DFILES)
--- make/gnu-dep.mk
+++
@@ -1,4 +0,0 @@
-CFILES = $(shell perl -e 'print join " ", <*.cpp>, <commands/*.cpp>, <modes/*.cpp>, <modules/*.cpp>, <modules/m_*/*.cpp>')
-CFILES += socketengines/$(SOCKETENGINE).cpp threadengines/threadengine_pthread.cpp
-alldep:
-	@../make/calcdep.pl $(CFILES)
