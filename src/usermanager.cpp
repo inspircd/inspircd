47c4da37d6b9c7f443ad871771558e01ffec791a
danieldg <danieldg@e03df62e-2008-0410-955e-edbf42e46eb7>
2009-09-17 23:13:50 +0000

    Prevent fake users from quitting
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@11743 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/usermanager.cpp
+++ src/usermanager.cpp
@@ -171,6 +171,12 @@ void UserManager::QuitUser(User *user, const std::string &quitreason, const char
 		return;
 	}
 
+	if (IS_FAKE(user))
+	{
+		ServerInstance->Logs->Log("CULLLIST",DEBUG, "*** Warning *** - You tried to quit a fake user (%s)", user->nick.c_str());
+		return;
+	}
+
 	user->quitting = true;
 
 	ServerInstance->Logs->Log("USERS", DEBUG, "QuitUser: %s '%s'", user->nick.c_str(), quitreason.c_str());
