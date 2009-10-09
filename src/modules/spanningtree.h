611eda94127cfca78591915155eff491337db446
danieldg <danieldg@e03df62e-2008-0410-955e-edbf42e46eb7>
2009-10-09 21:33:50 +0000

    Whoops, forgot to commit this file
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@11812 e03df62e-2008-0410-955e-edbf42e46eb7

---
+++ src/modules/spanningtree.h
@@ -0,0 +1,37 @@
+/*       +------------------------------------+
+ *       | Inspire Internet Relay Chat Daemon |
+ *       +------------------------------------+
+ *
+ *  InspIRCd: (C) 2002-2009 InspIRCd Development Team
+ * See: http://wiki.inspircd.org/Credits
+ *
+ * This program is free but copyrighted software; see
+ *            the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
+#ifndef __SPANNINGTREE_H__
+#define __SPANNINGTREE_H__
+
+struct AddServerEvent : public Event
+{
+	const std::string servername;
+	AddServerEvent(Module* me, const std::string& name)
+		: Event(me, "new_server"), servername(name)
+	{
+		Send();
+	}
+};
+
+struct DelServerEvent : public Event
+{
+	const std::string servername;
+	DelServerEvent(Module* me, const std::string& name)
+		: Event(me, "lost_server"), servername(name)
+	{
+		Send();
+	}
+};
+
+#endif
