3bfd0db65ff01c026e968af4de074cc1155a4061
danieldg <danieldg@e03df62e-2008-0410-955e-edbf42e46eb7>
2009-11-15 18:27:04 +0000

    Fix failed oper snomask being sent to local oper sno on remote servers
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@12136 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/commands/cmd_oper.cpp
+++ src/commands/cmd_oper.cpp
@@ -92,8 +92,7 @@ CmdResult CommandOper::HandleLocal(const std::vector<std::string>& parameters, L
 	user->CommandFloodPenalty += 10000;
 
 	snprintf(broadcast, MAXBUF, "WARNING! Failed oper attempt by %s!%s@%s using login '%s': The following fields do not match: %s", user->nick.c_str(), user->ident.c_str(), user->host.c_str(), parameters[0].c_str(), fields.c_str());
-	ServerInstance->SNO->WriteToSnoMask('o',std::string(broadcast));
-	ServerInstance->PI->SendSNONotice("o", std::string("OPER: ") + broadcast);
+	ServerInstance->SNO->WriteGlobalSno('o',std::string(broadcast));
 
 	ServerInstance->Logs->Log("OPER",DEFAULT,"OPER: Failed oper attempt by %s!%s@%s using login '%s': The following fields did not match: %s", user->nick.c_str(), user->ident.c_str(), user->host.c_str(), parameters[0].c_str(), fields.c_str());
 	return CMD_FAILURE;
