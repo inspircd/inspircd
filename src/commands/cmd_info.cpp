8bc4d958f85c42f86ca006b99e08ec9ce9aea71b
danieldg <danieldg@e03df62e-2008-0410-955e-edbf42e46eb7>
2010-01-17 22:16:35 +0000

    Route INFO to remote servers (fixes bug #924)
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@12289 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/commands/cmd_info.cpp
+++ src/commands/cmd_info.cpp
@@ -13,14 +13,6 @@
 
 #include "inspircd.h"
 
-#ifndef __CMD_INFO_H__
-#define __CMD_INFO_H__
-
-// include the common header files
-
-#include "users.h"
-#include "channels.h"
-
 /** Handle /INFO. These command handlers can be reloaded by the core,
  * and handle basic RFC1459 commands. Commands within modules work
  * the same way, however, they can be fully unloaded, where these
@@ -31,7 +23,7 @@ class CommandInfo : public Command
  public:
 	/** Constructor for info.
 	 */
-	CommandInfo ( Module* parent) : Command(parent,"INFO",0,0) { syntax = "[<servermask>]"; }
+	CommandInfo ( Module* parent) : Command(parent,"INFO") { syntax = "[<servermask>]"; }
 	/** Handle command.
 	 * @param parameters The parameters to the comamnd
 	 * @param pcnt The number of parameters passed to teh command
@@ -39,52 +31,65 @@ class CommandInfo : public Command
 	 * @return A value from CmdResult to indicate command success or failure.
 	 */
 	CmdResult Handle(const std::vector<std::string>& parameters, User *user);
+	RouteDescriptor GetRouting(User* user, const std::vector<std::string>& parameters)
+	{
+		if (parameters.size() > 0)
+			return ROUTE_UNICAST(parameters[0]);
+		return ROUTE_LOCALONLY;
+	}
 };
 
-#endif
-
+static const char* const lines[] = {
+	"                   -/\\- \2InspIRCd\2 -\\/-",
+	"                 November 2002 - Present",
+	" ",
+	"\2Core Developers\2:",
+	"    Craig Edwards,          Brain,     <brain@inspircd.org>",
+	"    Craig McLure,           Craig,     <craig@inspircd.org>",
+	"    Robin Burchell,         w00t,      <w00t@inspircd.org>",
+	"    Oliver Lupton,          Om,        <om@inspircd.org>",
+	"    John Brooks,            Special,   <special@inspircd.org>",
+	"    Dennis Friis,           peavey,    <peavey@inspircd.org>",
+	"    Thomas Stagner,         aquanight, <aquanight@inspircd.org>",
+	"    Uli Schlachter,         psychon,   <psychon@inspircd.org>",
+	"    Matt Smith,             dz,        <dz@inspircd.org>",
+	"    Daniel De Graaf,        danieldg,  <danieldg@inspircd.org>",
+	" ",
+	"\2Regular Contributors\2:",
+	"    Majic          MacGyver        Namegduf       Ankit",
+	"    Phoenix        Taros",
+	" ",
+	"\2Other Contributors\2:",
+	"   dmb             Zaba            skenmy         GreenReaper",
+	"   Dan             Jason           satmd          owine",
+	"   Adremelech      John2           jilles         HiroP",
+	"   eggy            Bricker         AnMaster       djGrrr",
+	"   nenolod         Quension        praetorian     pippijn",
+	" ",
+	"\2Former Contributors\2:",
+	"   CC              jamie           typobox43      Burlex (win32)",
+	"   Stskeeps        ThaPrince       BuildSmart     Thunderhacker",
+	"   Skip            LeaChim",
+	" ",
+	"\2Thanks To\2:",
+	"   searchirc.com   irc-junkie.org  Brik",
+	" ",
+	" Best experienced with: \2An IRC client\2",
+	NULL
+};
 
 /** Handle /INFO
  */
-CmdResult CommandInfo::Handle (const std::vector<std::string>&, User *user)
+CmdResult CommandInfo::Handle (const std::vector<std::string>& parameters, User *user)
 {
-	user->WriteNumeric(RPL_INFO, "%s :                   -/\\- \2InspIRCd\2 -\\/-", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :                 November 2002 - Present", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s : ", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :\2Core Developers\2:", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :    Craig Edwards,          Brain,     <brain@inspircd.org>", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :    Craig McLure,           Craig,     <craig@inspircd.org>", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :    Robin Burchell,         w00t,      <w00t@inspircd.org>", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :    Oliver Lupton,          Om,        <om@inspircd.org>", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :    John Brooks,            Special,   <special@inspircd.org>", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :    Dennis Friis,           peavey,    <peavey@inspircd.org>", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :    Thomas Stagner,         aquanight, <aquanight@inspircd.org>", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :    Uli Schlachter,         psychon,   <psychon@inspircd.org>", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :    Matt Smith,             dz,        <dz@inspircd.org>", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :    Daniel De Graaf,        danieldg,  <danieldg@inspircd.org>", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s : ", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :\2Regular Contributors\2:", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :    Majic          MacGyver        Namegduf       Ankit", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :    Phoenix        Taros", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s : ", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :\2Other Contributors\2:", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :   dmb             Zaba            skenmy         GreenReaper", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :   Dan             Jason           satmd          owine", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :   Adremelech      John2           jilles         HiroP", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :   eggy            Bricker         AnMaster       djGrrr", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :   nenolod         Quension        praetorian     pippijn", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s : ", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :\2Former Contributors\2:", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :   CC              jamie           typobox43      Burlex (win32)", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :   Stskeeps        ThaPrince       BuildSmart     Thunderhacker", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :   Skip            LeaChim", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s : ", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :\2Thanks To\2:", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s :   searchirc.com   irc-junkie.org  Brik", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s : ", user->nick.c_str());
-	user->WriteNumeric(RPL_INFO, "%s : Best experienced with: \2An IRC client\2", user->nick.c_str());
+	if (parameters.size() > 0 && parameters[0] != ServerInstance->Config->ServerName)
+		return CMD_SUCCESS;
+
+	int i=0;
+	while (lines[i])
+		user->SendText(":%s %03d %s :%s", ServerInstance->Config->ServerName.c_str(), RPL_INFO, user->nick.c_str(), lines[i++]);
 	FOREACH_MOD(I_OnInfo,OnInfo(user));
-	user->WriteNumeric(RPL_ENDOFINFO, "%s :End of /INFO list", user->nick.c_str());
+	user->SendText(":%s %03d %s :End of /INFO list", ServerInstance->Config->ServerName.c_str(), RPL_ENDOFINFO, user->nick.c_str());
 	return CMD_SUCCESS;
 }
 
