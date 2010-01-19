d80ce25af85d12fd97b77948439e0e2fe2caf0d5
danieldg <danieldg@e03df62e-2008-0410-955e-edbf42e46eb7>
2010-01-19 17:00:38 +0000

    Slightly more comprehensible message for connect throttle banning.
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@12307 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/modules/m_connectban.cpp
+++ src/modules/m_connectban.cpp
@@ -91,7 +91,7 @@ class ModuleConnectBan : public Module
 			if (i->second >= threshold)
 			{
 				// Create zline for set duration.
-				ZLine* zl = new ZLine(ServerInstance->Time(), banduration, ServerInstance->Config->ServerName.c_str(), "Connect flooding", mask.str().c_str());
+				ZLine* zl = new ZLine(ServerInstance->Time(), banduration, ServerInstance->Config->ServerName, "Your IP range has been attempting to connect too many times in too short a duration. Wait a while, and you will be able to connect.", u->GetCIDRMask(range));
 				if (ServerInstance->XLines->AddLine(zl,NULL))
 					ServerInstance->XLines->ApplyLines();
 				else
