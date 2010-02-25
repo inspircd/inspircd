c3ff8f2c56cc061f176837a3ad86e69280420b61
danieldg <danieldg@e03df62e-2008-0410-955e-edbf42e46eb7>
2010-02-25 16:15:46 +0000

    Fix /LUSERS miscounting
    git-svn-id: http://svn.inspircd.org/repository/trunk/inspircd@12565 e03df62e-2008-0410-955e-edbf42e46eb7

--- src/commands/cmd_lusers.cpp
+++ src/commands/cmd_lusers.cpp
@@ -55,7 +55,7 @@ CmdResult CommandLusers::Handle (const std::vector<std::string>&, User *user)
 	unsigned int n_invis = ServerInstance->Users->ModeCount('i');
 	ProtoServerList serverlist;
 	ServerInstance->PI->GetServerList(serverlist);
-	int n_serv = 1;
+	int n_serv = 0;
 	int n_local_servs = 0;
 	for(ProtoServerList::iterator i = serverlist.begin(); i != serverlist.end(); ++i)
 	{
@@ -63,6 +63,9 @@ CmdResult CommandLusers::Handle (const std::vector<std::string>&, User *user)
 		if (i->parentname == ServerInstance->Config->ServerName)
 			n_local_servs++;
 	}
+	// fix for default GetServerList not returning us
+	if (!n_serv)
+		n_serv = 1;
 
 	// these are updated on every connect (or /lusers invocation), which is good enough
 	if (ServerInstance->Users->LocalUserCount() > max_local)
@@ -71,7 +74,7 @@ CmdResult CommandLusers::Handle (const std::vector<std::string>&, User *user)
 		max_global = n_users;
 
 	user->WriteNumeric(251, "%s :There are %d users and %d invisible on %d servers",user->nick.c_str(),
-			n_users, n_users-n_invis, n_serv);
+			n_users-n_invis, n_invis, n_serv);
 
 	if (ServerInstance->Users->OperCount())
 		user->WriteNumeric(252, "%s %d :operator(s) online",user->nick.c_str(),ServerInstance->Users->OperCount());
@@ -79,9 +82,7 @@ CmdResult CommandLusers::Handle (const std::vector<std::string>&, User *user)
 	if (ServerInstance->Users->UnregisteredUserCount())
 		user->WriteNumeric(253, "%s %d :unknown connections",user->nick.c_str(),ServerInstance->Users->UnregisteredUserCount());
 
-	if (ServerInstance->ChannelCount())
-		user->WriteNumeric(254, "%s %ld :channels formed",user->nick.c_str(),ServerInstance->ChannelCount());
-
+	user->WriteNumeric(254, "%s %ld :channels formed",user->nick.c_str(),ServerInstance->ChannelCount());
 	user->WriteNumeric(255, "%s :I have %d clients and %d servers",user->nick.c_str(),ServerInstance->Users->LocalUserCount(),n_local_servs);
 	user->WriteNumeric(265, "%s :Current Local Users: %d  Max: %d",user->nick.c_str(),ServerInstance->Users->LocalUserCount(),max_local);
 	user->WriteNumeric(266, "%s :Current Global Users: %d  Max: %d",user->nick.c_str(),n_users,max_global);
