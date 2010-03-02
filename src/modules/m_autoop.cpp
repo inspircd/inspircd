dfa3e202a58ed2a57ba6c9a33d00b98ee46552f9
Daniel De Graaf <danieldg@inspircd.org>
2010-03-01 18:22:21 -0600

    Fix access check when m_autoop is granting multiple flags

--- src/modules/m_autoop.cpp
+++ src/modules/m_autoop.cpp
@@ -34,28 +34,29 @@ class AutoOpList : public ListModeBase
 		if (pos == 0 || pos == std::string::npos)
 			return adding ? MOD_RES_DENY : MOD_RES_PASSTHRU;
 		unsigned int mylevel = channel->GetPrefixValue(source);
-		std::string mid = parameter.substr(0, pos);
-		ModeHandler* mh = mid.length() == 1 ?
-			ServerInstance->Modes->FindMode(mid[0], MODETYPE_CHANNEL) :
-			ServerInstance->Modes->FindMode(mid);
-
-		if (adding && (!mh || !mh->GetPrefixRank()))
+		irc::commasepstream modes(parameter.substr(0, pos));
+		std::string mid;
+		while (modes.GetToken(mid))
 		{
-			source->WriteNumeric(415, "%s %s :Cannot find prefix mode '%s' for autoop",
-				source->nick.c_str(), mid.c_str(), mid.c_str());
-			return MOD_RES_DENY;
-		}
-		else if (!mh)
-			return MOD_RES_PASSTHRU;
+			ModeHandler* mh = mid.length() == 1 ?
+				ServerInstance->Modes->FindMode(mid[0], MODETYPE_CHANNEL) :
+				ServerInstance->Modes->FindMode(mid);
 
-		std::string dummy;
-		if (mh->AccessCheck(source, channel, dummy, true) == MOD_RES_DENY)
-			return MOD_RES_DENY;
-		if (mh->GetLevelRequired() > mylevel)
-		{
-			source->WriteNumeric(482, "%s %s :You must be able to set mode '%s' to include it in an autoop",
-				source->nick.c_str(), channel->name.c_str(), mid.c_str());
-			return MOD_RES_DENY;
+			if (adding && (!mh || !mh->GetPrefixRank()))
+			{
+				source->WriteNumeric(415, "%s %s :Cannot find prefix mode '%s' for autoop",
+					source->nick.c_str(), mid.c_str(), mid.c_str());
+				return MOD_RES_DENY;
+			}
+			else if (mh)
+			{
+				if (mh->GetLevelRequired() > mylevel)
+				{
+					source->WriteNumeric(482, "%s %s :You must be able to set mode '%s' to include it in an autoop",
+						source->nick.c_str(), channel->name.c_str(), mid.c_str());
+					return MOD_RES_DENY;
+				}
+			}
 		}
 		return MOD_RES_PASSTHRU;
 	}
