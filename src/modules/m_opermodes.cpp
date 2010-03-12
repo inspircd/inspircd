bca90c347a789963eef820d056eb95bcb70d533b
Daniel De Graaf <danieldg@inspircd.org>
2010-03-12 15:21:07 -0600

    Rename opermodes parameter to "automodes" to resolve conflict with the migrated <class:modes>

--- src/modules/m_opermodes.cpp
+++ src/modules/m_opermodes.cpp
@@ -43,7 +43,7 @@ class ModuleModesOnOper : public Module
 		// whenever a user opers, go through the oper types, find their <type:modes>,
 		// and if they have one apply their modes. The mode string can contain +modes
 		// to add modes to the user or -modes to take modes from the user.
-		std::string ThisOpersModes = user->oper->getConfig("modes");
+		std::string ThisOpersModes = user->oper->getConfig("automodes");
 		if (!ThisOpersModes.empty())
 		{
 			ApplyModes(user, ThisOpersModes);
