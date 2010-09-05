10ade1e23da96dee972be77d7cd3fe6d0f107a4d
Daniel De Graaf <danieldg@inspircd.org>
2010-09-05 12:43:13 -0400

    Break channel database read/write out of m_chanregister

---
+++ src/modules/m_flatfile_channels.cpp
@@ -0,0 +1,422 @@
+/*       +------------------------------------+
+ *       | Inspire Internet Relay Chat Daemon |
+ *       +------------------------------------+
+ *
+ *  InspIRCd: (C) 2002-2010 InspIRCd Development Team
+ * See: http://wiki.inspircd.org/Credits
+ *
+ * This program is free but copyrighted software; see
+ *            the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
+
+#include "inspircd.h"
+
+/******************************************************************************
+ * Flat-file database read/write
+ ******************************************************************************/
+
+/* structure for single entry */
+struct EntryDescriptor
+{
+	std::string name, ts, topicset, topicsetby, topic, modes, registrant;
+};
+/** reads the channel database and returns structure with it */
+class DatabaseReader
+{
+	/* the entry descriptor */
+	EntryDescriptor entry;
+	/* file */
+	FILE *fd;
+ public:
+	/* constructor, opens the file */
+	DatabaseReader (std::string filename)
+	{
+		/* initialize */
+		fd = NULL;
+		/* open the file */
+		fd = fopen (filename.c_str ( ), "r");
+		/* if we can't open the file, return. */
+		if (!fd) return;
+	}
+	/* destructor will close the file */
+	~DatabaseReader ( )
+	{
+		/* if fd is not null, close it */
+		if (fd) fclose (fd);
+	}
+	/* get next entry */
+	EntryDescriptor *next ( )
+	{
+		/* if fd is NULL, fake eof */
+		if (!fd) return 0;
+		/* clear data */
+		entry.name = "";
+		entry.ts = "";
+		entry.registrant = "";
+		entry.topicset = "";
+		entry.topicsetby = "";
+		entry.modes = "";
+		entry.topic = "";
+		std::string str;
+		/* read single characters from the file and add them to the string, end at EOF or \n, ignore \r */
+		while (1)
+		{
+			int c = fgetc (fd);
+			if ((c == EOF) || ((unsigned char)c == '\n')) break;
+			if ((unsigned char)c == '\r') continue;
+			str.push_back ((unsigned char)c);
+		}
+		/* ready to parse the line */
+		if (str == "") return 0;
+		std::string token;
+		irc::spacesepstream sep(str);
+		/* get first one */
+		bool res = sep.GetToken (token);
+		/* malformed if it is not chaninfo */
+		if (token != "chaninfo" || !res)
+		{
+			ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed channel database");
+			return 0;
+		}
+		/* okay, read channel name */
+		res = sep.GetToken (token);
+		/* name was get, but if it's the last token, database is malformed */
+		if (!res)
+		{
+			ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed channel database");
+			return 0;
+		}
+		/* save name */
+		entry.name = token;
+		/* get next token, if it's the last one, db is malformed */
+		res = sep.GetToken (token);
+		if (!res)
+		{
+			ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed channel database");
+			return 0;
+		}
+		/* set channel timestamp */
+			entry.ts = token;
+		/* get the last token */
+		res = sep.GetToken (token);
+		/* this time we ignore things after this token */
+		/* set registrant */
+		entry.registrant = token;
+		/* initial entry read, read next lines in a loop until end, eof means malformed database again */
+		while (1)
+		{
+			str.clear ( );
+			/* read the line */
+			while (1)
+			{
+				int c = fgetc (fd);
+				if (c == EOF)
+				{
+					ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed channel database");
+					return 0;
+				}
+					unsigned char c2 = (unsigned char)c;
+				if (c2 == '\n') break;
+				if (c2 == '\r') continue;
+				str.push_back (c2);
+			}
+			irc::spacesepstream sep2(str);
+			/* get the token */
+			if (str == "")
+			{
+				ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed channel database");
+				return 0;
+			}
+			res = sep2.GetToken (token);
+			/* break the loop if token is "end" */
+			if (token == "end") break;
+			/* it is not, so the large if statement there */
+			else if (token == "topic")
+			{
+				/* topic info, if end then it is malformed */
+				if (sep2.StreamEnd ( ))
+				{
+					ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed topic declaration in channel database for channel %s", entry.name.c_str ( ));
+				}
+				/* if not, then read topic set time */
+				res = sep2.GetToken (token);
+				/* if end then malformed */
+				if (!res)
+				{
+					ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed topic declaration in channel database for channel %s", entry.name.c_str ( ));
+				}
+				/* save that */
+				entry.topicset = token;
+				/* get next token */
+				res = sep2.GetToken (token);
+				/* if last then malformed */
+				if (!res)
+				{
+					ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed topic declaration in channel database for channel %s", entry.name.c_str ( ));
+				}
+				entry.topicsetby = token;
+				/* get rest of the declaration into the topic string, this is a topic */
+				entry.topic = sep2.GetRemaining ( );
+			} else if (token == "modes")
+			{
+				/* modes token, used for representing modes, it's the easier one, just load remaining data */
+				if (sep2.StreamEnd ( ))
+				{
+					ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed modes declaration in channel database for channel %s", entry.name.c_str ( ));
+					continue;
+				}
+				entry.modes = sep2.GetRemaining ( );
+			}
+		}
+		/* return entry address */
+		return &entry;
+	}
+};
+/* class being a database writer, gets a database file name on construct */
+class DatabaseWriter
+{
+	/* file stream */
+	FILE *fd;
+	std::string dbname, tmpname;
+	/* public */
+	public:
+	/* constructor */
+	DatabaseWriter (std::string filename)
+	{
+		fd = NULL;
+		dbname = filename;
+		tmpname = filename + ".tmp";
+		/* ready, open temporary database */
+		fd = fopen (tmpname.c_str ( ), "w");
+		if (!fd)
+		{
+			ServerInstance->Logs->Log ("MODULE", DEFAULT, "cannot save to the channel database");
+		}
+	}
+	/* destructor */
+	~DatabaseWriter ( )
+	{
+		if (fd)
+		{
+			/* saving has been ended, close the database and flush buffers */
+			fclose (fd);
+			/* rename the database file */
+			if (rename (tmpname.c_str ( ), dbname.c_str ( )) == -1)
+			{
+				ServerInstance->Logs->Log ("MODULE", DEFAULT, "can't rename the database file");
+			}
+		}
+	}
+	/* save the single channel */
+	void next (Channel *chan)
+	{
+		if (!fd) return;
+		/* first, construct the chaninfo line */
+		std::string line;
+		line.append ("chaninfo ").append (chan->name).append (" ").append (ConvToStr(chan->age)).append ("\n");
+		if (fputs (line.c_str ( ), fd) == EOF)
+		{
+			ServerInstance->Logs->Log ("MODULE", DEFAULT, "unable to write the channel entry");
+			fclose (fd);
+			fd = NULL;
+			return;
+		}
+		/* now, write the topic if present */
+		if (!chan->topic.empty ( ))
+		{
+			line.clear ( );
+			line.append ("topic ").append (ConvToStr (chan->topicset)).append (" ").append (chan->setby).append (" ").append (chan->topic).append ("\n");
+			if (fputs (line.c_str ( ), fd) == EOF)
+			{
+				ServerInstance->Logs->Log ("MODULE", DEFAULT, "unable to write channel's topic declaration");
+				fclose (fd);
+				fd = NULL;
+				return;
+			}
+		}
+		/* get all channel modes */
+		line.clear ( );
+		irc::modestacker ms;
+		chan->ChanModes (ms, MODELIST_FULL);
+		line.append ("modes ").append (ms.popModeLine (FORMAT_PERSIST, INT_MAX, INT_MAX)).append ("\nend\n");
+		if (fputs (line.c_str ( ), fd) == EOF)
+		{
+			ServerInstance->Logs->Log ("MODULE", DEFAULT, "can't write mode declaration");
+			fclose (fd);
+			fd = NULL;
+			return;
+		}
+	}
+};
+
+class FlatFileChannelDB : public Module
+{
+ private:
+	std::string filedb;
+	bool dirty; // filedb needs to be flushed to disk
+
+	void WriteFileDatabase ( )
+	{
+		// Dump entire database; open/close in constructor/destructor
+		DatabaseWriter db (filedb);
+		for (chan_hash::const_iterator i = ServerInstance->chanlist->begin(); i != ServerInstance->chanlist->end(); i++)
+		{
+			if (i->second->IsModeSet("permanent") || i->second->IsModeSet("registered"))
+				db.next(i->second);
+		}
+	}
+
+	/** Read flat-file database */
+	void ReadFileDatabase ( )
+	{
+		/* create the reader object and open the database */
+		DatabaseReader db (filedb);
+		/* start the database read loop */
+		EntryDescriptor *entry;
+		while ((entry = db.next ( )))
+		{
+			/* we get a database entry */
+			/* so if channel name doesn't start with #, give an error and continue */
+			if (entry->name[0] != '#')
+			{
+				ServerInstance->Logs->Log ("MODULE", DEFAULT, "invalid channel name in channel database");
+				continue;
+			}
+			/* entry is valid */
+			/* try to find the channel */
+			Channel *chan = ServerInstance->FindChan (entry->name);
+			/* now, things that will be done depends on if channel was found or not */
+			if (chan)
+			{
+				/* if it was found, then channel timestamp becomes not important, only things needing to be set is the registrant status, do it */
+				/* create the mode stacker */
+				irc::modestacker ms;
+				/* we need to push the mode change */
+				ms.push (irc::modechange ("registered", entry->registrant));
+				/* make the server process and apply the mode change */
+				ServerInstance->Modes->Process (ServerInstance->FakeClient, chan, ms);
+				/* we don't do it below because channel doesn't exist then */
+				/* but in this situation, channel exists and there are users on it, they must know that mode has been changed */
+				/* send that info to them */
+				ServerInstance->Modes->Send (ServerInstance->FakeClient, chan, ms);
+			}
+			else
+			{
+				/* channel does not exist, allocate it, it will be constructed and added to the network as an empty channel */
+				chan = new Channel (entry->name, atol (entry->ts.c_str ( )));
+				/* empty modeless channel added and allocated */
+				/* so, channel exists in the network, then if topic is not empty, set data */
+				if (!entry->topic.empty ( ))
+				{
+					chan->topic = entry->topic;
+					chan->topicset = atol (entry->topicset.c_str ( ));
+					chan->setby = entry->topicsetby;
+				}
+				/* if modestring is not empty, then set all modes in it */
+				if (!entry->modes.empty ( ))
+				{
+					/* create sepstream */
+					irc::spacesepstream sep(entry->modes);
+					/* create modestacker for stacking all mode changes */
+					irc::modestacker ms;
+					/* spacesepstream is used to iterate through all modes that were saved for a channel */
+					/* modestacker ms is used to stack them */
+					/* iterate through the mode list */
+					while (!sep.StreamEnd ( ))
+					{
+						/* get the mode to be applied */
+						std::string token, name, value;
+						sep.GetToken (token);
+						/* the mode looks like name=value unless it doesn't have any value then it's just name */
+						/* find the position of = sign */
+						size_t namepos = token.find ('=');
+						/* if we didn't find anything, set name to the token because mode is... valueless */
+						if (namepos == std::string::npos) name = token;
+						/* if it found that = sign, we have both name and value */
+						else
+						{
+							/* it found it, name is string before the character, value is the string after it */
+							name = token.substr (0, namepos);
+							value = token.substr (namepos + 1);
+						}
+						/* mode name and value found, we can now add it to the modestacker */
+						/* hmm, or we can't, reason is that letters are separate in user and channel modes, but mode names are not, so what if this thing we're setting is
+						an user mode? */
+						/* to check that, we must find the mode */
+						ModeHandler *mc = ServerInstance->Modes->FindMode (name);
+						/* those actions will be taken only if mode was found and if it's the channel mode */
+						if (mc && mc->GetModeType ( ) == MODETYPE_CHANNEL)
+						{
+							/* mode was found and is not an user mode but a channel mode, push it to the modestacker, we use id instead of name for quicker push because we
+							have the mode handler now */
+							ms.push (irc::modechange (mc->id, value));
+						}
+					}
+					/* okay, all modes were pushed, modestacker can now produce the mode line */
+					/* so, process all mode changes and apply them on a channel */
+					ServerInstance->Modes->Process (ServerInstance->FakeClient, chan, ms);
+				}
+			}
+		}
+	}
+
+ public:
+	/* module constructor, for initializing stuff */
+	FlatFileChannelDB() : dirty(true)
+	{
+	}
+	/* get module version and flags. */
+	Version GetVersion()
+	{
+		return Version("Provides channel database storage in a flat-file format", VF_VENDOR);
+	}
+	void init ( )
+	{
+		Implementation eventlist[] = {
+			I_OnBackgroundTimer, I_OnMode, I_OnPostTopicChange
+		};
+
+		ServerInstance->Modules->Attach(eventlist, this, sizeof(eventlist)/sizeof(Implementation));
+
+		ReadFileDatabase();
+	}
+	/* rehash event */
+	void ReadConfig(ConfigReadStatus& status)
+	{
+		ConfigTag *tag = ServerInstance->Config->GetTag ("chandb");
+
+		filedb = tag->getString ("dbfile");
+	}
+	/* called on background timer to write all channels to disk if they were changed */
+	void OnBackgroundTimer (time_t cur)
+	{
+		/* if not dirty then don't do anything */
+		if (!dirty)
+			return;
+		/* dirty, one of registered channels was changed, save it */
+		WriteFileDatabase();
+		/* clear dirty to prevent next savings */
+		dirty = false;
+	}
+	/* after the topic has been changed, call this to check if channel was registered */
+	void OnPostTopicChange (User *user, Channel *chan, const std::string &topic)
+	{
+		dirty = true;
+	}
+	/* this event is called after each modechange */
+	void OnMode (User *user, Extensible *target, const irc::modestacker &modes)
+	{
+		dirty = true;
+	}
+
+	void Prioritize()
+	{
+		// database reading may depend on channel modes being loaded
+		ServerInstance->Modules->SetPriority(this, I_ModuleInit, PRIORITY_LAST);
+	}
+};
+/* register the module */
+MODULE_INIT(FlatFileChannelDB)
---
+++ src/modules/m_sql_channels.cpp
@@ -0,0 +1,192 @@
+/*       +------------------------------------+
+ *       | Inspire Internet Relay Chat Daemon |
+ *       +------------------------------------+
+ *
+ *  InspIRCd: (C) 2002-2010 InspIRCd Development Team
+ * See: http://wiki.inspircd.org/Credits
+ *
+ * This program is free but copyrighted software; see
+ *            the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
+
+#include "inspircd.h"
+#include "sql.h"
+
+/* $ModDesc: Provides channel mode +r for channel registration */
+
+static bool reading = false;
+
+class DatabaseReadQuery : public SQLQuery
+{
+ public:
+	LocalIntExt& InDB;
+	DatabaseReadQuery(Module* me, LocalIntExt& indb) : SQLQuery(me), InDB(indb)
+	{
+	}
+
+	void OnResult(SQLResult& res)
+	{
+		SQLEntries row;
+		reading = true;
+		while (res.GetRow(row))
+		{
+			std::string channame = row[0];
+			Channel* chan = ServerInstance->FindChan(channame);
+			if (!chan)
+			{
+				time_t ts = atol(row[1].value.c_str());
+				if (!ts)
+					ts = ServerInstance->Time();
+				chan = new Channel(channame, ts);
+			}
+			InDB.set(chan, 1);
+			if (!row[2].nul)
+			{
+				irc::spacesepstream modes(row[2]);
+				std::string mode;
+				irc::modestacker ms;
+				while (modes.GetToken(mode))
+				{
+					std::string::size_type eq = mode.find('=');
+					std::string name = mode.substr(0, eq);
+					std::string value;
+					if (eq != std::string::npos)
+						value = mode.substr(eq + 1);
+					ModeHandler *mh = ServerInstance->Modes->FindMode(name);
+					if (!mh)
+						continue;
+					ms.push(irc::modechange(mh->id, value, true));
+				}
+				ServerInstance->Modes->Process(ServerInstance->FakeClient, chan, ms);
+			}
+			if (!row[3].nul)
+			{
+				chan->topic = row[3];
+				chan->setby = row[4];
+				chan->topicset = atol(row[5].value.c_str());
+			}
+		}
+		reading = false;
+	}
+};
+class DiscardQuery : public SQLQuery
+{
+ public:
+	DiscardQuery(Module* me) : SQLQuery(me) {}
+	void OnResult(SQLResult& res) {}
+	void OnError(SQLerror& e)
+	{
+		ServerInstance->Logs->Log("m_chanregister", DEFAULT, "SQL update returned error: %s", e.str.c_str());
+	}
+};
+
+class ChanSQLDB : public Module
+{
+ private:
+	std::string tablename;
+	dynamic_reference<SQLProvider> sqldb;
+	LocalIntExt InDB;
+
+ public:
+	ChanSQLDB() : sqldb("SQL"), InDB("in_sql_chans", this)
+	{
+	}
+	Version GetVersion()
+	{
+		return Version("Provides channel state updates to an SQL databse", VF_VENDOR);
+	}
+	void init()
+	{
+		Implementation eventlist[] = {
+			I_OnMode, I_OnPostTopicChange
+		};
+
+		ServerInstance->Modules->Attach(eventlist, this, sizeof(eventlist)/sizeof(Implementation));
+
+		ParamL n;
+		n.push_back(tablename);
+		// just dump the entire thing back to us, please
+		sqldb->submit(new DatabaseReadQuery(this, InDB),
+			"SELECT (name, ts, modes, topic, topicset, topicts) FROM ?", n);
+	}
+	void ReadConfig(ConfigReadStatus& status)
+	{
+		ConfigTag *tag = ServerInstance->Config->GetTag ("chandb");
+
+		tablename = tag->getString("sqltable");
+		std::string dbid = tag->getString("dbid");
+		if (dbid.empty())
+			sqldb.SetProvider("SQL");
+		else
+			sqldb.SetProvider("SQL/" + dbid);
+		if (!sqldb)
+			status.ReportError(tag, "SQL database not found!");
+	}
+	void OnPostTopicChange (User *user, Channel *chan, const std::string &topic)
+	{
+		if (reading || !InDB.get(chan))
+			return;
+		ParamL n;
+		n.push_back(tablename);
+		n.push_back(chan->topic);
+		n.push_back(chan->setby);
+		n.push_back(ConvToStr(chan->topicset));
+		n.push_back(chan->name);
+		sqldb->submit(new DiscardQuery(this),
+			"UPDATE ? SET (topic, topicset, topicts) = ('?', '?', '?') WHERE name = '?'", n);
+	}
+
+	void OnMode (User *user, Extensible *target, const irc::modestacker &modes)
+	{
+		Channel *chan = IS_CHANNEL(target);
+		if (reading || !chan)
+			return;
+		if (!InDB.get(chan))
+		{
+			AddToDB(chan);
+			return;
+		}
+		irc::modestacker ms;
+		chan->ChanModes(ms, MODELIST_FULL);
+		ParamL n;
+		n.push_back(tablename);
+		n.push_back(ms.popModeLine(FORMAT_PERSIST, INT_MAX, INT_MAX));
+		n.push_back(chan->name);
+		sqldb->submit(new DiscardQuery(this),
+			"UPDATE ? SET modes = '?' WHERE name = '?'", n);
+	}
+
+	void AddToDB(Channel* chan)
+	{
+		irc::modestacker ms;
+		chan->ChanModes(ms, MODELIST_FULL);
+		ParamL n;
+		n.push_back(tablename);
+		n.push_back(chan->name);
+		n.push_back(ConvToStr(chan->age));
+		n.push_back(ms.popModeLine(FORMAT_PERSIST, INT_MAX, INT_MAX));
+		if (chan->topic.empty())
+		{
+			sqldb->submit(new DiscardQuery(this),
+				"INSERT INTO ? (name, ts, modes) VALUES ('?', '?', '?')", n);
+		}
+		else
+		{
+			n.push_back(chan->topic);
+			n.push_back(chan->setby);
+			n.push_back(ConvToStr(chan->topicset));
+			sqldb->submit(new DiscardQuery(this),
+				"INSERT INTO ? (name, ts, modes, topic, topicset, topicts) VALUES ('?', '?', '?', '?', '?', '?')", n);
+		}
+	}
+
+	void Prioritize()
+	{
+		// database reading may depend on channel modes being loaded
+		ServerInstance->Modules->SetPriority(this, I_ModuleInit, PRIORITY_LAST);
+	}
+};
+MODULE_INIT(ChanSQLDB)
