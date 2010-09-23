0e3ca2ead8bf68ca9c92d07f3065b26cf6bf182e
Jackmcbarn <jackmcbarn@jackmcbarn.no-ip.org>
2010-09-23 11:26:31 -0400

    Change m_flatfile_auth to derive its data storage class from Extensible, and correctly send account info read from the file

--- src/modules/m_flatfile_auth.cpp
+++ src/modules/m_flatfile_auth.cpp
@@ -20,26 +20,24 @@
 static dynamic_reference<AccountProvider> account("account");
 
 /* structure for single entry */
-class AccountDBEntry /* : public Extensible */
+class AccountDBEntry : public Extensible
 {
  public:
 	irc::string name;
 	time_t ts, hash_password_ts, connectclass_ts, tag_ts;
 	std::string hash, password, connectclass, tag;
-	AccountDBEntry() : name(""), ts(0), hash_password_ts(0), connectclass_ts(0), tag_ts(0), hash(""), password(""), connectclass(""), tag("") {}
+	AccountDBEntry() : Extensible(EXTENSIBLE_ACCOUNT), name(""), ts(0), hash_password_ts(0), connectclass_ts(0), tag_ts(0), hash(""), password(""), connectclass(""), tag("") {}
 };
 
 /******************************************************************************
  * Flat-file database read/write
  ******************************************************************************/
 
-typedef std::map<irc::string, AccountDBEntry> AccountDB;
+typedef std::map<irc::string, AccountDBEntry*> AccountDB;
 
 /** reads the account database and returns structure with it */
 class DatabaseReader
 {
-	/* the entry descriptor */
-	AccountDBEntry entry;
 	/* file */
 	FILE *fd;
  public:
@@ -64,16 +62,6 @@ class DatabaseReader
 	{
 		/* if fd is NULL, fake eof */
 		if (!fd) return 0;
-		/* clear data */
-		entry.name = "";
-		entry.ts = 0;
-		entry.hash = "";
-		entry.password = "";
-		entry.hash_password_ts = 0;
-		entry.connectclass = "";
-		entry.connectclass_ts = 0;
-		entry.tag = "";
-		entry.tag_ts = 0;
 		std::string str;
 		/* read single characters from the file and add them to the string, end at EOF or \n, ignore \r */
 		while (1)
@@ -85,6 +73,7 @@ class DatabaseReader
 		}
 		/* ready to parse the line */
 		if (str == "") return 0;
+		AccountDBEntry* entry = new AccountDBEntry();
 		std::string token;
 		irc::spacesepstream sep(str);
 		/* get first one */
@@ -92,13 +81,17 @@ class DatabaseReader
 		if (!sep.GetToken (token) || token != "acctinfo")
 		{
 			ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed account database - entry didn't start with acctinfo");
+			entry->cull();
+			delete entry;
 			return 0;
 		}
 		/* read account name */
 		/* if we don't have one, database is malformed */
-		if (!sep.GetToken (entry.name))
+		if (!sep.GetToken (entry->name))
 		{
 			ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed account database - ran out of tokens, expected name");
+			entry->cull();
+			delete entry;
 			return 0;
 		}
 		/* read account TS */
@@ -106,21 +99,27 @@ class DatabaseReader
 		if (!sep.GetToken (token))
 		{
 			ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed account database - ran out of tokens, expected ts");
+			entry->cull();
+			delete entry;
 			return 0;
 		}
-		entry.ts = atol(token.c_str());
+		entry->ts = atol(token.c_str());
 		/* read hash type */
 		/* if we don't have one, database is malformed */
-		if (!sep.GetToken (entry.hash))
+		if (!sep.GetToken (entry->hash))
 		{
 			ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed account database - ran out of tokens, expected hash");
+			entry->cull();
+			delete entry;
 			return 0;
 		}
 		/* read password */
 		/* if we don't have one, database is malformed */
-		if (!sep.GetToken (entry.password))
+		if (!sep.GetToken (entry->password))
 		{
 			ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed account database - ran out of tokens, expected password");
+			entry->cull();
+			delete entry;
 			return 0;
 		}
 		/* read hash and password TS */
@@ -128,14 +127,18 @@ class DatabaseReader
 		if (!sep.GetToken (token))
 		{
 			ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed account database - ran out of tokens, expected hash/password ts");
+			entry->cull();
+			delete entry;
 			return 0;
 		}
-		entry.hash_password_ts = atol(token.c_str());
+		entry->hash_password_ts = atol(token.c_str());
 		/* read connect class */
 		/* if we don't have one, database is malformed. if it's blank, GetToken returns true and fills in an empty string */
-		if (!sep.GetToken (entry.connectclass))
+		if (!sep.GetToken (entry->connectclass))
 		{
 			ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed account database - ran out of tokens, expected connectclass");
+			entry->cull();
+			delete entry;
 			return 0;
 		}
 		/* read connect class TS */
@@ -143,14 +146,18 @@ class DatabaseReader
 		if (!sep.GetToken (token))
 		{
 			ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed account database - ran out of tokens, expected connectclass ts");
+			entry->cull();
+			delete entry;
 			return 0;
 		}
-		entry.connectclass_ts = atol(token.c_str());
+		entry->connectclass_ts = atol(token.c_str());
 		/* read tag */
 		/* if we don't have one, database is malformed. if it's blank, GetToken returns true and fills in an empty string */
-		if (!sep.GetToken (entry.tag))
+		if (!sep.GetToken (entry->tag))
 		{
 			ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed account database - ran out of tokens, expected tag");
+			entry->cull();
+			delete entry;
 			return 0;
 		}
 		/* read tag TS */
@@ -158,9 +165,11 @@ class DatabaseReader
 		if (!sep.GetToken (token))
 		{
 			ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed account database - ran out of tokens, expected tag ts");
+			entry->cull();
+			delete entry;
 			return 0;
 		}
-		entry.tag_ts = atol(token.c_str());
+		entry->tag_ts = atol(token.c_str());
 		/* initial entry read, read next lines in a loop until end, eof means malformed database again */
 		while (1)
 		{
@@ -172,6 +181,8 @@ class DatabaseReader
 				if (c == EOF)
 				{
 					ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed account database - eof, expected end");
+					entry->cull();
+					delete entry;
 					return 0;
 				}
 					unsigned char c2 = (unsigned char)c;
@@ -184,6 +195,8 @@ class DatabaseReader
 			if (str == "")
 			{
 				ServerInstance->Logs->Log ("MODULE", DEFAULT, "malformed account database - empty line");
+				entry->cull();
+				delete entry;
 				return 0;
 			}
 			sep2.GetToken (token);
@@ -191,8 +204,7 @@ class DatabaseReader
 			if (token == "end") break;
 			/* in the future, let other modules read their own data here */
 		}
-		/* return entry address */
-		return &entry;
+		return entry;
 	}
 };
 /* class being a database writer, gets a database file name on construct */
@@ -278,57 +290,68 @@ class CommandAcctinfo : public Command
 		if(parameters[0] == "SET")
 		{
 			if(iter == db.end()) return CMD_FAILURE; /* if this ever happens, we're desynced */
-			if(iter->second.ts < atol(parameters[2].c_str())) return CMD_FAILURE; /* we have an older account with the same name */
-			if(iter->second.ts > atol(parameters[2].c_str()))
+			if(iter->second->ts < atol(parameters[2].c_str())) return CMD_FAILURE; /* we have an older account with the same name */
+			if(iter->second->ts > atol(parameters[2].c_str()))
 			{
 				/* Nuke the entry. */
+				iter->second->cull();
+				delete iter->second;
 				db.erase(iter);
-				AccountDBEntry entry;
-				entry.name = parameters[1];
-				entry.ts = atol(parameters[2].c_str());
+				AccountDBEntry* entry = new AccountDBEntry();
+				entry->name = parameters[1];
+				entry->ts = atol(parameters[2].c_str());
 				std::pair<AccountDB::iterator, bool> result = db.insert(std::make_pair(parameters[1], entry));
 				iter = result.first;
 			}
 			/* in the future, let other modules look for their own data here */
 			if(parameters[3] == "hash_password")
 			{
-				if(iter->second.hash_password_ts > atol(parameters[4].c_str())) return CMD_FAILURE;
-				size_t delim = parameters[5].find_first_of(' ');
-				iter->second.hash = parameters[5].substr(0, delim);
-				iter->second.password = parameters[5].substr(delim + 1);
-				iter->second.hash_password_ts = atol(parameters[4].c_str());
+				if(iter->second->hash_password_ts > atol(parameters[4].c_str())) return CMD_FAILURE;
+				size_t delim;
+				if(parameters.size() > 5 && (delim = parameters[5].find_first_of(' ')) != std::string::npos)
+				{
+					iter->second->hash = parameters[5].substr(0, delim);
+					iter->second->password = parameters[5].substr(delim + 1);
+				}
+				else iter->second->hash = iter->second->password = "";
+				iter->second->hash_password_ts = atol(parameters[4].c_str());
 			}
 			else if(parameters[3] == "connectclass")
 			{
-				if(iter->second.connectclass_ts > atol(parameters[4].c_str())) return CMD_FAILURE;
-				iter->second.connectclass = parameters[5];
-				iter->second.connectclass_ts = atol(parameters[4].c_str());
+				if(iter->second->connectclass_ts > atol(parameters[4].c_str())) return CMD_FAILURE;
+				iter->second->connectclass = parameters.size() > 5 ? parameters[5] : "";
+				iter->second->connectclass_ts = atol(parameters[4].c_str());
 			}
 			else if(parameters[3] == "tag")
 			{
-				if(iter->second.tag_ts > atol(parameters[4].c_str())) return CMD_FAILURE;
-				iter->second.tag = parameters[5];
-				iter->second.tag_ts = atol(parameters[4].c_str());
+				if(iter->second->tag_ts > atol(parameters[4].c_str())) return CMD_FAILURE;
+				iter->second->tag = parameters.size() > 5 ? parameters[5] : "";
+				iter->second->tag_ts = atol(parameters[4].c_str());
 			}
 		}
 		else if(parameters[0] == "ADD")
 		{
-			if(iter == db.end() || iter->second.ts > atol(parameters[2].c_str()))
+			if(iter == db.end() || iter->second->ts > atol(parameters[2].c_str()))
 			{
-				if(iter->second.ts > atol(parameters[2].c_str())) db.erase(iter);
-				AccountDBEntry entry;
-				entry.name = parameters[1];
-				entry.ts = atol(parameters[2].c_str());
+				if(iter->second->ts > atol(parameters[2].c_str()))
+				{
+					iter->second->cull();
+					delete iter->second;
+					db.erase(iter);
+				}
+				AccountDBEntry* entry = new AccountDBEntry();
+				entry->name = parameters[1];
+				entry->ts = atol(parameters[2].c_str());
 				std::pair<AccountDB::iterator, bool> result = db.insert(std::make_pair(parameters[1], entry));
 				iter = result.first;
 			}
-			else if(iter->second.ts < atol(parameters[2].c_str())) return CMD_FAILURE;
+			else if(iter->second->ts < atol(parameters[2].c_str())) return CMD_FAILURE;
 		}
 		else if(parameters[0] == "DEL")
 		{
 			if(iter != db.end())
 			{
-				if(iter->second.ts < atol(parameters[2].c_str())) return CMD_FAILURE;
+				if(iter->second->ts < atol(parameters[2].c_str())) return CMD_FAILURE;
 				db.erase(iter);
 			}
 		}
@@ -363,13 +386,13 @@ class CommandLogin : public Command
 			password = parameters[1];
 		}
 		AccountDB::iterator iter = db.find(username);
-		if(iter == db.end() || ServerInstance->PassCompare(user, iter->second.password, password, iter->second.hash))
+		if(iter == db.end() || ServerInstance->PassCompare(user, iter->second->password, password, iter->second->hash))
 		{
 			user->WriteServ("NOTICE " + user->nick + " :Invalid username or password");
 			return CMD_FAILURE;
 		}
-		if(account) account->DoLogin(user, iter->first, iter->second.tag);
-		if(!iter->second.connectclass.empty()) ServerInstance->ForcedClass.set(user, iter->second.connectclass);
+		if(account) account->DoLogin(user, iter->first, iter->second->tag);
+		if(!iter->second->connectclass.empty()) ServerInstance->ForcedClass.set(user, iter->second->connectclass);
 		return CMD_SUCCESS;
 	}
 };
@@ -389,7 +412,7 @@ class ModuleFlatfileAuth : public Module
 		// Dump entire database; open/close in constructor/destructor
 		DatabaseWriter dbwriter (dbfile);
 		for (AccountDB::const_iterator i = db.begin(); i != db.end(); ++i)
-			dbwriter.next(&i->second);
+			dbwriter.next(i->second);
 	}
 
 	/** Read flat-file database */
@@ -401,11 +424,11 @@ class ModuleFlatfileAuth : public Module
 		AccountDBEntry *entry;
 		while ((entry = dbreader.next ( )))
 		{
-			std::pair<AccountDB::iterator, bool> result = db.insert(std::make_pair(entry->name, *entry));
+			std::pair<AccountDB::iterator, bool> result = db.insert(std::make_pair(entry->name, entry));
 			/* if it didn't insert because we already have one, look at its TS */
 			if(!result.second)
 			{
-				AccountDBEntry* existing = &result.first->second;
+				AccountDBEntry* existing = result.first->second;
 				/* if the TS's are the same, merge each field individually */
 				if(entry->ts == existing->ts)
 				{
@@ -429,11 +452,57 @@ class ModuleFlatfileAuth : public Module
 				/* if this one is older, replace the existing one with it completely */
 				else if(entry->ts < existing->ts)
 				{
+					result.first->second->cull();
+					delete result.first->second;
 					db.erase(result.first);
-					db.insert(std::make_pair(entry->name, *entry));
+					db.insert(std::make_pair(entry->name, entry));
 				}
-				/* the third case is that the one we read is newer, in which case we ignore it completely */
+				/* the third case is that the one we read is newer, in which case we get rid of it */
+				else
+				{
+					entry->cull();
+					delete entry;
+					continue;
+				}
 			}
+			std::vector<std::string> params;
+			params.push_back("*");
+			params.push_back("ACCTINFO");
+			params.push_back("ADD");
+			params.push_back(entry->name);
+			params.push_back(":" + ConvToStr(entry->ts));
+			ServerInstance->PI->SendEncapsulatedData(params);
+			params.clear();
+			params.push_back("*");
+			params.push_back("ACCTINFO");
+			params.push_back("SET");
+			params.push_back(entry->name);
+			params.push_back(ConvToStr(entry->ts));
+			params.push_back("hash_password");
+			params.push_back(ConvToStr(entry->hash_password_ts));
+			params.push_back(":" + entry->hash + " " + entry->password);
+			ServerInstance->PI->SendEncapsulatedData(params);
+			params.clear();
+			params.push_back("*");
+			params.push_back("ACCTINFO");
+			params.push_back("SET");
+			params.push_back(entry->name);
+			params.push_back(ConvToStr(entry->ts));
+			params.push_back("connectclass");
+			params.push_back(ConvToStr(entry->connectclass_ts));
+			params.push_back(":" + entry->connectclass);
+			ServerInstance->PI->SendEncapsulatedData(params);
+			params.clear();
+			params.push_back("*");
+			params.push_back("ACCTINFO");
+			params.push_back("SET");
+			params.push_back(entry->name);
+			params.push_back(ConvToStr(entry->ts));
+			params.push_back("tag");
+			params.push_back(ConvToStr(entry->tag_ts));
+			params.push_back(":" + entry->tag);
+			ServerInstance->PI->SendEncapsulatedData(params);
+			/* in the future, let other modules send their own data here */
 		}
 	}
 
@@ -446,9 +515,19 @@ class ModuleFlatfileAuth : public Module
 	{
 		ServerInstance->AddCommand(&cmd_acctinfo);
 		ServerInstance->AddCommand(&cmd_login);
-		Implementation eventlist[] = { I_OnSyncNetwork, I_OnBackgroundTimer };
+		Implementation eventlist[] = { I_OnSyncNetwork, I_OnBackgroundTimer /* , I_OnUnloadModule */ };
 		ServerInstance->Modules->Attach(eventlist, this, sizeof(eventlist)/sizeof(Implementation));
 		ReadFileDatabase();
+	}
+
+	virtual ~ModuleFlatfileAuth()
+	{
+		for(AccountDB::iterator iter = db.begin(); iter != db.end(); ++iter)
+		{
+			iter->second->cull();
+			delete iter->second;
+		}
+		db.clear();
 	}
 
 	void ReadConfig(ConfigReadStatus&)
@@ -461,14 +540,14 @@ class ModuleFlatfileAuth : public Module
 	{
 		for (AccountDB::const_iterator i = db.begin(); i != db.end(); ++i)
 		{
-			std::string name = i->first, ts = ConvToStr(i->second.ts);
+			std::string name = i->first, ts = ConvToStr(i->second->ts);
 			target->SendCommand("ENCAP * ACCTINFO ADD " + name + " :" + ts);
 			target->SendCommand("ENCAP * ACCTINFO SET " + name + " " + ts + " hash_password "
-				+ ConvToStr(i->second.hash_password_ts) + " :" + i->second.hash + " " + i->second.password);
+				+ ConvToStr(i->second->hash_password_ts) + " :" + i->second->hash + " " + i->second->password);
 			target->SendCommand("ENCAP * ACCTINFO SET " + name + " " + ts + " connectclass "
-				+ ConvToStr(i->second.connectclass_ts) + " :" + i->second.connectclass);
+				+ ConvToStr(i->second->connectclass_ts) + " :" + i->second->connectclass);
 			target->SendCommand("ENCAP * ACCTINFO SET " + name + " " + ts + " tag "
-				+ ConvToStr(i->second.tag_ts) + " :" + i->second.tag);
+				+ ConvToStr(i->second->tag_ts) + " :" + i->second->tag);
 			/* in the future, let other modules send their own data here */
 		}
 	}
@@ -483,6 +562,17 @@ class ModuleFlatfileAuth : public Module
 		WriteFileDatabase();
 		/* clear dirty to prevent next savings */
 		dirty = false;
+	}
+
+	void OnUnloadModule(Module* mod)
+	{
+		std::vector<reference<ExtensionItem> > acct_exts;
+		ServerInstance->Extensions.BeginUnregister(mod, EXTENSIBLE_ACCOUNT, acct_exts);
+		for(AccountDB::iterator iter = db.begin(); iter != db.end(); ++iter)
+		{
+			mod->OnCleanup(TYPE_OTHER, iter->second);
+			iter->second->doUnhookExtensions(acct_exts);
+		}
 	}
 
 	Version GetVersion()
