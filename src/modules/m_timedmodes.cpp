d19ac14dd25111b29349ae42d455b6844a5aa103
Jackmcbarn <jackmcbarn@jackmcbarn.no-ip.org>
2011-01-26 09:49:43 -0500

    Use SplitCommand

--- src/modules/m_timedmodes.cpp
+++ src/modules/m_timedmodes.cpp
@@ -61,14 +61,14 @@ And possibly make the following operator<=:
 
 /** Handle /TMODE
  */
-class CommandTmode : public Command
+class CommandTmode : public SplitCommand
 {
  public:
 	SimpleExtItem<timedmodes> tmodes;
 	TimedMode* current;
 	irc::modestacker toannounce;
 	limitlist chanlimits;
-	CommandTmode(Module* Creator) : Command(Creator,"TMODE", 1), tmodes(EXTENSIBLE_CHANNEL, "tmodes", Creator)
+	CommandTmode(Module* Creator) : SplitCommand(Creator,"TMODE", 1), tmodes(EXTENSIBLE_CHANNEL, "tmodes", Creator)
 	{
 		syntax = "<channel> [<duration> <modes> {<mode-parameters>}]"; // NB: This is the syntax for clients to use.  The s2s syntax is different: <channel> <expiry> <modes to set/unset upon expiry> {<mode-parameters>}
 		ServerInstance->Extensions.Register(&tmodes);
@@ -82,114 +82,81 @@ class CommandTmode : public Command
 		for (timedmodes::iterator i = existing->begin(); i != existing->end(); ++i)
 			if(T == *i) return true;
 		return false;
-	} 
+	}
 
-	CmdResult Handle (const std::vector<std::string> &parameters, User *user)
+	CmdResult HandleLocal (const std::vector<std::string> &parameters, LocalUser *user)
 	{
-		if(IS_SERVER(user)) {
-			// handle a server sending us a partially processed TMODE
-			TimedMode T;
-			Channel* channel = ServerInstance->FindChan(parameters[0]);
-			if(!channel || parameters.size() < 3) return CMD_FAILURE;
-			T.expire = atol(parameters[1].c_str());
-			std::vector<std::string> tmpparams = parameters;
-			tmpparams.erase(tmpparams.begin()+1); // Remove the expiry from the parameters we're giving to Parse()
-			Extensible* tmp;
-			ServerInstance->Modes->Parse(tmpparams, user, tmp, T.modes);
-			if(T.modes.sequence.empty()) return CMD_SUCCESS;
-			stable_sort(T.modes.sequence.begin(), T.modes.sequence.end(), &modeChangeLessThan);
+		TimedMode T;
+		Channel* channel = ServerInstance->FindChan(parameters[0]);
+		long duration;
+		if (!channel)
+		{
+			user->WriteNumeric(401, "%s %s :No such channel",user->nick.c_str(), parameters[0].c_str());
+			return CMD_FAILURE;
+		}
+		if (parameters.size() < 3)
+		{
 			timedmodes* existing = tmodes.get(channel);
-			if(!existing) {
-				existing = new timedmodes();
-				tmodes.set(channel, existing);
-			}
-			else { // if we just created the new container, it's obviously empty, so obviously we don't need to check for duplicates
+			if(existing)
 				for (timedmodes::iterator i = existing->begin(); i != existing->end(); ++i)
-					if(T == *i) return CMD_SUCCESS; // we already have this TimedMode, so this one's a duplicate
-			}
-			existing->push_back(T);
-			return CMD_SUCCESS;
-		} else {
-			// handle a user sending /TMODE
-			TimedMode T;
-			Channel* channel = ServerInstance->FindChan(parameters[0]);
-			long duration;
-			if (!channel)
-			{
-				user->WriteNumeric(401, "%s %s :No such channel",user->nick.c_str(), parameters[0].c_str());
-				return CMD_FAILURE;
-			}
-			if (parameters.size() < 3)
-			{
-				timedmodes* existing = tmodes.get(channel);
-				if(existing)
-					for (timedmodes::iterator i = existing->begin(); i != existing->end(); ++i)
-					{
-						irc::modestacker modes = i->modes;
-						std::string msgbegin = user->nick + " " + channel->name + " " + ConvToStr(i->expire) + " :";
-						int maxModeLength = MAXBUF - 9 - ServerInstance->Config->ServerName.length() - msgbegin.length();
-						while(!modes.sequence.empty()) {
-							user->WriteNumeric(954, msgbegin + modes.popModeLine(FORMAT_USER, maxModeLength, INT_MAX));
-						}
-					}
-				user->WriteNumeric(953, "%s %s :End of channel timed modes", user->nick.c_str(), channel->name.c_str());
-				return CMD_SUCCESS;
-			}
-			unsigned int maxsize = 30;
-			for (limitlist::iterator it = chanlimits.begin(); it != chanlimits.end(); ++it)
-				if (InspIRCd::Match(channel->name, it->mask)) {
-					maxsize = it->limit;
-					break;
-				}
-			timedmodes* existing = tmodes.get(channel);
-			if ((existing && existing->size() >= maxsize) || maxsize == 0)
-			{
-				user->WriteServ("NOTICE %s :Channel %s timedmodes list is full", user->nick.c_str(), channel->name.c_str());
-				return CMD_FAILURE;
-			}
-			duration = ServerInstance->Duration(parameters[1]);
-			if (duration < 1)
-			{
-				user->WriteServ("NOTICE %s :Invalid timed mode duration", user->nick.c_str());
-				return CMD_FAILURE;
-			}
-			T.expire = duration + ServerInstance->Time();
-			irc::modestacker modes;
-			std::vector<std::string> tmpparams = parameters;
-			tmpparams.erase(tmpparams.begin()+1); // Remove the duration from the parameters we're giving to Parse()
-			Extensible* tmp;
-			ServerInstance->Modes->Parse(tmpparams, user, tmp, modes);
-			if(modes.sequence.empty()) return CMD_SUCCESS;
-			stable_sort(modes.sequence.begin(), modes.sequence.end(), &modeChangeLessThan);
-			ModeHandler* regmh = ServerInstance->Modes->FindMode("registered");
-			std::string token;
-			for (std::vector<irc::modechange>::iterator iter = modes.sequence.begin(); iter != modes.sequence.end();)
-			{
-				ModeHandler* mh = ServerInstance->Modes->FindMode(iter->mode);
-				if(mh == regmh && !user->HasPrivPermission("channels/set-registration", false)) {
-					user->WriteNumeric(ERR_NOPRIVILEGES, "%s :Permission denied - you may not set the registered channel mode as a timed mode", user->nick.c_str());
-					iter = modes.sequence.erase(iter);
-					continue;
-				}
-
-				if(mh->IsListMode())
 				{
-					if(mh->GetTranslateType() == TR_NICK) 
+					irc::modestacker modes = i->modes;
+					std::string msgbegin = user->nick + " " + channel->name + " " + ConvToStr(i->expire) + " :";
+					int maxModeLength = MAXBUF - 9 - ServerInstance->Config->ServerName.length() - msgbegin.length();
+					while(!modes.sequence.empty()) {
+						user->WriteNumeric(954, msgbegin + modes.popModeLine(FORMAT_USER, maxModeLength, INT_MAX));
+					}
+				}
+			user->WriteNumeric(953, "%s %s :End of channel timed modes", user->nick.c_str(), channel->name.c_str());
+			return CMD_SUCCESS;
+		}
+		unsigned int maxsize = 30;
+		for (limitlist::iterator it = chanlimits.begin(); it != chanlimits.end(); ++it)
+			if (InspIRCd::Match(channel->name, it->mask)) {
+				maxsize = it->limit;
+				break;
+			}
+		timedmodes* existing = tmodes.get(channel);
+		if ((existing && existing->size() >= maxsize) || maxsize == 0)
+		{
+			user->WriteServ("NOTICE %s :Channel %s timedmodes list is full", user->nick.c_str(), channel->name.c_str());
+			return CMD_FAILURE;
+		}
+		duration = ServerInstance->Duration(parameters[1]);
+		if (duration < 1)
+		{
+			user->WriteServ("NOTICE %s :Invalid timed mode duration", user->nick.c_str());
+			return CMD_FAILURE;
+		}
+		T.expire = duration + ServerInstance->Time();
+		irc::modestacker modes;
+		std::vector<std::string> tmpparams = parameters;
+		tmpparams.erase(tmpparams.begin()+1); // Remove the duration from the parameters we're giving to Parse()
+		Extensible* tmp;
+		ServerInstance->Modes->Parse(tmpparams, user, tmp, modes);
+		if(modes.sequence.empty()) return CMD_SUCCESS;
+		stable_sort(modes.sequence.begin(), modes.sequence.end(), &modeChangeLessThan);
+		ModeHandler* regmh = ServerInstance->Modes->FindMode("registered");
+		std::string token;
+		for (std::vector<irc::modechange>::iterator iter = modes.sequence.begin(); iter != modes.sequence.end();)
+		{
+			ModeHandler* mh = ServerInstance->Modes->FindMode(iter->mode);
+			if(mh == regmh && !user->HasPrivPermission("channels/set-registration", false)) {
+				user->WriteNumeric(ERR_NOPRIVILEGES, "%s :Permission denied - you may not set the registered channel mode as a timed mode", user->nick.c_str());
+				iter = modes.sequence.erase(iter);
+				continue;
+			}
+
+			if(mh->IsListMode())
+			{
+				if(mh->GetTranslateType() == TR_NICK) 
+				{
+					if(iter->value.find_first_of(',') != std::string::npos)
 					{
-						if(iter->value.find_first_of(',') != std::string::npos)
+						irc::commasepstream sep(iter->value);
+						while(sep.GetToken(token))
 						{
-							irc::commasepstream sep(iter->value);
-							while(sep.GetToken(token))
-							{
-								User* u = ServerInstance->FindNick(token);
-								if(u)
-									T.modes.sequence.push_back(irc::modechange(iter->mode,
-										u->uuid, !iter->adding));
-							}
-						}
-						else
-						{
-							User* u = ServerInstance->FindNick(iter->value);
+							User* u = ServerInstance->FindNick(token);
 							if(u)
 								T.modes.sequence.push_back(irc::modechange(iter->mode,
 									u->uuid, !iter->adding));
@@ -197,69 +164,101 @@ class CommandTmode : public Command
 					}
 					else
 					{
-						T.modes.sequence.push_back(irc::modechange(iter->mode,
-							iter->value, !iter->adding));
+						User* u = ServerInstance->FindNick(iter->value);
+						if(u)
+							T.modes.sequence.push_back(irc::modechange(iter->mode,
+								u->uuid, !iter->adding));
 					}
-				}
-				else if(!iter->adding)
-				{
-					T.modes.sequence.push_back(irc::modechange(iter->mode,
-						mh->GetNumParams(true) ? channel->GetModeParameter(mh) : "", true));
-				}
-				else if (!channel->IsModeSet(mh))
-				{
-					T.modes.sequence.push_back(irc::modechange(iter->mode,
-						mh->GetNumParams(false) ? iter->value : "", false));
 				}
 				else
 				{
 					T.modes.sequence.push_back(irc::modechange(iter->mode,
-						channel->GetModeParameter(mh), true));
+						iter->value, !iter->adding));
 				}
-
-				++iter;
+			}
+			else if(!iter->adding)
+			{
+				T.modes.sequence.push_back(irc::modechange(iter->mode,
+					mh->GetNumParams(true) ? channel->GetModeParameter(mh) : "", true));
+			}
+			else if (!channel->IsModeSet(mh))
+			{
+				T.modes.sequence.push_back(irc::modechange(iter->mode,
+					mh->GetNumParams(false) ? iter->value : "", false));
+			}
+			else
+			{
+				T.modes.sequence.push_back(irc::modechange(iter->mode,
+					channel->GetModeParameter(mh), true));
 			}
 
-			current = &T;
-			ServerInstance->SendMode(user, channel, modes, true);
-			current = NULL;
-			if(T.modes.sequence.empty()) return CMD_SUCCESS;
-			if(!existing) {
-				existing = new timedmodes();
-				tmodes.set(channel, existing);
-			}
-			else {
-				for (timedmodes::iterator i = existing->begin(); i != existing->end(); ++i)
-					if(T == *i) {
-						user->WriteServ("NOTICE %s :The timed mode given is an exact duplicate of one already set", user->nick.c_str());
-						return CMD_FAILURE; // we already have this TimedMode, so this one's a duplicate
-					}
-			}
-			existing->push_back(T);
-
-			modes = toannounce;
-			toannounce = T.modes;
-			// Tell the channel and other servers what happened
-			int maxModeLength = MAXBUF - 24 - channel->name.length() - ConvToStr(T.expire).length();
-			while(!toannounce.sequence.empty()) {
-				std::vector<std::string> params;
-				params.push_back("*");
-				params.push_back("TMODE");
-				params.push_back(channel->name);
-				params.push_back(ConvToStr(T.expire + 5)); // Adding 5 seconds is a kludge to prevent race conditions resulting in multiple "timed modes expired" notices
-				params.push_back(toannounce.popModeLine(FORMAT_NETWORK, maxModeLength, INT_MAX));
-				ServerInstance->PI->SendEncapsulatedData(params);
-			}
-			toannounce = modes;
-			std::string msgbegin = "*** " + user->nick + " set the following timed modes lasting for " + ConvToStr(duration) + " seconds: ", modeline;
-			maxModeLength = MAXBUF - 15 - ServerInstance->Config->ServerName.length() - channel->name.length() - msgbegin.length();
-			while(!toannounce.sequence.empty()) {
-				modeline = toannounce.popModeLine(FORMAT_USER, maxModeLength, INT_MAX);
-				channel->WriteChannelWithServ(ServerInstance->Config->ServerName.c_str(), "NOTICE %s :%s%s", channel->name.c_str(), msgbegin.c_str(), modeline.c_str());
-				ServerInstance->PI->SendChannelNotice(channel, 0, msgbegin + modeline);
-			}
-			return CMD_SUCCESS;
+			++iter;
 		}
+
+		current = &T;
+		ServerInstance->SendMode(user, channel, modes, true);
+		current = NULL;
+		if(T.modes.sequence.empty()) return CMD_SUCCESS;
+		if(!existing) {
+			existing = new timedmodes();
+			tmodes.set(channel, existing);
+		}
+		else {
+			for (timedmodes::iterator i = existing->begin(); i != existing->end(); ++i)
+				if(T == *i) {
+					user->WriteServ("NOTICE %s :The timed mode given is an exact duplicate of one already set", user->nick.c_str());
+					return CMD_FAILURE; // we already have this TimedMode, so this one's a duplicate
+				}
+		}
+		existing->push_back(T);
+
+		modes = toannounce;
+		toannounce = T.modes;
+		// Tell the channel and other servers what happened
+		int maxModeLength = MAXBUF - 24 - channel->name.length() - ConvToStr(T.expire).length();
+		while(!toannounce.sequence.empty()) {
+			std::vector<std::string> params;
+			params.push_back("*");
+			params.push_back("TMODE");
+			params.push_back(channel->name);
+			params.push_back(ConvToStr(T.expire + 5)); // Adding 5 seconds is a kludge to prevent race conditions resulting in multiple "timed modes expired" notices
+			params.push_back(toannounce.popModeLine(FORMAT_NETWORK, maxModeLength, INT_MAX));
+			ServerInstance->PI->SendEncapsulatedData(params);
+		}
+		toannounce = modes;
+		std::string msgbegin = "*** " + user->nick + " set the following timed modes lasting for " + ConvToStr(duration) + " seconds: ", modeline;
+		maxModeLength = MAXBUF - 15 - ServerInstance->Config->ServerName.length() - channel->name.length() - msgbegin.length();
+		while(!toannounce.sequence.empty()) {
+			modeline = toannounce.popModeLine(FORMAT_USER, maxModeLength, INT_MAX);
+			channel->WriteChannelWithServ(ServerInstance->Config->ServerName.c_str(), "NOTICE %s :%s%s", channel->name.c_str(), msgbegin.c_str(), modeline.c_str());
+			ServerInstance->PI->SendChannelNotice(channel, 0, msgbegin + modeline);
+		}
+		return CMD_SUCCESS;
+	}
+
+	CmdResult HandleServer (const std::vector<std::string> &parameters, FakeUser *user)
+	{
+		TimedMode T;
+		Channel* channel = ServerInstance->FindChan(parameters[0]);
+		if(!channel || parameters.size() < 3) return CMD_FAILURE;
+		T.expire = atol(parameters[1].c_str());
+		std::vector<std::string> tmpparams = parameters;
+		tmpparams.erase(tmpparams.begin()+1); // Remove the expiry from the parameters we're giving to Parse()
+		Extensible* tmp;
+		ServerInstance->Modes->Parse(tmpparams, user, tmp, T.modes);
+		if(T.modes.sequence.empty()) return CMD_SUCCESS;
+		stable_sort(T.modes.sequence.begin(), T.modes.sequence.end(), &modeChangeLessThan);
+		timedmodes* existing = tmodes.get(channel);
+		if(!existing) {
+			existing = new timedmodes();
+			tmodes.set(channel, existing);
+		}
+		else { // if we just created the new container, it's obviously empty, so obviously we don't need to check for duplicates
+			for (timedmodes::iterator i = existing->begin(); i != existing->end(); ++i)
+				if(T == *i) return CMD_SUCCESS; // we already have this TimedMode, so this one's a duplicate
+		}
+		existing->push_back(T);
+		return CMD_SUCCESS;
 	}
 };
 
