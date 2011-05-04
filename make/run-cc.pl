3cb24a1ec87a7ef5bd87eae2509b33ff0246712c
Jackmcbarn <jackmcbarn@jackmcbarn.no-ip.org>
2011-05-04 18:28:12 -0400

    Update copyright notices to 2011

--- make/run-cc.pl
+++ make/run-cc.pl
@@ -4,7 +4,7 @@
 #       | Inspire Internet Relay Chat Daemon |
 #       +------------------------------------+
 #
-#  InspIRCd: (C) 2002-2010 InspIRCd Development Team
+#  InspIRCd: (C) 2002-2011 InspIRCd Development Team
 # See: http://wiki.inspircd.org/Credits
 #
 # This program is free but copyrighted software; see
