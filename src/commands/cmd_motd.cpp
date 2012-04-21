183fd751d319f6106e5e50057155d6f156a90e02
Justin Crawford <Justasic@Gmail.com>
2012-04-21 03:06:54 -0700

    Added C/C++ style escape codes for color codes in the MOTD along with @SaberUK's \x, \u, \b, \c aliases

--- src/commands/cmd_motd.cpp
+++ src/commands/cmd_motd.cpp
@@ -39,6 +39,58 @@ class CommandMotd : public Command
 	}
 };
 
+inline std::string replace_all(const std::string &str, const std::string &orig, const std::string &repl)
+{
+	std::string new_str = str;
+	std::string::size_type pos = new_str.find(orig), orig_length = orig.length(), repl_length = repl.length();
+	while (pos != std::string::npos)
+	{
+		new_str = new_str.substr(0, pos) + repl + new_str.substr(pos + orig_length);
+		pos = new_str.find(orig, pos + repl_length);
+	}
+	return new_str;
+}
+
+/*
+ * Replace all color codes from the special[] array to actual
+ * color code chars using C++ style escape sequences. You
+ * can append other chars to replace if you like (such as %U
+ * being underline). -- Justasic
+ */
+std::string ProcessColors(const std::string &string)
+{
+	static struct special_chars
+	{
+		std::string character;
+		std::string replace;
+		special_chars(const std::string &c, const std::string &r) : character(c), replace(r) { }
+	}
+
+	special[] = {
+		special_chars("\\002", "\002"),  // Bold
+                special_chars("\\037", "\037"),  // underline
+                special_chars("\\003", "\003"),  // Color
+		special_chars("\\0017", "\017"), // Stop colors
+		special_chars("\\u", "\037"),    // Alias for underline
+		special_chars("\\b", "\002"),    // Alias for Bold
+		special_chars("\\x", "\017"),    // Alias for stop
+		special_chars("\\c", "\003"),    // Alias for color
+                special_chars("", "")
+	};
+
+	std::string ret = string;
+	for(int i = 0; special[i].character.empty() == false; ++i)
+	{
+		std::string::size_type pos = ret.find(special[i].character);
+		if(pos != std::string::npos && ret[pos-1] == '\\' && ret[pos] == '\\')
+			continue; // Skip double slashes.
+
+		ret = replace_all(ret, special[i].character, special[i].replace);
+	}
+	// Replace double slashes with a single slash before we return
+	return replace_all(ret, "\\\\", "\\");
+}
+
 /** Handle /MOTD
  */
 CmdResult CommandMotd::Handle (const std::vector<std::string>& parameters, User *user)
@@ -62,7 +114,7 @@ CmdResult CommandMotd::Handle (const std::vector<std::string>& parameters, User 
 		RPL_MOTDSTART, user->nick.c_str(), ServerInstance->Config->ServerName.c_str());
 
 	for (file_cache::iterator i = motd->second.begin(); i != motd->second.end(); i++)
-		user->SendText(":%s %03d %s :- %s", ServerInstance->Config->ServerName.c_str(), RPL_MOTD, user->nick.c_str(),i->c_str());
+		user->SendText(":%s %03d %s :- %s", ServerInstance->Config->ServerName.c_str(), RPL_MOTD, user->nick.c_str(), ProcessColors(*i).c_str());
 
 	user->SendText(":%s %03d %s :End of message of the day.", ServerInstance->Config->ServerName.c_str(), RPL_ENDOFMOTD, user->nick.c_str());
 
