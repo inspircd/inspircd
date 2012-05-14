fc1bf82519ca236dd68cde2f0cbb94c572bf237a
Chin Lee <kwangchin@gmail.com>
2012-05-14 18:41:35 +0800

    Cleaning codes

--- src/modules/m_httpd_rehash.cpp
+++ src/modules/m_httpd_rehash.cpp
@@ -1,11 +1,6 @@
 /*
  * InspIRCd -- Internet Relay Chat Daemon
  *
- *   Copyright (C) 2009 Daniel De Graaf <danieldg@inspircd.org>
- *   Copyright (C) 2007-2008 Robin Burchell <robin+git@viroteck.net>
- *   Copyright (C) 2008 Pippijn van Steenhoven <pip88nl@gmail.com>
- *   Copyright (C) 2006-2008 Craig Edwards <craigedwards@brainbox.cc>
- *   Copyright (C) 2007 Dennis Friis <peavey@inspircd.org>
  *   Copyright (C) 2012 Chin Lee <kwangchin@gmail.com>
  *
  * This file is part of InspIRCd.  InspIRCd is free software: you can
@@ -29,48 +24,11 @@
 
 class ModuleHttpRehash : public Module
 {
-	static std::map<char, char const*> const &entities;
-
- public:
-
+public:
 	void init()
 	{
 		Implementation eventlist[] = { I_OnEvent };
 		ServerInstance->Modules->Attach(eventlist, this, sizeof(eventlist)/sizeof(Implementation));
-	}
-
-	std::string Sanitize(const std::string &str)
-	{
-		std::string ret;
-
-		for (std::string::const_iterator x = str.begin(); x != str.end(); ++x)
-		{
-			switch (*x)
-			{
-				case '<':
-					ret += "&lt;";
-				break;
-				case '>':
-					ret += "&gt;";
-				break;
-				case '&':
-					ret += "&amp;";
-				break;
-				case '"':
-					ret += "&quot;";
-				break;
-				default:
-					if (*x < 32 || *x > 126)
-					{
-						int n = *x;
-						ret += ("&#" + ConvToStr(n) + ";");
-					}
-					else
-						ret += *x;
-				break;
-			}
-		}
-		return ret;
 	}
 
 	void OnEvent(Event& event)
@@ -113,16 +71,5 @@ class ModuleHttpRehash : public Module
 	}
 };
 
-static std::map<char, char const*> const &init_entities()
-{
-	static std::map<char, char const*> entities;
-	entities['<'] = "lt";
-	entities['>'] = "gt";
-	entities['&'] = "amp";
-	entities['"'] = "quot";
-	return entities;
-}
-
-std::map<char, char const*> const &ModuleHttpRehash::entities = init_entities ();
-
 MODULE_INIT(ModuleHttpRehash)
+
