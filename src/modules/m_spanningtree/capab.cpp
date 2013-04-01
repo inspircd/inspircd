inspircd/inspircd

commit 56cae0f3a484cbcb20569b68917f1810a0c2f4a4

Remove legacy code, mostly related to 1.2 compatibility

src/modules/m_spanningtree/capab.cpp
@@ -40,17 +40,14 @@ std::string TreeSocket::MyModules(int filter)
 	for (std::vector<std::string>::const_iterator i = modlist.begin(); i != modlist.end(); ++i)
 	{
 		if (i != modlist.begin())
-			capabilities.push_back(proto_version > 1201 ? ' ' : ',');
+			capabilities.push_back(' ');
 		capabilities.append(*i);
 		Module* m = ServerInstance->Modules->Find(*i);
-		if (m && proto_version > 1201)
+		Version v = m->GetVersion();
+		if (!v.link_data.empty())
 		{
-			Version v = m->GetVersion();
-			if (!v.link_data.empty())
-			{
-				capabilities.push_back('=');
-				capabilities.append(v.link_data);
-			}
+			capabilities.push_back('=');
+			capabilities.append(v.link_data);
 		}
 	}
 	return capabilities;
@@ -90,7 +87,7 @@ void TreeSocket::SendCapabilities(int phase)
 	if (phase < 2)
 		return;
 
-	char sep = proto_version > 1201 ? ' ' : ',';
+	const char sep = ' ';
 	irc::sepstream modulelist(MyModules(VF_COMMON), sep);
 	irc::sepstream optmodulelist(MyModules(VF_OPTCOMMON), sep);
 	/* Send module names, split at 509 length */
@@ -139,8 +136,6 @@ void TreeSocket::SendCapabilities(int phase)
 		SetOurChallenge(ServerInstance->GenRandomStr(20));
 		extra = " CHALLENGE=" + this->GetOurChallenge();
 	}
-	if (proto_version < 1202)
-		extra += ServerInstance->Modes->FindMode('h', MODETYPE_CHANNEL) ? " HALFOP=1" : " HALFOP=0";
 
 	this->WriteLine("CAPAB CAPABILITIES " /* Preprocessor does this one. */
 			":NICKMAX="+ConvToStr(ServerInstance->Config->Limits.NickMax)+
@@ -212,7 +207,7 @@ bool TreeSocket::Capab(const parameterlist &params)
 		if ((this->capab->ModuleList != this->MyModules(VF_COMMON)) && (this->capab->ModuleList.length()))
 		{
 			std::string diffIneed, diffUneed;
-			ListDifference(this->capab->ModuleList, this->MyModules(VF_COMMON), proto_version > 1201 ? ' ' : ',', diffIneed, diffUneed);
+			ListDifference(this->capab->ModuleList, this->MyModules(VF_COMMON), ' ', diffIneed, diffUneed);
 			if (diffIneed.length() || diffUneed.length())
 			{
 				reason = "Modules incorrectly matched on these servers.";
@@ -348,7 +343,7 @@ bool TreeSocket::Capab(const parameterlist &params)
 		}
 		else
 		{
-			capab->ModuleList.push_back(proto_version > 1201 ? ' ' : ',');
+			capab->ModuleList.push_back(' ');
 			capab->ModuleList.append(params[1]);
 		}
 	}

docs/conf/inspircd.conf.example
@@ -614,12 +614,7 @@
 
          # nosnoticestack: This prevents snotices from 'stacking' and giving you
          # the message saying '(last message repeated X times)'. Defaults to no.
-         nosnoticestack="no"
-
-         # welcomenotice: When turned on, this sends a NOTICE to connecting users 
-         # with the text Welcome to <networkname>! after successful registration.
-         # Defaults to yes.
-         welcomenotice="yes">
+         nosnoticestack="no">
 
 
 #-#-#-#-#-#-#-#-#-#-#-# PERFORMANCE CONFIGURATION #-#-#-#-#-#-#-#-#-#-#

docs/conf/modules.conf.example
@@ -494,7 +494,7 @@
 # cloak prefix as shown below. The cloak key must be shared across    #
 # the network for correct cloaking.                                   #
 #                                                                     #
-# There are four methods of cloaking:                                 #
+# There are two methods of cloaking:                                  #
 #                                                                     #
 #   half           Cloak only the "unique" portion of a host; show    #
 #                  the last 2 parts of the domain, /16 subnet of IPv4 #
@@ -503,19 +503,8 @@
 #   full           Cloak the users completely, using three slices for #
 #                  common CIDR bans (IPv4: /16, /24; IPv6: /48, /64)  #
 #                                                                     #
-# These methods use a single key that can be any length of text.      #
+# The methods use a single key that can be any length of text.        #
 # An optional prefix may be specified to mark cloaked hosts.          #
-#                                                                     #
-# The following methods are maintained for backwards compatibility;   #
-# they are slightly less secure, and always hide unresolved IPs       #
-#                                                                     #
-#   compat-host    InspIRCd 1.2-compatible host-based cloaking        #
-#   compat-ip      InspIRCd 1.2-compatible ip-always cloaking         #
-#                                                                     #
-# If you use a compat cloaking mode then you must specify key1, key2, #
-# key3, key4; the values must be less than 0x80000000 and should be   #
-# picked at random. Prefix is mandatory, will default to network name #
-# if not specified, and will always have a "-" appended.              #
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 #
 #<cloak mode="half"
@@ -618,9 +607,7 @@
 #   quitmsg="Throttled" bootwait="10">
 
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
-# Custom prefixes: allows for channel prefixes to be added. 
-# This can replace m_chanprotect and m_halfop, except for services that
-# recognise those modules by name
+# Custom prefixes: allows for channel prefixes to be added.
 #<module name="m_customprefix.so">
 #
 # name       The name of the mode, must be unique from other modes
@@ -811,10 +798,6 @@
 # To use, GLOADMODULE, GUNLOADMODULE and GRELOADMODULE
 # must be in one of your oper class blocks.
 #<module name="m_globalload.so">
-
-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
-# Halfop module: Provides the +h (halfops) channel status mode.
-#<module name="m_halfop.so">
 
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 # HELPOP module: Provides the /HELPOP command

include/configreader.h
@@ -578,11 +578,6 @@ class CoreExport ServerConfig
 	/** If this value is true, snotices will not stack when repeats are sent
 	 */
 	bool NoSnoticeStack;
-
-	/** If true, a "Welcome to <networkname>!" NOTICE will be sent to
-	 * connecting users
-	 */
-	bool WelcomeNotice;
 };
 
 /** The background thread for config reading, so that reading from executable includes

src/configreader.cpp
@@ -551,7 +551,6 @@ void ServerConfig::Fill()
 	Limits.MaxAway = ConfValue("limits")->getInt("maxaway", 200);
 	InvBypassModes = options->getBool("invitebypassmodes", true);
 	NoSnoticeStack = options->getBool("nosnoticestack", false);
-	WelcomeNotice = options->getBool("welcomenotice", true);
 
 	range(SoftLimit, 10, ServerInstance->SE->GetMaxFds(), ServerInstance->SE->GetMaxFds(), "<performance:softlimit>");
 	range(MaxConn, 0, SOMAXCONN, SOMAXCONN, "<performance:somaxconn>");

src/modules/hash.h
@@ -40,20 +40,6 @@ class HashProvider : public DataProvider
 		return BinToBase64(sum(data), NULL, 0);
 	}
 
-	/** Allows the IVs for the hash to be specified. As the choice of initial IV is
-	 * important for the security of a hash, this should not be used except to
-	 * maintain backwards compatability. This also allows you to change the hex
-	 * sequence from its default of "0123456789abcdef", which does not improve the
-	 * strength of the output, but helps confuse those attempting to implement it.
-	 *
-	 * Example:
-	 * \code
-	 * unsigned int iv[] = { 0xFFFFFFFF, 0x00000000, 0xAAAAAAAA, 0xCCCCCCCC };
-	 * std::string result = Hash.sumIV(iv, "fedcba9876543210", "data");
-	 * \endcode
-	 */
-	virtual std::string sumIV(unsigned int* IV, const char* HexMap, const std::string &sdata) = 0;
-
 	/** HMAC algorithm, RFC 2104 */
 	std::string hmac(const std::string& key, const std::string& msg)
 	{

src/modules/m_cloaking.cpp
@@ -30,10 +30,6 @@
 
 enum CloakMode
 {
-	/** 1.2-compatible host-based cloak */
-	MODE_COMPAT_HOST,
-	/** 1.2-compatible IP-only cloak */
-	MODE_COMPAT_IPONLY,
 	/** 2.0 cloak of "half" of the hostname plus the full IP hash */
 	MODE_HALF_CLOAK,
 	/** 2.0 cloak of IP hash, split at 2 common CIDR range points */
@@ -147,7 +143,6 @@ class ModuleCloaking : public Module
 	std::string prefix;
 	std::string suffix;
 	std::string key;
-	unsigned int compatkey[4];
 	const char* xtab[4];
 	dynamic_reference<HashProvider> Hash;
 
@@ -222,63 +217,6 @@ class ModuleCloaking : public Module
 			rv[i] = base32[rv[i] & 0x1F];
 		}
 		return rv;
-	}
-
-	std::string CompatCloak4(const char* ip)
-	{
-		irc::sepstream seps(ip, '.');
-		std::string octet[4];
-		int i[4];
-
-		for (int j = 0; j < 4; j++)
-		{
-			seps.GetToken(octet[j]);
-			i[j] = atoi(octet[j].c_str());
-		}
-
-		octet[3] = octet[0] + "." + octet[1] + "." + octet[2] + "." + octet[3];
-		octet[2] = octet[0] + "." + octet[1] + "." + octet[2];
-		octet[1] = octet[0] + "." + octet[1];
-
-		/* Reset the Hash module and send it our IV */
-
-		std::string rv;
-
-		/* Send the Hash module a different hex table for each octet group's Hash sum */
-		for (int k = 0; k < 4; k++)
-		{
-			rv.append(Hash->sumIV(compatkey, xtab[(compatkey[k]+i[k]) % 4], octet[k]).substr(0,6));
-			if (k < 3)
-				rv.append(".");
-		}
-		/* Stick them all together */
-		return rv;
-	}
-
-	std::string CompatCloak6(const char* ip)
-	{
-		std::vector<std::string> hashies;
-		std::string item;
-		int rounds = 0;
-
-		/* Reset the Hash module and send it our IV */
-
-		for (const char* input = ip; *input; input++)
-		{
-			item += *input;
-			if (item.length() > 7)
-			{
-				hashies.push_back(Hash->sumIV(compatkey, xtab[(compatkey[0]+rounds) % 4], item).substr(0,8));
-				item.clear();
-			}
-			rounds++;
-		}
-		if (!item.empty())
-		{
-			hashies.push_back(Hash->sumIV(compatkey, xtab[(compatkey[0]+rounds) % 4], item).substr(0,8));
-		}
-		/* Stick them all together */
-		return irc::stringjoiner(":", hashies, 0, hashies.size() - 1).GetJoined();
 	}
 
 	std::string SegmentIP(const irc::sockets::sockaddrs& ip, bool full)
@@ -395,12 +333,6 @@ class ModuleCloaking : public Module
 		{
 			switch (mode)
 			{
-				case MODE_COMPAT_HOST:
-					testcloak = prefix + "-" + Hash->sumIV(compatkey, xtab[0], "*").substr(0,10);
-					break;
-				case MODE_COMPAT_IPONLY:
-					testcloak = Hash->sumIV(compatkey, xtab[0], "*").substr(0,10);
-					break;
 				case MODE_HALF_CLOAK:
 					testcloak = prefix + SegmentCloak("*", 3, 8) + suffix;
 					break;
@@ -418,75 +350,16 @@ class ModuleCloaking : public Module
 		suffix = tag->getString("suffix", ".IP");
 
 		std::string modestr = tag->getString("mode");
-		if (modestr == "compat-host")
-			mode = MODE_COMPAT_HOST;
-		else if (modestr == "compat-ip")
-			mode = MODE_COMPAT_IPONLY;
-		else if (modestr == "half")
+		if (modestr == "half")
 			mode = MODE_HALF_CLOAK;
 		else if (modestr == "full")
 			mode = MODE_OPAQUE;
 		else
-			throw ModuleException("Bad value for <cloak:mode>; must be one of compat-host, compat-ip, half, full");
+			throw ModuleException("Bad value for <cloak:mode>; must be half or full");
 
-		if (mode == MODE_COMPAT_HOST || mode == MODE_COMPAT_IPONLY)
-		{
-			bool lowercase = tag->getBool("lowercase");
-
-			/* These are *not* using the need_positive parameter of ReadInteger -
-			 * that will limit the valid values to only the positive values in a
-			 * signed int. Instead, accept any value that fits into an int and
-			 * cast it to an unsigned int. That will, a bit oddly, give us the full
-			 * spectrum of an unsigned integer. - Special
-			 *
-			 * We must limit the keys or else we get different results on
-			 * amd64/x86 boxes. - psychon */
-			const unsigned int limit = 0x80000000;
-			compatkey[0] = (unsigned int) tag->getInt("key1");
-			compatkey[1] = (unsigned int) tag->getInt("key2");
-			compatkey[2] = (unsigned int) tag->getInt("key3");
-			compatkey[3] = (unsigned int) tag->getInt("key4");
-
-			if (!lowercase)
-			{
-				xtab[0] = "F92E45D871BCA630";
-				xtab[1] = "A1B9D80C72E653F4";
-				xtab[2] = "1ABC078934DEF562";
-				xtab[3] = "ABCDEF5678901234";
-			}
-			else
-			{
-				xtab[0] = "f92e45d871bca630";
-				xtab[1] = "a1b9d80c72e653f4";
-				xtab[2] = "1abc078934def562";
-				xtab[3] = "abcdef5678901234";
-			}
-
-			if (prefix.empty())
-				prefix = ServerInstance->Config->Network;
-
-			if (!compatkey[0] || !compatkey[1] || !compatkey[2] || !compatkey[3] ||
-				compatkey[0] >= limit || compatkey[1] >= limit || compatkey[2] >= limit || compatkey[3] >= limit)
-			{
-				std::string detail;
-				if (!compatkey[0] || compatkey[0] >= limit)
-					detail = "<cloak:key1> is not valid, it may be set to a too high/low value, or it may not exist.";
-				else if (!compatkey[1] || compatkey[1] >= limit)
-					detail = "<cloak:key2> is not valid, it may be set to a too high/low value, or it may not exist.";
-				else if (!compatkey[2] || compatkey[2] >= limit)
-					detail = "<cloak:key3> is not valid, it may be set to a too high/low value, or it may not exist.";
-				else if (!compatkey[3] || compatkey[3] >= limit)
-					detail = "<cloak:key4> is not valid, it may be set to a too high/low value, or it may not exist.";
-
-				throw ModuleException("You have not defined cloak keys for m_cloaking!!! THIS IS INSECURE AND SHOULD BE CHECKED! - " + detail);
-			}
-		}
-		else
-		{
-			key = tag->getString("key");
-			if (key.empty() || key == "secret")
-				throw ModuleException("You have not defined cloak keys for m_cloaking. Define <cloak:key> as a network-wide secret.");
-		}
+		key = tag->getString("key");
+		if (key.empty() || key == "secret")
+			throw ModuleException("You have not defined cloak keys for m_cloaking. Define <cloak:key> as a network-wide secret.");
 	}
 
 	std::string GenCloak(const irc::sockets::sockaddrs& ip, const std::string& ipstr, const std::string& host)
@@ -495,29 +368,6 @@ class ModuleCloaking : public Module
 
 		switch (mode)
 		{
-			case MODE_COMPAT_HOST:
-			{
-				if (ipstr != host)
-				{
-					std::string tail = LastTwoDomainParts(host);
-
-					// xtab is not used here due to a bug in 1.2 cloaking
-					chost = prefix + "-" + Hash->sumIV(compatkey, "0123456789abcdef", host).substr(0,8) + tail;
-
-					/* Fix by brain - if the cloaked host is > the max length of a host (64 bytes
-					 * according to the DNS RFC) then they get cloaked as an IP.
-					 */
-					if (chost.length() <= 64)
-						break;
-				}
-				// fall through to IP cloak
-			}
-			case MODE_COMPAT_IPONLY:
-				if (ip.sa.sa_family == AF_INET6)
-					chost = CompatCloak6(ipstr.c_str());
-				else
-					chost = CompatCloak4(ipstr.c_str());
-				break;
 			case MODE_HALF_CLOAK:
 			{
 				if (ipstr != host)

src/modules/m_halfop.cpp
@@ -1,104 +1,0 @@
-/*
- * InspIRCd -- Internet Relay Chat Daemon
- *
- *   Copyright (C) 2009 Daniel De Graaf <danieldg@inspircd.org>
- *
- * This file is part of InspIRCd.  InspIRCd is free software: you can
- * redistribute it and/or modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation, version 2.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-
-/* $ModDesc: Channel half-operator mode provider */
-
-#include "inspircd.h"
-
-class ModeChannelHalfOp : public ModeHandler
-{
- public:
-	ModeChannelHalfOp(Module* parent);
-	ModeAction OnModeChange(User* source, User* dest, Channel* channel, std::string &parameter, bool adding);
-	unsigned int GetPrefixRank();
-	void RemoveMode(Channel* channel, irc::modestacker* stack = NULL);
-	void RemoveMode(User* user, irc::modestacker* stack = NULL);
-
-	ModResult AccessCheck(User* src, Channel*, std::string& value, bool adding)
-	{
-		if (!adding && src->nick == value)
-			return MOD_RES_ALLOW;
-		return MOD_RES_PASSTHRU;
-	}
-};
-
-ModeChannelHalfOp::ModeChannelHalfOp(Module* parent) : ModeHandler(parent, "halfop", 'h', PARAM_ALWAYS, MODETYPE_CHANNEL)
-{
-	list = true;
-	prefix = '%';
-	levelrequired = OP_VALUE;
-	m_paramtype = TR_NICK;
-}
-
-unsigned int ModeChannelHalfOp::GetPrefixRank()
-{
-	return HALFOP_VALUE;
-}
-
-void ModeChannelHalfOp::RemoveMode(Channel* channel, irc::modestacker* stack)
-{
-	const UserMembList* clist = channel->GetUsers();
-
-	for (UserMembCIter i = clist->begin(); i != clist->end(); i++)
-	{
-		if (stack)
-		{
-			stack->Push(this->GetModeChar(), i->first->nick);
-		}
-		else
-		{
-			std::vector<std::string> parameters;
-			parameters.push_back(channel->name);
-			parameters.push_back("-h");
-			parameters.push_back(i->first->nick);
-			ServerInstance->SendMode(parameters, ServerInstance->FakeClient);
-		}
-	}
-
-}
-
-void ModeChannelHalfOp::RemoveMode(User*, irc::modestacker* stack)
-{
-}
-
-ModeAction ModeChannelHalfOp::OnModeChange(User* source, User*, Channel* channel, std::string &parameter, bool adding)
-{
-	return MODEACTION_ALLOW;
-}
-
-class ModuleHalfop : public Module
-{
-	ModeChannelHalfOp mh;
- public:
-	ModuleHalfop() : mh(this)
-	{
-	}
-
-	void init()
-	{
-		ServerInstance->Modules->AddService(mh);
-	}
-
-	Version GetVersion()
-	{
-		return Version("Channel half-operator mode provider", VF_VENDOR);
-	}
-};
-
-MODULE_INIT(ModuleHalfop)

commit 65155ef5c520e89b1d96d1768c1d7308334940c9

Fix linker error when linking spanningtree caused by 56cae0f3a484cbcb20569b68917f1810a0c2f4a4

src/modules/m_spanningtree/capab.cpp
@@ -32,9 +32,6 @@ std::string TreeSocket::MyModules(int filter)
 {
 	std::vector<std::string> modlist = ServerInstance->Modules->GetAllModuleNames(filter);
 
-	if (filter == VF_COMMON && proto_version != ProtocolVersion)
-		CompatAddModules(modlist);
-
 	std::string capabilities;
 	sort(modlist.begin(),modlist.end());
 	for (std::vector<std::string>::const_iterator i = modlist.begin(); i != modlist.end(); ++i)

src/modules/m_spanningtree/treesocket.h
@@ -186,9 +186,6 @@ class TreeSocket : public BufferedSocket
 	 */
 	void SendCapabilities(int phase);
 
-	/** Add modules to VF_COMMON list for backwards compatability */
-	void CompatAddModules(std::vector<std::string>& modlist);
-
 	/* Isolate and return the elements that are different between two lists */
 	void ListDifference(const std::string &one, const std::string &two, char sep,
 		std::string& mleft, std::string& mright);