inspircd/inspircd

commit accccc212cd4f08a3c5532b1ae7a17e76bac8718

Replace some C-isms with C++-isms.
* 'const char*' to 'const std::string&'.
* snprintf to std::string concatenation.
* Replace duplicated OneOfMatches with InspIRCd::MatchMask.

src/modules/m_dnsbl.cpp
@@ -361,16 +361,12 @@ class ModuleDNSBL : public Module
 			ServerInstance->Logs->Log("m_dnsbl", LOG_DEBUG, "User has no connect class in OnSetUserIP");
 
 		unsigned char a, b, c, d;
-		char reversedipbuf[128];
-		std::string reversedip;
-
 		d = (unsigned char) (user->client_sa.in4.sin_addr.s_addr >> 24) & 0xFF;
 		c = (unsigned char) (user->client_sa.in4.sin_addr.s_addr >> 16) & 0xFF;
 		b = (unsigned char) (user->client_sa.in4.sin_addr.s_addr >> 8) & 0xFF;
 		a = (unsigned char) user->client_sa.in4.sin_addr.s_addr & 0xFF;
 
-		snprintf(reversedipbuf, 128, "%d.%d.%d.%d", d, c, b, a);
-		reversedip = std::string(reversedipbuf);
+		const std::string reversedip = ConvToStr(d) + "." + ConvToStr(c) + "." + ConvToStr(b) + "." + ConvToStr(a);
 
 		countExt.set(user, DNSBLConfEntries.size());
 

include/channels.h
@@ -176,7 +176,7 @@ class CoreExport Channel : public Extensible, public InviteBase
 	 * @param user The user being kicked (must be on this channel)
 	 * @param reason The reason for the kick
 	 */
-	void KickUser(User *src, User *user, const char* reason);
+	void KickUser(User *src, User *user, const std::string& reason);
 
 	/** Part a user from this channel with the given reason.
 	 * If the reason field is NULL, no reason will be sent.

include/hashcomp.h
@@ -284,7 +284,7 @@ namespace irc
 		 * mode changes to be obtained.
 		 */
 		int GetStackedLine(std::vector<std::string> &result, int max_line_size = 360);
-		
+
 	};
 
 	/** irc::tokenstream reads a string formatted as per RFC1459 and RFC2812.

include/inspircd.h
@@ -597,7 +597,7 @@ class CoreExport InspIRCd
 	 * @param map The character map to use when matching.
 	 */
 	static bool Match(const std::string &str, const std::string &mask, unsigned const char *map = NULL);
-	static bool Match(const  char *str, const char *mask, unsigned const char *map = NULL);
+	static bool Match(const char *str, const char *mask, unsigned const char *map = NULL);
 
 	/** Match two strings using pattern matching, optionally, with a map
 	 * to check case against (may be NULL). If map is null, match will be case insensitive.
@@ -607,7 +607,14 @@ class CoreExport InspIRCd
 	 * @param map The character map to use when matching.
 	 */
 	static bool MatchCIDR(const std::string &str, const std::string &mask, unsigned const char *map = NULL);
-	static bool MatchCIDR(const  char *str, const char *mask, unsigned const char *map = NULL);
+	static bool MatchCIDR(const char *str, const char *mask, unsigned const char *map = NULL);
+
+	/** Matches a hostname and IP against a space delimited list of hostmasks.
+	 * @param masks The space delimited masks to match against.
+	 * @param hostname The hostname to try and match.
+	 * @param ipaddr The IP address to try and match.
+	 */
+	static bool MatchMask(const std::string& masks, const std::string& hostname, const std::string& ipaddr);
 
 	/** Return true if the given parameter is a valid nick!user\@host mask
 	 * @param mask A nick!user\@host masak to match against
@@ -683,7 +690,7 @@ class CoreExport InspIRCd
 	/** Returns the full version string of this ircd
 	 * @return The version string
 	 */
-	std::string GetVersionString(bool rawversion = false);
+	std::string GetVersionString(bool getFullVersion = false);
 
 	/** Attempt to write the process id to a given file
 	 * @param filename The PID file to attempt to write to

src/channels.cpp
@@ -472,10 +472,9 @@ bool Channel::CheckBan(User* user, const std::string& mask)
 	if (at == std::string::npos)
 		return false;
 
-	char tomatch[MAXBUF];
-	snprintf(tomatch, MAXBUF, "%s!%s", user->nick.c_str(), user->ident.c_str());
+	const std::string nickIdent = user->nick + "!" + user->ident;
 	std::string prefix = mask.substr(0, at);
-	if (InspIRCd::Match(tomatch, prefix, NULL))
+	if (InspIRCd::Match(nickIdent, prefix, NULL))
 	{
 		std::string suffix = mask.substr(at + 1);
 		if (InspIRCd::Match(user->host, suffix, NULL) ||
@@ -532,9 +531,9 @@ void Channel::PartUser(User *user, std::string &reason)
 	this->DelUser(user);
 }
 
-void Channel::KickUser(User *src, User *user, const char* reason)
+void Channel::KickUser(User *src, User *user, const std::string& reason)
 {
-	if (!src || !user || !reason)
+	if (!src || !user)
 		return;
 
 	Membership* memb = GetUser(user);
@@ -585,7 +584,7 @@ void Channel::KickUser(User *src, User *user, const char* reason)
 		CUList except_list;
 		FOREACH_MOD(I_OnUserKick,OnUserKick(src, memb, reason, except_list));
 
-		WriteAllExcept(src, false, 0, except_list, "KICK %s %s :%s", name.c_str(), user->nick.c_str(), reason);
+		WriteAllExcept(src, false, 0, except_list, "KICK %s %s :%s", name.c_str(), user->nick.c_str(), reason.c_str());
 
 		user->chans.erase(this);
 		this->RemoveAllPrefixes(user);
@@ -611,18 +610,15 @@ void Channel::WriteChannel(User* user, const char* text, ...)
 
 void Channel::WriteChannel(User* user, const std::string &text)
 {
-	char tb[MAXBUF];
-
 	if (!user)
 		return;
 
-	snprintf(tb,MAXBUF,":%s %s", user->GetFullHost().c_str(), text.c_str());
-	std::string out = tb;
+	const std::string message = ":" + user->GetFullHost() + " " + text;
 
 	for (UserMembIter i = userlist.begin(); i != userlist.end(); i++)
 	{
 		if (IS_LOCAL(i->first))
-			i->first->Write(out);
+			i->first->Write(message);
 	}
 }
 
@@ -643,15 +639,12 @@ void Channel::WriteChannelWithServ(const std::string& ServName, const char* text
 
 void Channel::WriteChannelWithServ(const std::string& ServName, const std::string &text)
 {
-	char tb[MAXBUF];
-
-	snprintf(tb,MAXBUF,":%s %s", ServName.empty() ? ServerInstance->Config->ServerName.c_str() : ServName.c_str(), text.c_str());
-	std::string out = tb;
+	const std::string message = ":" + (ServName.empty() ? ServerInstance->Config->ServerName : ServName) + " " + text;
 
 	for (UserMembIter i = userlist.begin(); i != userlist.end(); i++)
 	{
 		if (IS_LOCAL(i->first))
-			i->first->Write(out);
+			i->first->Write(message);
 	}
 }
 
@@ -691,12 +684,8 @@ void Channel::WriteAllExcept(User* user, bool serversource, char status, CUList 
 
 void Channel::WriteAllExcept(User* user, bool serversource, char status, CUList &except_list, const std::string &text)
 {
-	char tb[MAXBUF];
-
-	snprintf(tb,MAXBUF,":%s %s", serversource ? ServerInstance->Config->ServerName.c_str() : user->GetFullHost().c_str(), text.c_str());
-	std::string out = tb;
-
-	this->RawWriteAllExcept(user, serversource, status, except_list, std::string(tb));
+	const std::string message = ":" + (serversource ? ServerInstance->Config->ServerName : user->GetFullHost()) + " " + text;
+	this->RawWriteAllExcept(user, serversource, status, except_list, message);
 }
 
 void Channel::RawWriteAllExcept(User* user, bool serversource, char status, CUList &except_list, const std::string &out)

src/commands/cmd_kick.cpp
@@ -77,7 +77,7 @@ CmdResult CommandKick::Handle (const std::vector<std::string>& parameters, User 
 		reason.assign(user->nick, 0, ServerInstance->Config->Limits.MaxKick);
 	}
 
-	c->KickUser(user, u, reason.c_str());
+	c->KickUser(user, u, reason);
 
 	return CMD_SUCCESS;
 }

src/commands/cmd_oper.cpp
@@ -21,8 +21,6 @@
 
 #include "inspircd.h"
 
-bool OneOfMatches(const char* host, const char* ip, const char* hostlist);
-
 /** Handle /OPER. These command handlers can be reloaded by the core,
  * and handle basic RFC1459 commands. Commands within modules work
  * the same way, however, they can be fully unloaded, where these
@@ -43,30 +41,14 @@ class CommandOper : public SplitCommand
 	CmdResult HandleLocal(const std::vector<std::string>& parameters, LocalUser *user);
 };
 
-bool OneOfMatches(const char* host, const char* ip, const std::string& hostlist)
-{
-	std::stringstream hl(hostlist);
-	std::string xhost;
-	while (hl >> xhost)
-	{
-		if (InspIRCd::Match(host, xhost, ascii_case_insensitive_map) || InspIRCd::MatchCIDR(ip, xhost, ascii_case_insensitive_map))
-		{
-			return true;
-		}
-	}
-	return false;
-}
-
 CmdResult CommandOper::HandleLocal(const std::vector<std::string>& parameters, LocalUser *user)
 {
-	char TheHost[MAXBUF];
-	char TheIP[MAXBUF];
 	bool match_login = false;
 	bool match_pass = false;
 	bool match_hosts = false;
 
-	snprintf(TheHost,MAXBUF,"%s@%s",user->ident.c_str(),user->host.c_str());
-	snprintf(TheIP, MAXBUF,"%s@%s",user->ident.c_str(),user->GetIPString().c_str());
+	const std::string userHost = user->ident + "@" + user->host;
+	const std::string userIP = user->ident + "@" + user->GetIPString();
 
 	OperIndex::iterator i = ServerInstance->Config->oper_blocks.find(parameters[0]);
 	if (i != ServerInstance->Config->oper_blocks.end())
@@ -75,7 +57,7 @@ CmdResult CommandOper::HandleLocal(const std::vector<std::string>& parameters, L
 		ConfigTag* tag = ifo->oper_block;
 		match_login = true;
 		match_pass = !ServerInstance->PassCompare(user, tag->getString("password"), parameters[1], tag->getString("hash"));
-		match_hosts = OneOfMatches(TheHost,TheIP,tag->getString("host"));
+		match_hosts = InspIRCd::MatchMask(tag->getString("host"), userHost, userIP);
 
 		if (match_pass && match_hosts)
 		{

src/commands/cmd_stats.cpp
@@ -283,7 +283,7 @@ void CommandStats::DoStats(char statschar, User* user, string_list &results)
 				double n_eaten = (double)( ( (uint64_t)(KernelTime.dwHighDateTime - ServerInstance->stats->LastCPU.dwHighDateTime) << 32 ) + (uint64_t)(KernelTime.dwLowDateTime - ServerInstance->stats->LastCPU.dwLowDateTime) )/100000;
 				double n_elapsed = (double)(ThisSample.QuadPart - ServerInstance->stats->LastSampled.QuadPart) / ServerInstance->stats->QPFrequency.QuadPart;
 				double per = (n_eaten/n_elapsed);
-				
+
 				char percent[30];
 
 				snprintf(percent, 30, "%03.5f%%", per);

commit 1f38b9adb576580d8e8638fefc5c7cb157f90a5c

Issue #604, fix m_dnsbl, broken in accccc212cd4f08a3c5532b1ae7a17e76bac8718

src/modules/m_dnsbl.cpp
@@ -356,11 +356,11 @@ class ModuleDNSBL : public Module
 		else
 			ServerInstance->Logs->Log(MODNAME, LOG_DEBUG, "User has no connect class in OnSetUserIP");
 
-		unsigned char a, b, c, d;
-		d = (unsigned char) (user->client_sa.in4.sin_addr.s_addr >> 24) & 0xFF;
-		c = (unsigned char) (user->client_sa.in4.sin_addr.s_addr >> 16) & 0xFF;
-		b = (unsigned char) (user->client_sa.in4.sin_addr.s_addr >> 8) & 0xFF;
-		a = (unsigned char) user->client_sa.in4.sin_addr.s_addr & 0xFF;
+		unsigned int a, b, c, d;
+		d = (unsigned int) (user->client_sa.in4.sin_addr.s_addr >> 24) & 0xFF;
+		c = (unsigned int) (user->client_sa.in4.sin_addr.s_addr >> 16) & 0xFF;
+		b = (unsigned int) (user->client_sa.in4.sin_addr.s_addr >> 8) & 0xFF;
+		a = (unsigned int) user->client_sa.in4.sin_addr.s_addr & 0xFF;
 
 		const std::string reversedip = ConvToStr(d) + "." + ConvToStr(c) + "." + ConvToStr(b) + "." + ConvToStr(a);
 