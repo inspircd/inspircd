ddf93e685c19b39fcf0f401f5c42bef6fc148504
William Pitcock <nenolod@dereferenced.org>
2013-11-26 23:51:16 +0000

    numerics: move RPL_MAPUSERS to numeric 745
    This resolves a conflict between ircu/charybdis's usage of numeric 270 for a server
    privilege listing (PRIVS command) and inspircd's map summary output.
    
    Fixes atheme/ircv3-specifications#25.
    
    Signed-off-by: William Pitcock <kaniini@dereferenced.org>.

--- include/numerics.h
+++ include/numerics.h
@@ -70,8 +70,6 @@ enum Numerics
 	RPL_LOCALUSERS                  = 265,
 	RPL_GLOBALUSERS                 = 266,
 
-	RPL_MAPUSERS                    = 270, // insp-specific
-
 	RPL_AWAY                        = 301,
 
 	RPL_SYNTAX                      = 304, // insp-specific
@@ -189,6 +187,8 @@ enum Numerics
 
 	ERR_CHANOPEN                    = 713,
 	ERR_KNOCKONCHAN                 = 714,
+
+	RPL_MAPUSERS                    = 745, // insp-specific
 
 	ERR_WORDFILTERED                = 936, // insp-specific, would be nice if we could get rid of this..
 	ERR_CANTUNLOADMODULE            = 972, // insp-specific
