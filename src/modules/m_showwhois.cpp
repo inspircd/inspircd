57d02ca02e441971bf27fae2c2ba5302c60e43c9
Attila Molnar <attilamolnar@hush.com>
2013-12-19 17:02:41 +0100

    m_showwhois Update settings on rehash

--- src/modules/m_showwhois.cpp
+++ src/modules/m_showwhois.cpp
@@ -82,7 +82,7 @@ class ModuleShowwhois : public Module
 	{
 	}
 
-	void init() CXX11_OVERRIDE
+	void ReadConfig(ConfigStatus& status) CXX11_OVERRIDE
 	{
 		ConfigTag* tag = ServerInstance->Config->ConfValue("showwhois");
 
