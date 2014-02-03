26fecc0bf2c72b9a0dea09c92ed89f78181f39f6
Attila Molnar <attilamolnar@hush.com>
2014-02-03 10:08:05 +0100

    m_banredirect Fix typo in a message (transfered -> transferred)
    Fixes issue #757 reported by @guikcd

--- src/modules/m_banredirect.cpp
+++ src/modules/m_banredirect.cpp
@@ -321,7 +321,7 @@ class ModuleBanRedirect : public Module
 						else
 						{
 							user->WriteNumeric(474, "%s %s :Cannot join channel (You are banned)", user->nick.c_str(), chan->name.c_str());
-							user->WriteNumeric(470, "%s %s %s :You are banned from this channel, so you are automatically transfered to the redirected channel.", user->nick.c_str(), chan->name.c_str(), redir->targetchan.c_str());
+							user->WriteNumeric(470, "%s %s %s :You are banned from this channel, so you are automatically transferred to the redirected channel.", user->nick.c_str(), chan->name.c_str(), redir->targetchan.c_str());
 							nofollow = true;
 							Channel::JoinUser(user, redir->targetchan.c_str(), false, "", false, ServerInstance->Time());
 							nofollow = false;
