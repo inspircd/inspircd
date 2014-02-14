85a14cb41f90ef31f6e88e4b83d67c326ccb75b5
Simon Guerrier <simon.guerrier@lightbulbcrew.fr>
2014-02-14 16:13:44 +0100

    Added method fetchDisplayName to the Module. It calls the backend to fetch the real name of the player.

--- src/modules/extra/m_oauthauth.cpp
+++ src/modules/extra/m_oauthauth.cpp
@@ -8,6 +8,7 @@
 #include "inspircd.h"
 #include <curl/curl.h>
 #include <curl/easy.h>
+
 /* $ModDesc: Allow/Deny connections based on an OAuth2 tokens and a userId/avatarId combination */
 /* $LinkerFlags: -lcurl */
 
@@ -17,12 +18,26 @@ enum AuthState {
 	AUTH_STATE_FAIL = 2
 };
 
+static size_t callback_func(char *stream, size_t size, size_t nmemb, void *buffer)
+{
+	void **buffer2 = (void**)buffer;
+	size_t realsize = size*nmemb;
+	ServerInstance->Logs->Log("MODULE", DEBUG, "callback lenght = " + std::to_string(realsize));
+	*buffer2 = malloc(realsize + 1);
+	memcpy(*buffer2, stream, (realsize));
+	((char*)*buffer2)[realsize] = '\0';
+	ServerInstance->Logs->Log("MODULE", DEBUG, "buffer = " + std::string((char*)*buffer2));
+	return realsize;
+}
+
 class ModuleOauthAuth : public Module
 {
 	LocalIntExt pendingExt;
 
 	std::string killreason;
 	std::string identityServer;
+	std::string worldServer;
+	std::string displayName;
 
  public:
 	ModuleOauthAuth() : pendingExt("oauthauth-wait", this)
@@ -33,8 +48,7 @@ class ModuleOauthAuth : public Module
 	{
 	    CURLcode curlInit = curl_global_init(CURL_GLOBAL_ALL);
 	    if (curlInit) {
-	        //TODO: Check how to quit inspircd gracefully
-	        exit(1);
+			return;
 	    }
 	    
 		ServerInstance->Modules->AddService(pendingExt);
@@ -48,6 +62,7 @@ class ModuleOauthAuth : public Module
     	ConfigTag* conf = ServerInstance->Config->ConfValue("oauthauth");
 		killreason = conf->getString("killreason");
 		identityServer = conf->getString("identityServer");
+        worldServer = conf->getString("worldServer");
 	}
 
 	ModResult OnUserRegister(LocalUser* user)
@@ -56,30 +71,64 @@ class ModuleOauthAuth : public Module
 			return MOD_RES_PASSTHRU;
 
         //TODO: Use of fields other than nick possible?
-        std::string token = user->password; 
+        std::string toParse  = user->password;
         std::string nick = user->nick;
-        size_t uPos = nick.find_first_of('u');
-        size_t aPos = nick.find_first_of('a');
+        size_t uPos = toParse.find_first_of('&');
 
-        //TODO: Possible to change nick here to the avatarName?
+        if (uPos == std::string::npos)
+        {
+            //Pass not in correct format
+        	pendingExt.set(user, AUTH_STATE_FAIL);
+			ServerInstance->Logs->Log("MODULE", DEBUG, "uPos failed");
+            return MOD_RES_PASSTHRU;
+        } 
+        uPos++;
+        std::string userId = toParse.substr(0, uPos - 1);
+        toParse = toParse.substr(uPos, toParse.size() - uPos);
+		size_t aPos = toParse.find_first_of('&');
 
-        if (uPos == std::string::npos || aPos == std::string::npos) {
-            //Nick not in correct format
-        	pendingExt.set(user, AUTH_STATE_FAIL);            
-        } else {
-            uPos++;
-            aPos++;
-            std::string userId = nick.substr(uPos, aPos - uPos - 1);
-            std::string avatarId = nick.substr(aPos);        
-            //TODO: Sync call might not be ok - look into threading model...
-    		//Consider doing this async?
-            bool validToken = checkToken(identityServer, userId, avatarId, token);
-            if (validToken) {
-                pendingExt.set(user, AUTH_STATE_NONE);
-            } else {
-            	pendingExt.set(user, AUTH_STATE_FAIL);
-            }        
-        }
+        if (aPos == std::string::npos)
+        {
+            //Pass not in correct format
+            pendingExt.set(user, AUTH_STATE_FAIL);
+			ServerInstance->Logs->Log("MODULE", DEBUG, "aPos failed");
+            return MOD_RES_PASSTHRU;
+        } 
+        aPos++;
+        std::string avatarId = toParse.substr(0, aPos - 1);
+		std::string token = toParse.substr(aPos, toParse.size() - aPos);
+		if (token.empty())
+		{
+			//Pass not in correct format
+            pendingExt.set(user, AUTH_STATE_FAIL);
+			ServerInstance->Logs->Log("MODULE", DEBUG, "token not found");
+            return MOD_RES_PASSTHRU;
+		}
+
+        //TODO: Sync call might not be ok - look into threading model...
+		//Consider doing this async?
+        bool validToken = checkToken(identityServer, userId, avatarId, token);
+        if (validToken) 
+        {
+            pendingExt.set(user, AUTH_STATE_NONE);
+            //Change nick to the displayName
+			if (fetchDisplayName(worldServer, avatarId))
+			{
+				user->ChangeNick(displayName);
+			}
+			else
+			{
+				pendingExt.set(user, AUTH_STATE_FAIL);
+				ServerInstance->Logs->Log("MODULE", DEBUG, "changeNick failed");
+				return MOD_RES_PASSTHRU;
+			}
+        } 
+        else 
+        {
+			ServerInstance->Logs->Log("MODULE", DEBUG, "token failed");
+        	pendingExt.set(user, AUTH_STATE_FAIL);
+        }        
+
    		return MOD_RES_PASSTHRU;
 	}
 
@@ -132,6 +181,30 @@ class ModuleOauthAuth : public Module
                 return true;        
         }
         return false;
+    }
+
+	bool fetchDisplayName(const std::string& baseUrl, const std::string& avatarId)
+    {
+    	std::string url = baseUrl;
+        url += "/avatars/";
+        url += avatarId;
+        url += "/name";
+
+        CURL *curl = curl_easy_init();
+        if (curl) 
+        {
+			char* data;
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &callback_func);
+            curl_easy_perform(curl);
+			displayName = std::string(data);
+			displayName.erase(0,1);
+			displayName.erase(displayName.size() - 1,1);
+            curl_easy_cleanup(curl);
+			return true;
+    	}
+		return false;
     }
 };
 
