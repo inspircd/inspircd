1abb1488b7a81f7bc6f8dfcbdc6108915dd5b142
Attila Molnar <attilamolnar@hush.com>
2014-03-06 12:20:19 +0100

    Move UNLOADMODULE handler into core_loadmodule

--- src/coremods/core_loadmodule.cpp
+++ src/coremods/core_loadmodule.cpp
@@ -53,4 +53,74 @@ CmdResult CommandLoadmodule::Handle (const std::vector<std::string>& parameters,
 	}
 }
 
-COMMAND_INIT(CommandLoadmodule)
+/** Handle /UNLOADMODULE.
+ */
+class CommandUnloadmodule : public Command
+{
+ public:
+	/** Constructor for unloadmodule.
+	 */
+	CommandUnloadmodule(Module* parent)
+		: Command(parent,"UNLOADMODULE", 1)
+	{
+		flags_needed = 'o';
+		syntax = "<modulename>";
+	}
+
+	/** Handle command.
+	 * @param parameters The parameters to the command
+	 * @param user The user issuing the command
+	 * @return A value from CmdResult to indicate command success or failure.
+	 */
+	CmdResult Handle(const std::vector<std::string>& parameters, User* user);
+};
+
+CmdResult CommandUnloadmodule::Handle(const std::vector<std::string>& parameters, User* user)
+{
+	if (!ServerInstance->Config->ConfValue("security")->getBool("allowcoreunload") &&
+		InspIRCd::Match(parameters[0], "cmd_*.so", ascii_case_insensitive_map))
+	{
+		user->WriteNumeric(ERR_CANTUNLOADMODULE, "%s :You cannot unload core commands!", parameters[0].c_str());
+		return CMD_FAILURE;
+	}
+
+	if (parameters[0] == "cmd_unloadmodule.so" || parameters[0] == "cmd_loadmodule.so")
+	{
+		user->WriteNumeric(ERR_CANTUNLOADMODULE, "%s :You cannot unload module loading commands!", parameters[0].c_str());
+		return CMD_FAILURE;
+	}
+
+	Module* m = ServerInstance->Modules->Find(parameters[0]);
+	if (m && ServerInstance->Modules->Unload(m))
+	{
+		ServerInstance->SNO->WriteGlobalSno('a', "MODULE UNLOADED: %s unloaded %s", user->nick.c_str(), parameters[0].c_str());
+		user->WriteNumeric(RPL_UNLOADEDMODULE, "%s :Module successfully unloaded.", parameters[0].c_str());
+	}
+	else
+	{
+		user->WriteNumeric(ERR_CANTUNLOADMODULE, "%s :%s", parameters[0].c_str(),
+			m ? ServerInstance->Modules->LastError().c_str() : "No such module");
+		return CMD_FAILURE;
+	}
+
+	return CMD_SUCCESS;
+}
+
+class CoreModLoadModule : public Module
+{
+	CommandLoadmodule cmdloadmod;
+	CommandUnloadmodule cmdunloadmod;
+
+ public:
+	CoreModLoadModule()
+		: cmdloadmod(this), cmdunloadmod(this)
+	{
+	}
+
+	Version GetVersion()
+	{
+		return Version("Provides the LOADMODULE and UNLOADMODULE commands", VF_VENDOR|VF_CORE);
+	}
+};
+
+MODULE_INIT(CoreModLoadModule)
