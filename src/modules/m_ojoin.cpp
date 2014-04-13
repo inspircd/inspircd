2334ebd9273bf7f9751a723a6196efb222997185
Attila Molnar <attilamolnar@hush.com>
2014-04-13 15:41:35 +0200

    m_ojoin Remove pointless FindChan() call
    Found by Coverity, CID: 1135649

--- src/modules/m_ojoin.cpp
+++ src/modules/m_ojoin.cpp
@@ -75,7 +75,6 @@ class CommandOjoin : public Command
 
 			if (notice)
 			{
-				channel = ServerInstance->FindChan(parameters[0]);
 				channel->WriteChannelWithServ(ServerInstance->Config->ServerName, "NOTICE %s :%s joined on official network business.",
 					parameters[0].c_str(), user->nick.c_str());
 				ServerInstance->PI->SendChannelNotice(channel, 0, user->nick + " joined on official network business.");
