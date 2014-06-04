14d3f4d09f51c4b761f8df5afe78abee933d36d4
Peter Powell <petpow@saberuk.com>
2014-06-04 15:44:25 +0100

    Add systemd service file.

--- make/template/main.mk
+++ make/template/main.mk
@@ -235,6 +235,9 @@ install: target
 @IFEQ $(SYSTEM) darwin
 	-$(INSTALL) -m $(INSTMODE_BIN) org.inspircd.plist $(BASE) 2>/dev/null
 @ENDIF
+@IFEQ $(SYSTEM) linux
+	-$(INSTALL) -m $(INSTMODE_LIB) inspircd.service $(BASE) 2>/dev/null
+@ENDIF
 	-$(INSTALL) -m $(INSTMODE_BIN) tools/genssl $(BINPATH)/inspircd-genssl 2>/dev/null
 	-$(INSTALL) -m $(INSTMODE_LIB) docs/conf/*.example $(CONPATH)/examples
 	-$(INSTALL) -m $(INSTMODE_LIB) docs/conf/aliases/*.example $(CONPATH)/examples/aliases
