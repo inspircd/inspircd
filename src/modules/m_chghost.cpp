983a22cef84af9201996094df137cf7b9ec5dceb
Attila Molnar <attilamolnar@hush.com>
2014-06-18 13:50:39 +0200

    m_chghost Allow ulined servers to change the host of unregistered users (for SASL)

--- src/modules/m_chghost.cpp
+++ src/modules/m_chghost.cpp
@@ -59,7 +59,8 @@ class CommandChghost : public Command
 
 		User* dest = ServerInstance->FindNick(parameters[0]);
 
-		if ((!dest) || (dest->registered != REG_ALL))
+		// Allow services to change the host of unregistered users
+		if ((!dest) || ((dest->registered != REG_ALL) && (!ServerInstance->ULine(user->server))))
 		{
 			user->WriteNumeric(ERR_NOSUCHNICK, "%s %s :No such nick/channel", user->nick.c_str(), parameters[0].c_str());
 			return CMD_FAILURE;
