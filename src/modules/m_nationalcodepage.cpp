3b321ceffa327174501c6eb2e976b63cb7de2562
Googolplexed <Googol@Googolplexed.net>
2014-09-27 17:26:52 +1000

    Tidied up whitespace

--- src/modules/m_nationalcodepage.cpp
+++ src/modules/m_nationalcodepage.cpp
@@ -21,109 +21,92 @@
 class ModuleNationalCodePage : public Module,public HandlerBase1<bool, const std::string&>
 {
         caller1<bool, const std::string&> rememberer;
-        const unsigned char * lowermap_rememberer;
-        
+        const unsigned char * lowermap_rememberer;        
         std::bitset<256> m_allowedchar;
-        
         unsigned char m_casemap[256];
-        
         std::string charset,casemapping;
                 
  public:
 	ModuleNationalCodePage()
-		: rememberer(ServerInstance->IsNick), lowermap_rememberer(national_case_insensitive_map)
-	{  
-	}
+	: rememberer(ServerInstance->IsNick), lowermap_rememberer(national_case_insensitive_map) {}
                 
-        //Check if Nick is valid using the current codepage                        
-        bool Call(const std::string& nick)
-        {
-            for(std::string::const_iterator it = nick.begin(); it != nick.end(); ++it)
-            {
-                //Check if character in normal range
-                if ((*it >= 'A') && (*it <= '}'))
-                    continue;
-                
-                //Check if character is okay, but needs to be after the initial
-                if ((((*it >= '0') && (*it <= '9')) || (*it == '-')) && (it != nick.begin()))
-                    continue;
+	//Check if Nick is valid using the current codepage                        
+	bool Call(const std::string& nick)
+	{
+		for(std::string::const_iterator it = nick.begin(); it != nick.end(); ++it)
+		{
+			//Check if character in normal range
+			if ((*it >= 'A') && (*it <= '}'))
+					continue;
+			
+			//Check if character is okay, but needs to be after the initial
+			if ((((*it >= '0') && (*it <= '9')) || (*it == '-')) && (it != nick.begin()))
+					continue;
 
-                //std::string's iterator returns signed chars.
-                //codepoints are handled unsigned
-                //Check if character is in the allowed range
-                if(m_allowedchar[(unsigned char) *it] == true)
-                    continue;
-                
-                return false; //Uncaught character, not valid
-            }
+			//std::string's iterator returns signed chars.
+			//codepoints are handled unsigned
+			//Check if character is in the allowed range
+			if(m_allowedchar[(unsigned char) *it] == true)
+					continue;
 
-            return true;            
-            
-            
-        }             
-      
-        void init() CXX11_OVERRIDE
+			return false; //Uncaught character, not valid
+		}
+		return true;            	
+	}             
+    void init() CXX11_OVERRIDE
 	{
             national_case_insensitive_map = m_casemap;
             ServerInstance->IsNick = this;
 	}
         
-        void On005Numeric(std::map<std::string, std::string>& tokens) CXX11_OVERRIDE
+	void On005Numeric(std::map<std::string, std::string>& tokens) CXX11_OVERRIDE
 	{
 		tokens["CASEMAPPING"] = charset;
 	}
         
-        void ReadConfig(ConfigStatus& status) CXX11_OVERRIDE
+	void ReadConfig(ConfigStatus& status) CXX11_OVERRIDE
 	{
-            //Copy the existing generic ASCII casemap into place
-            memcpy(m_casemap, rfc_case_insensitive_map, 256);
-            m_allowedchar.reset();
-            
-            
-            ConfigTag* nationalcodepage = ServerInstance->Config->ConfValue("nationalcodepage");
-            charset = nationalcodepage->getString("codepage");
-            
-            
-            //Use InspIRCd config engine to read tags for characters in decimal
-            //Split a range of codepoints in decimal form
-            ConfigTagList chars = ServerInstance->Config->ConfTags("cpchars");
-            for (ConfigIter i = chars.first; i != chars.second; ++i)
-            {
-                ConfigTag* tag = i->second;
-                std::string range = tag->getString("range");
-                irc::portparser coderange(range, false);
-                int j=-1;
-                while(0 != (j = coderange.GetToken()))
-                {
-                    //For each codepoint, add it to the allowed list
-                    m_allowedchar[j] = true;
-
-                }
-            }
-            
-            //Load in Upper->lowercase conversions.
-            ConfigTagList casemap = ServerInstance->Config->ConfTags("casemap");
-            for (ConfigIter i = casemap.first; i != casemap.second; ++i)
-            {
-                ConfigTag* tag = i->second;
-                unsigned char lower = (unsigned char) tag->getInt("lower",0,1,256);
-                unsigned char upper = (unsigned char) tag->getInt("upper",0,1,256);
-                
-                m_casemap[upper] = lower;
-            }
-            
+		//Copy the existing generic ASCII casemap into place
+		memcpy(m_casemap, rfc_case_insensitive_map, 256);
+		m_allowedchar.reset();	
+		ConfigTag* nationalcodepage = ServerInstance->Config->ConfValue("nationalcodepage");
+		charset = nationalcodepage->getString("codepage");
+		
+		//Use InspIRCd config engine to read tags for characters in decimal
+		//Split a range of codepoints in decimal form
+		ConfigTagList chars = ServerInstance->Config->ConfTags("cpchars");
+		for (ConfigIter i = chars.first; i != chars.second; ++i)
+		{
+			ConfigTag* tag = i->second;
+			std::string range = tag->getString("range");
+			irc::portparser coderange(range, false);
+			int j=-1;
+			while(0 != (j = coderange.GetToken()))
+			{
+				//For each codepoint, add it to the allowed list
+				m_allowedchar[j] = true;
+			}
+		}
+		//Load in Upper->lowercase conversions.
+		ConfigTagList casemap = ServerInstance->Config->ConfTags("casemap");
+		for (ConfigIter i = casemap.first; i != casemap.second; ++i)
+		{
+			ConfigTag* tag = i->second;
+			unsigned char lower = (unsigned char) tag->getInt("lower",0,1,256);
+			unsigned char upper = (unsigned char) tag->getInt("upper",0,1,256);			
+			m_casemap[upper] = lower;
+		}
 	}
         
-        Version GetVersion() CXX11_OVERRIDE
+    Version GetVersion() CXX11_OVERRIDE
 	{
 		return Version("8bit national character codepage support", VF_VENDOR|VF_COMMON);
 	}
-        
-        ~ModuleNationalCodePage()
-        {
-            ServerInstance->IsNick = rememberer;
-            national_case_insensitive_map = lowermap_rememberer;
-        }
-                
+
+	~ModuleNationalCodePage()
+	{
+		ServerInstance->IsNick = rememberer;
+		national_case_insensitive_map = lowermap_rememberer;
+	}	
 };
 MODULE_INIT(ModuleNationalCodePage)
--- src/modules/m_nationalutf.cpp
+++ src/modules/m_nationalutf.cpp
@@ -21,97 +21,96 @@
 class ModuleNationalUTF : public Module,public HandlerBase1<bool, const std::string&>
 {
         caller1<bool, const std::string&> rememberer;
-        
         typedef struct Range{
             unsigned long min;
             unsigned long max;
         } Range;
-        
+
         std::vector<Range> ranges;
-        
+
         //Returns the next character in UTF32 encoding and advances the iterator
         //past it
         unsigned long utf8to32(std::string str,std::string::const_iterator& it)
         {
-            
-                /*
-                 * UTF8 is a variable length encoding
-                 * The first UTF byte determines the number of following bytes
-                 * We set aside four 8-bit bitsets to hold the theoretical maximum number of bytes
-                 * As the initial header byte uses a different header, we keep that aside
-                 * as 'ltr'
-                 */
-                std::bitset<8> ltr = *it;
-                std::bitset<8> sets[3];
-                
-                //Blank out the newly created bitsets
-                for(int i=0;i<3;i++) sets[i].reset();
-                /*
-                 * As per the UTF spec, the number of initial true bits
-                 * determines the number of following bytes.
-                 * For each true bit, we advance the iterator forward and copy
-                 * the byte into the the relevant bitset.
-                 * We also clean up the initial headers of the first byte so we
-                 * don't have to do this later.
-                 */
-                
-                //MSB is 7, but we've already handled that in ltr, so start at 6
-                //Only a maximum of 4 bits can be set, including the initial
-                //Won't accidentally iterate past the 
-                int bytes = 0;
-                for(int i=6;ltr[i]!=0 && i>3;i--)
-                {
-                    //For true bit in the UTF header, we grab an extra byte.
-                    it++;
-                    if(it == str.end()) return 0; //Prevent invalid header from iterating too far.
-                    bytes++;
-                    sets[6-i] = std::bitset<8>(*it);
-                    
-                    //Remove the UTF header of the first byte
-                    ltr[i] = 0;
-                    
-                }
-                it--;//Set the iterator back so that the loop increments into right position;
-                ltr[7] = 0; //Remove the initial header bit
-                /*
-                 Here we do a basic check to ensure that the encoding is valid
-                 and a client isn't sending garbage data.
-                 */
-                for(int i=0;i<3;i++)
-                {
-                    
-                    //If an empty bitset is encountered, we can skip checking the rest
-                    //(They will all be empty too)
-                    if(!sets[i].any())
-                        break;
-                    
-                    if(sets[i][7] != 1 || sets[i][6] != 0)
-                        return false; //Invalid UTF encoding, subsequent byte did not have correct header
-                    
-                    //Strip UTF header
-                    sets[i][7] = 0;
-                }
-                /*
-                 * All bitsets are now in position, validated, and with headers
-                 * removed.
-                 */
-                
-                //Shift all bitsets left into position.
-                //Aside from the initial set, each needs to be shifted
-                //slightly further than normal to compansate for where the header
-                //bits were.
-                unsigned long l1 = ltr.to_ulong() << (24);
-                unsigned long l2 = sets[0].to_ulong() << (16+2);
-                unsigned long l3 = sets[1].to_ulong() << (8+4);
-                unsigned long l4 = sets[2].to_ulong() << (6);
-                
-                //The above operation assumes that the UTF8 encoding used four bytes
-                //If it didn't, the below shift will compensate, moving them into
-                //proper position
-                unsigned long utf32 = (l1+l2+l3+l4) >> ((bytes*-6)+30);
-                return utf32;            
+		
+			/*
+			 * UTF8 is a variable length encoding
+			 * The first UTF byte determines the number of following bytes
+			 * We set aside four 8-bit bitsets to hold the theoretical maximum number of bytes
+			 * As the initial header byte uses a different header, we keep that aside
+			 * as 'ltr'
+			 */
+			std::bitset<8> ltr = *it;
+			std::bitset<8> sets[3];
+			
+			//Blank out the newly created bitsets
+			for(int i=0;i<3;i++) sets[i].reset();
+			/*
+			 * As per the UTF spec, the number of initial true bits
+			 * determines the number of following bytes.
+			 * For each true bit, we advance the iterator forward and copy
+			 * the byte into the the relevant bitset.
+			 * We also clean up the initial headers of the first byte so we
+			 * don't have to do this later.
+			 */
+			
+			//MSB is 7, but we've already handled that in ltr, so start at 6
+			//Only a maximum of 4 bits can be set, including the initial
+			//Won't accidentally iterate past the 
+			int bytes = 0;
+			for(int i=6;ltr[i]!=0 && i>3;i--)
+			{
+				//For true bit in the UTF header, we grab an extra byte.
+				it++;
+				if(it == str.end()) return 0; //Prevent invalid header from iterating too far.
+				bytes++;
+				sets[6-i] = std::bitset<8>(*it);
+				
+				//Remove the UTF header of the first byte
+				ltr[i] = 0;
+				
+			}
+			it--;//Set the iterator back so that the loop increments into right position;
+			ltr[7] = 0; //Remove the initial header bit
+			/*
+			 Here we do a basic check to ensure that the encoding is valid
+			 and a client isn't sending garbage data.
+			 */
+			for(int i=0;i<3;i++)
+			{
+				
+				//If an empty bitset is encountered, we can skip checking the rest
+				//(They will all be empty too)
+				if(!sets[i].any())
+					break;
+				
+				if(sets[i][7] != 1 || sets[i][6] != 0)
+					return false; //Invalid UTF encoding, subsequent byte did not have correct header
+				
+				//Strip UTF header
+				sets[i][7] = 0;
+			}
+			/*
+			 * All bitsets are now in position, validated, and with headers
+			 * removed.
+			 */
+			
+			//Shift all bitsets left into position.
+			//Aside from the initial set, each needs to be shifted
+			//slightly further than normal to compansate for where the header
+			//bits were.
+			unsigned long l1 = ltr.to_ulong() << (24);
+			unsigned long l2 = sets[0].to_ulong() << (16+2);
+			unsigned long l3 = sets[1].to_ulong() << (8+4);
+			unsigned long l4 = sets[2].to_ulong() << (6);
+			
+			//The above operation assumes that the UTF8 encoding used four bytes
+			//If it didn't, the below shift will compensate, moving them into
+			//proper position
+			unsigned long utf32 = (l1+l2+l3+l4) >> ((bytes*-6)+30);
+			return utf32;            
         }
-        
+
         bool isValidUTFChar(const unsigned long& utf32)
         {
             for(std::vector<Range>::iterator it = ranges.begin();it != ranges.end();++it)
@@ -125,74 +124,69 @@ class ModuleNationalUTF : public Module,public HandlerBase1<bool, const std::str
                 
  public:
 	ModuleNationalUTF()
-		: rememberer(ServerInstance->IsNick)
-	{  
-	}
-                
-        //Check if Nick is valid using the current codepage                        
-        bool Call(const std::string& nick)
-        {
-            for(std::string::const_iterator it = nick.begin(); it != nick.end(); ++it)
-            {
-                //Check if character in normal range
-                if ((*it >= 'A') && (*it <= '}'))
-                    continue;
-                
-                //Check if character is okay, but needs to be after the initial
-                if ((((*it >= '0') && (*it <= '9')) || (*it == '-')) && (it != nick.begin()))
-                    continue;
-                
-                unsigned long utf32 = utf8to32(nick,it);
-                if(isValidUTFChar(utf32))
-                    continue;
-                
-                return false; //Uncaught character, not valid
-            }
+		: rememberer(ServerInstance->IsNick) {}
+			
+	//Check if Nick is valid using the current codepage                        
+	bool Call(const std::string& nick)
+	{
+		for(std::string::const_iterator it = nick.begin(); it != nick.end(); ++it)
+		{
+			//Check if character in normal range
+			if ((*it >= 'A') && (*it <= '}'))
+				continue;
+			
+			//Check if character is okay, but needs to be after the initial
+			if ((((*it >= '0') && (*it <= '9')) || (*it == '-')) && (it != nick.begin()))
+				continue;
 
-            return true;            
-            
-            
-        }             
+			unsigned long utf32 = utf8to32(nick,it);
+			if(isValidUTFChar(utf32))
+				continue;
+
+			return false; //Uncaught character, not valid
+		}
+		return true;            
+	}             
                              
-        void init() CXX11_OVERRIDE
+	void init() CXX11_OVERRIDE
 	{
             ServerInstance->IsNick = this;
 	}
         
-        void ReadConfig(ConfigStatus& status) CXX11_OVERRIDE
+	void ReadConfig(ConfigStatus& status) CXX11_OVERRIDE
 	{
-            ranges.clear();
-            
-            ConfigTagList chars = ServerInstance->Config->ConfTags("utfrange");
-            for (ConfigIter i = chars.first; i != chars.second; ++i)
-            {
-                int lower = i->second->getInt("lower");
-                int upper = i->second->getInt("upper");
-                int value = i->second->getInt("value");
-                if(value)
-                {
-                    lower = value;
-                    upper = value;
-                }   
-                if(lower && upper)
-                {
-                    Range mrange;
-                    mrange.min = lower;
-                    mrange.max = upper;
-                    ranges.push_back(mrange);
-                }
-            }            
-            
+		ranges.clear();
+		
+		ConfigTagList chars = ServerInstance->Config->ConfTags("utfrange");
+		for (ConfigIter i = chars.first; i != chars.second; ++i)
+		{
+			int lower = i->second->getInt("lower");
+			int upper = i->second->getInt("upper");
+			int value = i->second->getInt("value");
+			if(value)
+			{
+				lower = value;
+				upper = value;
+			}   
+			if(lower && upper)
+			{
+				Range mrange;
+				mrange.min = lower;
+				mrange.max = upper;
+				ranges.push_back(mrange);
+			}
+		}            	
 	}
         
-        Version GetVersion() CXX11_OVERRIDE
+	Version GetVersion() CXX11_OVERRIDE
 	{
 		return Version("Provides UTF nick support", VF_VENDOR|VF_COMMON);
 	}
         
-        ~ModuleNationalUTF()
-        {
-            ServerInstance->IsNick = rememberer;
-        }    
+	~ModuleNationalUTF()
+	{
+		ServerInstance->IsNick = rememberer;
+	}    
 };
+
 MODULE_INIT(ModuleNationalUTF)
