inspircd/inspircd

commit 234be2f3cb00735ec07c9df749a0d53effce5f65

Fix listmodes when the config does not specify a wildcard size entry.

src/modules/u_listmode.h
@@ -201,13 +201,13 @@ class ListModeBase : public ModeHandler
 			if (limit.mask.size() && limit.limit > 0)
 				chanlimits.push_back(limit);
 		}
-		if (chanlimits.empty())
-		{
-			ListLimit limit;
-			limit.mask = "*";
-			limit.limit = 64;
-			chanlimits.push_back(limit);
-		}
+
+		// Add the default entry. This is inserted last so if the user specifies a
+		// wildcard record in the config it will take precedence over this entry.
+		ListLimit limit;
+		limit.mask = "*";
+		limit.limit = 64;
+		chanlimits.push_back(limit);
 	}
 
 	/** Populate the Implements list with the correct events for a List Mode

commit d11323cf349815197d824facf79b6fdbfc9f5a98

Allow setting a limit of less than 64 entries for non-ban listmodes via <banlist>
Fixes regression introduced in 234be2f3cb

src/modules/u_listmode.h
@@ -294,6 +294,8 @@ class ListModeBase : public ModeHandler
 							return MODEACTION_DENY;
 						}
 					}
+					else
+						break;
 				}
 			}
 