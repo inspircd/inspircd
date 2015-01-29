inspircd/inspircd

commit bbeb5ea38686dfeb9537860770bbdb3bd2f9cd3f

Use iostream instead of C-style file operations.

src/modules/m_xline_db.cpp
@@ -20,6 +20,7 @@
 
 #include "inspircd.h"
 #include "xline.h"
+#include <fstream>
 
 /* $ModConfig: <xlinedb filename="data/xline.db">
  *  Specify the filename for the xline database here*/
@@ -85,18 +86,16 @@ class ModuleXLineDB : public Module
 
 	bool WriteDatabase()
 	{
-		FILE *f;
-
 		/*
 		 * We need to perform an atomic write so as not to fuck things up.
-		 * So, let's write to a temporary file, flush and sync the FD, then rename the file..
+		 * So, let's write to a temporary file, flush it, then rename the file..
 		 * Technically, that means that this can block, but I have *never* seen that.
-		 *		-- w00t
+		 *     -- w00t
 		 */
 		ServerInstance->Logs->Log("m_xline_db", LOG_DEBUG, "xlinedb: Opening temporary database");
 		std::string xlinenewdbpath = xlinedbpath + ".new";
-		f = fopen(xlinenewdbpath.c_str(), "w");
-		if (!f)
+		std::ofstream stream(xlinenewdbpath.c_str());
+		if (!stream.is_open())
 		{
 			ServerInstance->Logs->Log("m_xline_db", LOG_DEBUG, "xlinedb: Cannot create database! %s (%d)", strerror(errno), errno);
 			ServerInstance->SNO->WriteToSnoMask('a', "database: cannot create new db: %s (%d)", strerror(errno), errno);
@@ -112,7 +111,7 @@ class ModuleXLineDB : public Module
 		 * semblance of backwards compatibility for reading on startup..
 		 * 		-- w00t
 		 */
-		fprintf(f, "VERSION 1\n");
+		stream << "VERSION 1" << std::endl;
 
 		// Now, let's write.
 		std::vector<std::string> types = ServerInstance->XLines->GetAllTypes();
@@ -125,26 +124,21 @@ class ModuleXLineDB : public Module
 			for (LookupIter i = lookup->begin(); i != lookup->end(); ++i)
 			{
 				XLine* line = i->second;
-				fprintf(f, "LINE %s %s %s %lu %lu :%s\n",
-					line->type.c_str(),
-					line->Displayable().c_str(),
-					ServerInstance->Config->ServerName.c_str(),
-					(unsigned long)line->set_time,
-					(unsigned long)line->duration, line->reason.c_str());
+				stream << "LINE " << line->type << " " << line->Displayable() << " "
+					<< ServerInstance->Config->ServerName << " " << line->set_time << " "
+					<< line->duration << " " << line->reason << std::endl;
 			}
 		}
 
 		ServerInstance->Logs->Log("m_xline_db", LOG_DEBUG, "xlinedb: Finished writing XLines. Checking for error..");
 
-		int write_error = 0;
-		write_error = ferror(f);
-		write_error |= fclose(f);
-		if (write_error)
+		if (stream.fail())
 		{
 			ServerInstance->Logs->Log("m_xline_db", LOG_DEBUG, "xlinedb: Cannot write to new database! %s (%d)", strerror(errno), errno);
 			ServerInstance->SNO->WriteToSnoMask('a', "database: cannot write to new db: %s (%d)", strerror(errno), errno);
 			return false;
 		}
+		stream.close();
 
 #ifdef _WIN32
 		if (remove(xlinedbpath.c_str()))
@@ -167,42 +161,23 @@ class ModuleXLineDB : public Module
 
 	bool ReadDatabase()
 	{
-		FILE *f;
-		char linebuf[MAXBUF];
+		// If the xline database doesn't exist then we don't need to load it.
+		if (!ServerConfig::FileExists(xlinedbpath.c_str()))
+			return true;
 
-		f = fopen(xlinedbpath.c_str(), "r");
-		if (!f)
+		std::ifstream stream(xlinedbpath.c_str());
+		if (!stream.is_open())
 		{
-			if (errno == ENOENT)
-			{
-				/* xline.db doesn't exist, fake good return value (we don't care about this) */
-				return true;
-			}
-			else
-			{
-				/* this might be slightly more problematic. */
-				ServerInstance->Logs->Log("m_xline_db", LOG_DEBUG, "xlinedb: Cannot read database! %s (%d)", strerror(errno), errno);
-				ServerInstance->SNO->WriteToSnoMask('a', "database: cannot read db: %s (%d)", strerror(errno), errno);
-				return false;
-			}
+			ServerInstance->Logs->Log("m_xline_db", LOG_DEBUG, "xlinedb: Cannot read database! %s (%d)", strerror(errno), errno);
+			ServerInstance->SNO->WriteToSnoMask('a', "database: cannot read db: %s (%d)", strerror(errno), errno);
+			return false;
 		}
-
-		while (fgets(linebuf, MAXBUF, f))
+		
+		std::string line;
+		while (std::getline(stream, line))
 		{
-			char *c = linebuf;
-
-			while (c && *c)
-			{
-				if (*c == '\n')
-				{
-					*c = '\0';
-				}
-
-				c++;
-			}
-
 			// Inspired by the command parser. :)
-			irc::tokenstream tokens(linebuf);
+			irc::tokenstream tokens(line);
 			int items = 0;
 			std::string command_p[7];
 			std::string tmp;
@@ -213,7 +188,7 @@ class ModuleXLineDB : public Module
 				items++;
 			}
 
-			ServerInstance->Logs->Log("m_xline_db", LOG_DEBUG, "xlinedb: Processing %s", linebuf);
+			ServerInstance->Logs->Log("m_xline_db", LOG_DEBUG, "xlinedb: Processing %s", line.c_str());
 
 			if (command_p[0] == "VERSION")
 			{
@@ -223,7 +198,7 @@ class ModuleXLineDB : public Module
 				}
 				else
 				{
-					fclose(f);
+					stream.close();
 					ServerInstance->Logs->Log("m_xline_db", LOG_DEBUG, "xlinedb: I got database version %s - I don't understand it", command_p[1].c_str());
 					ServerInstance->SNO->WriteToSnoMask('a', "database: I got a database version (%s) I don't understand", command_p[1].c_str());
 					return false;
@@ -251,8 +226,7 @@ class ModuleXLineDB : public Module
 					delete xl;
 			}
 		}
-
-		fclose(f);
+		stream.close();
 		return true;
 	}
 

include/configreader.h
@@ -521,6 +521,12 @@ class CoreExport ServerConfig
 	 * @return True if the file exists and is readable.
 	 */
 	static bool FileExists(const char* file);
+	
+	/** Escapes a value for storage in a configuration key.
+	 * @param str The string to escape.
+	 * @param xml Are we using the XML config format?
+	 */
+	static std::string Escape(const std::string& str, bool xml = true);
 
 	/** If this value is true, invites will bypass more than just +i
 	 */

src/configreader.cpp
@@ -773,14 +773,31 @@ bool ServerConfig::FileExists(const char* file)
 	if ((sb.st_mode & S_IFDIR) > 0)
 		return false;
 
-	FILE *input = fopen(file, "r");
-	if (input == NULL)
-		return false;
-	else
+	return !access(file, F_OK);
+}
+
+std::string ServerConfig::Escape(const std::string& str, bool xml)
+{
+	std::string escaped;
+	for (std::string::const_iterator it = str.begin(); it != str.end(); ++it)
 	{
-		fclose(input);
-		return true;
+		switch (*it)
+		{
+			case '"':
+				escaped += xml ? "&quot;" : "\"";
+				break;
+			case '&':
+				escaped += xml ? "&amp;" : "&";
+				break;
+			case '\\':
+				escaped += xml ? "\\" : "\\\\";
+				break;
+			default:
+				escaped += *it;
+				break;
+		}
 	}
+	return escaped;
 }
 
 const char* ServerConfig::CleanFilename(const char* name)

src/modules/m_permchannels.cpp
@@ -19,6 +19,7 @@
 
 
 #include "inspircd.h"
+#include <fstream>
 
 /* $ModDesc: Provides support for channel mode +P to provide permanent channels */
 
@@ -26,79 +27,47 @@
 static std::string permchannelsconf;
 static bool WriteDatabase()
 {
-	FILE *f;
-
-	if (permchannelsconf.empty())
-	{
-		// Fake success.
-		return true;
-	}
-
-	std::string tempname = permchannelsconf + ".tmp";
-
 	/*
 	 * We need to perform an atomic write so as not to fuck things up.
-	 * So, let's write to a temporary file, flush and sync the FD, then rename the file..
-	 *		-- w00t
+	 * So, let's write to a temporary file, flush it, then rename the file..
+	 *     -- w00t
 	 */
-	f = fopen(tempname.c_str(), "w");
-	if (!f)
+	
+	// If the user has not specified a configuration file then we don't write one.
+	if (permchannelsconf.empty())
+		return true;
+
+	std::string permchannelsnewconf = permchannelsconf + ".tmp";
+	std::ofstream stream(permchannelsnewconf.c_str());
+	if (!stream.is_open())
 	{
 		ServerInstance->Logs->Log("m_permchannels", LOG_DEFAULT, "permchannels: Cannot create database! %s (%d)", strerror(errno), errno);
 		ServerInstance->SNO->WriteToSnoMask('a', "database: cannot create new db: %s (%d)", strerror(errno), errno);
 		return false;
 	}
+	
+	stream << "# This file is automatically generated by m_permchannels. Any changes will be overwritten." << std::endl
+		<< "<config format=\"xml\">" << std::endl;
 
-	fputs("# Permchannels DB\n# This file is autogenerated; any changes will be overwritten!\n<config format=\"compat\">\n", f);
-	// Now, let's write.
 	for (chan_hash::const_iterator i = ServerInstance->chanlist->begin(); i != ServerInstance->chanlist->end(); i++)
 	{
 		Channel* chan = i->second;
 		if (!chan->IsModeSet('P'))
 			continue;
 
-		char line[1024];
-		const char* items[] =
-		{
-			"<permchannels channel=",
-			chan->name.c_str(),
-			" topic=",
-			chan->topic.c_str(),
-			" modes=",
-			chan->ChanModes(true),
-			">\n"
-		};
-
-		int lpos = 0, item = 0, ipos = 0;
-		while (lpos < 1022 && item < 7)
-		{
-			char c = items[item][ipos++];
-			if (c == 0)
-			{
-				// end of this string; hop to next string, insert a quote
-				item++;
-				ipos = 0;
-				c = '"';
-			}
-			else if (c == '\\' || c == '"')
-			{
-				line[lpos++] = '\\';
-			}
-			line[lpos++] = c;
-		}
-		line[--lpos] = 0;
-		fputs(line, f);
+		stream << "<permchannels channel=\"" << ServerConfig::Escape(chan->name)
+			<< "\" topic=\"" << ServerConfig::Escape(chan->topic)
+			<< "\" modes=\"" << ServerConfig::Escape(chan->ChanModes(true))
+			<< "\">" << std::endl;
 	}
 
-	int write_error = 0;
-	write_error = ferror(f);
-	write_error |= fclose(f);
-	if (write_error)
+	if (stream.fail())
 	{
 		ServerInstance->Logs->Log("m_permchannels", LOG_DEFAULT, "permchannels: Cannot write to new database! %s (%d)", strerror(errno), errno);
 		ServerInstance->SNO->WriteToSnoMask('a', "database: cannot write to new db: %s (%d)", strerror(errno), errno);
 		return false;
 	}
+	stream.close();
 
 #ifdef _WIN32
 	if (remove(permchannelsconf.c_str()))
@@ -109,7 +78,7 @@ static bool WriteDatabase()
 	}
 #endif
 	// Use rename to move temporary to new db - this is guarenteed not to fuck up, even in case of a crash.
-	if (rename(tempname.c_str(), permchannelsconf.c_str()) < 0)
+	if (rename(permchannelsnewconf.c_str(), permchannelsconf.c_str()) < 0)
 	{
 		ServerInstance->Logs->Log("m_permchannels", LOG_DEFAULT, "permchannels: Cannot move new to old database! %s (%d)", strerror(errno), errno);
 		ServerInstance->SNO->WriteToSnoMask('a', "database: cannot replace old with new db: %s (%d)", strerror(errno), errno);
@@ -118,7 +87,6 @@ static bool WriteDatabase()
 
 	return true;
 }
-
 
 
 /** Handles the +P channel mode

win/inspircd_win32wrapper.h
@@ -101,6 +101,9 @@ __inline void sleep(int seconds) { Sleep(seconds * 1000); }
 #define popen _popen
 #define pclose _pclose
 
+/* _access */
+#define access _access
+
 /* IPV4 only convert string to address struct */
 __inline int inet_aton(const char *cp, struct in_addr *addr)
 { 

commit 5be578ce5e84c3d71bf2a1ac97fe6793bb124177

Fix xline reasons being truncated in m_xline_db.
This error was introduced in bbeb5ea3.

src/modules/m_xline_db.cpp
@@ -120,7 +120,7 @@ class ModuleXLineDB : public Module
 				XLine* line = i->second;
 				stream << "LINE " << line->type << " " << line->Displayable() << " "
 					<< ServerInstance->Config->ServerName << " " << line->set_time << " "
-					<< line->duration << " " << line->reason << std::endl;
+					<< line->duration << " :" << line->reason << std::endl;
 			}
 		}
 