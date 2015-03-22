4b3e466c8027989fc3b453bdd5a55b2ed42e9d43
Peter Powell <petpow@saberuk.com>
2015-03-22 01:52:59 +0000

    unit-cc: Remove workaround which existed for 2.0.x only.

--- make/unit-cc.pl
+++ make/unit-cc.pl
@@ -35,21 +35,7 @@ my $type = shift;
 my $out = shift;
 my $verbose = ($type =~ s/-v$//);
 
-## BEGIN HACK: REMOVE IN 2.2!
-sub read_config_cache {
-	my %cfg = ();
-	open(CACHE, '../.config.cache') or return %cfg;
-	while (my $line = <CACHE>) {
-		next if $line =~ /^\s*($|\#)/;
-		my ($key, $value) = ($line =~ /^(\S+)="(.*)"$/);
-		$cfg{$key} = $value;
-	}
-	close(CACHE);
-	return %cfg;
-}
-
-our %config = read_config_cache();
-## END HACK
+our %config = read_configure_cache();
 
 if ($type eq 'gen-ld') {
 	do_static_find(@ARGV);
