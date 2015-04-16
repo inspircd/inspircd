9f69e159adcc0892f0bdef16ac4617630737b156
Attila Molnar <attilamolnar@hush.com>
2015-04-16 23:00:56 +0200

    m_dccallow Validate tokens before use

--- src/modules/m_dccallow.cpp
+++ src/modules/m_dccallow.cpp
@@ -339,6 +339,9 @@ class ModuleDCCAllow : public Module
 					while (ss >> buf)
 						tokens.push_back(buf);
 
+					if (tokens.size() < 2)
+						return MOD_RES_PASSTHRU;
+
 					irc::string type = tokens[1].c_str();
 
 					ConfigTag* conftag = ServerInstance->Config->ConfValue("dccallow");
@@ -346,6 +349,9 @@ class ModuleDCCAllow : public Module
 
 					if (type == "SEND")
 					{
+						if (tokens.size() < 3)
+							return MOD_RES_PASSTHRU;
+
 						std::string defaultaction = conftag->getString("action");
 						std::string filename = tokens[2];
 
