38e4e2e34eb7338c1fb3d26852976b2bd7d532b1
Antonio Costa <bigua@brasirc.com.br>
2015-07-23 19:09:15 -0300

    Update m_hideoper.cpp
    I've change at code the ident@host, to type of IRCop, also it's check and show  if the ircop's still have a away msg ! 
    I did remove the use of User *i = oper; using just the iterator with pointers properly.

--- src/modules/m_hideoper.cpp
+++ src/modules/m_hideoper.cpp
@@ -1,6 +1,7 @@
 /*
  * InspIRCd -- Internet Relay Chat Daemon
  *
+ *   Copyright (C) 2015 Antonio Costa <bigua@brasirc.com.br>
  *   Copyright (C) 2007 Dennis Friis <peavey@inspircd.org>
  *   Copyright (C) 2007 Robin Burchell <robin+git@viroteck.net>
  *   Copyright (C) 2006 Craig Edwards <craigedwards@brainbox.cc>
@@ -100,18 +101,27 @@ class ModuleHideOper : public Module
 		if (symbol != 'P')
 			return MOD_RES_PASSTHRU;
 
-		unsigned int count = 0;
-		for (std::list<User*>::const_iterator i = ServerInstance->Users->all_opers.begin(); i != ServerInstance->Users->all_opers.end(); ++i)
+		unsigned int count = 0, count_away = 0;
+		for (std::list<User*>::const_iterator oper = ServerInstance->Users->all_opers.begin(); oper != ServerInstance->Users->all_opers.end(); ++oper)
 		{
-			User* oper = *i;
-			if (!ServerInstance->ULine(oper->server) && (IS_OPER(user) || !oper->IsModeSet('H')))
+			if (!ServerInstance->ULine((*oper)->server) && (IS_OPER(user) || !(*oper)->IsModeSet('H')))
 			{
-				results.push_back(ServerInstance->Config->ServerName+" 249 " + user->nick + " :" + oper->nick + " (" + oper->ident + "@" + oper->dhost + ") Idle: " +
-						(IS_LOCAL(oper) ? ConvToStr(ServerInstance->Time() - oper->idle_lastmsg) + " secs" : "unavailable"));
+				if(!(*oper)->awaymsg.empty())
+				{
+					count_away++;
+					results.push_back(ServerInstance->Config->ServerName+" 249 " + user->nick + " :\2" + (*oper)->nick +"\2 -  " + std::string((*oper)->oper->NameStr()) +  " - Idle: " +
+							(IS_LOCAL(*oper) ? ConvToStr(ServerInstance->Time() - (*oper)->idle_lastmsg) + " secs" : "unavailable" ) + " - (\2Currently marked away\2) ");
+				}
+				
+				else
+				{
+					results.push_back(ServerInstance->Config->ServerName+" 249 " + user->nick + " :\2" + (*oper)->nick + "\2 - " + std::string((*oper)->oper->NameStr()) +  " - Idle: " +
+					(IS_LOCAL(*oper) ? ConvToStr(ServerInstance->Time() - (*oper)->idle_lastmsg) + " secs" : "unavailable" ) + " - (\2Avaliable for help\2) " );
+				}
 				count++;
 			}
 		}
-		results.push_back(ServerInstance->Config->ServerName+" 249 "+user->nick+" :"+ConvToStr(count)+" OPER(s)");
+		results.push_back(ServerInstance->Config->ServerName+" 249 "+user->nick+" :Total: \2"+ConvToStr(count)+"\2 - OPER(s) and \2" +ConvToStr(count_away) + "\2 - AWAY(s)");
 
 		return MOD_RES_DENY;
 	}
