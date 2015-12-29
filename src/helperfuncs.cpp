b8c3f13887711c5d260b66e1bdedfebd7b8d1626
Attila Molnar <attilamolnar@hush.com>
2015-12-29 12:40:20 +0100

    Fix InspIRCd::StripColor() stripping characters that shouldn't be stripped

--- src/helperfuncs.cpp
+++ src/helperfuncs.cpp
@@ -128,7 +128,7 @@ void InspIRCd::StripColor(std::string &sentence)
 			seq = 0;
 
 		// Strip all control codes too except \001 for CTCP
-		if (seq || ((*i < 32) && (*i != 1)))
+		if (seq || ((*i >= 0) && (*i < 32) && (*i != 1)))
 			i = sentence.erase(i);
 		else
 			++i;
