ccc99fa8f4766d60e87bb0ac58be2ce8e77e34ef
Attila Molnar <attilamolnar@hush.com>
2016-03-29 16:23:39 +0200

    m_spanningtree Add class SpanningTree::RemoteUser

---
+++ src/modules/m_spanningtree/remoteuser.h
@@ -0,0 +1,31 @@
+/*
+ * InspIRCd -- Internet Relay Chat Daemon
+ *
+ *   Copyright (C) 2016 Attila Molnar <attilamolnar@hush.com>
+ *
+ * This file is part of InspIRCd.  InspIRCd is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#pragma once
+
+namespace SpanningTree
+{
+	class RemoteUser;
+}
+
+class SpanningTree::RemoteUser : public ::RemoteUser
+{
+ public:
+	RemoteUser(const std::string& uid, Server* srv);
+};
