f9fd78c01623514a060c607534fc52cb73140200
Adam <Adam@anope.org>
2016-08-15 19:25:54 -0400

    core_hostname_lookup: find answer record of the correct type instead of assuming it is first

--- src/coremods/core_hostname_lookup.cpp
+++ src/coremods/core_hostname_lookup.cpp
@@ -1,7 +1,7 @@
 /*
  * InspIRCd -- Internet Relay Chat Daemon
  *
- *   Copyright (C) 2013 Adam <Adam@anope.org>
+ *   Copyright (C) 2013-2016 Adam <Adam@anope.org>
  *
  * This file is part of InspIRCd.  InspIRCd is free software: you can
  * redistribute it and/or modify it under the terms of the GNU General Public
@@ -37,6 +37,21 @@ class UserResolver : public DNS::Request
 	 */
 	const bool fwd;
 
+	const DNS::ResourceRecord* FindAnswerOfType(const DNS::Query* response, DNS::QueryType qtype)
+	{
+		for (std::vector<DNS::ResourceRecord>::const_iterator it = response->answers.begin(); it != response->answers.end(); ++it)
+		{
+			const DNS::ResourceRecord& rr = *it;
+
+			if (rr.type == qtype)
+			{
+				return &rr;
+			}
+		}
+
+		return NULL;
+	}
+
  public:
 	/** Create a resolver.
 	 * @param mgr DNS Manager
@@ -65,25 +80,30 @@ class UserResolver : public DNS::Request
 			return;
 		}
 
-		const DNS::ResourceRecord& ans_record = r->answers[0];
+		const DNS::ResourceRecord* ans_record = FindAnswerOfType(r, this->type);
+		if (ans_record == NULL)
+		{
+			ServerInstance->Logs->Log(MODNAME, LOG_DEBUG, "DNS result for %s: no record of type %d", uuid.c_str(), this->type);
+			return;
+		}
 
-		ServerInstance->Logs->Log(MODNAME, LOG_DEBUG, "DNS result for %s: '%s' -> '%s'", uuid.c_str(), ans_record.name.c_str(), ans_record.rdata.c_str());
+		ServerInstance->Logs->Log(MODNAME, LOG_DEBUG, "DNS result for %s: '%s' -> '%s'", uuid.c_str(), ans_record->name.c_str(), ans_record->rdata.c_str());
 
 		if (!fwd)
 		{
 			// first half of resolution is done. We now need to verify that the host matches.
-			ph->set(bound_user, ans_record.rdata);
+			ph->set(bound_user, ans_record->rdata);
 
 			UserResolver* res_forward;
 			if (bound_user->client_sa.sa.sa_family == AF_INET6)
 			{
 				/* IPV6 forward lookup */
-				res_forward = new UserResolver(this->manager, this->creator, bound_user, ans_record.rdata, DNS::QUERY_AAAA);
+				res_forward = new UserResolver(this->manager, this->creator, bound_user, ans_record->rdata, DNS::QUERY_AAAA);
 			}
 			else
 			{
 				/* IPV4 lookup */
-				res_forward = new UserResolver(this->manager, this->creator, bound_user, ans_record.rdata, DNS::QUERY_A);
+				res_forward = new UserResolver(this->manager, this->creator, bound_user, ans_record->rdata, DNS::QUERY_A);
 			}
 			try
 			{
@@ -107,7 +127,7 @@ class UserResolver : public DNS::Request
 			if (user_ip->sa.sa_family == AF_INET6)
 			{
 				struct in6_addr res_bin;
-				if (inet_pton(AF_INET6, ans_record.rdata.c_str(), &res_bin))
+				if (inet_pton(AF_INET6, ans_record->rdata.c_str(), &res_bin))
 				{
 					rev_match = !memcmp(&user_ip->in6.sin6_addr, &res_bin, sizeof(res_bin));
 				}
@@ -115,7 +135,7 @@ class UserResolver : public DNS::Request
 			else
 			{
 				struct in_addr res_bin;
-				if (inet_pton(AF_INET, ans_record.rdata.c_str(), &res_bin))
+				if (inet_pton(AF_INET, ans_record->rdata.c_str(), &res_bin))
 				{
 					rev_match = !memcmp(&user_ip->in4.sin_addr, &res_bin, sizeof(res_bin));
 				}
