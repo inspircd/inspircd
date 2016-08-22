76710ee4c6193c2c0f702c67ccb6ec68f0a1f315
Attila Molnar <attilamolnar@hush.com>
2016-08-22 16:39:08 +0200

    m_timedbans Remove one irc::string usage

--- src/modules/m_timedbans.cpp
+++ src/modules/m_timedbans.cpp
@@ -148,12 +148,13 @@ class BanWatcher : public ModeWatcher
 			return;
 
 		irc::string listitem = banmask.c_str();
-		irc::string thischan = chan->name.c_str();
 		for (timedbans::iterator i = TimedBanList.begin(); i != TimedBanList.end(); ++i)
 		{
+			if (i->chan != chan)
+				continue;
+
 			irc::string target = i->mask.c_str();
-			irc::string tchan = i->channel.c_str();
-			if ((listitem == target) && (tchan == thischan))
+			if (listitem == target)
 			{
 				TimedBanList.erase(i);
 				break;
