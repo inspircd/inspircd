a22f9e6b4688f5435d23a3d91bfc6574e74c7113
Attila Molnar <attilamolnar@hush.com>
2016-08-23 19:51:06 +0200

    Remove broken mssql module
    Issue #1006

--- extras/m_sqloper.mssql.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[ircd_opers] (
-  [id] int IDENTITY(1, 1) NOT NULL,
-  [username] varchar(255) NULL,
-  [password] varchar(255) NULL,
-  [hostname] varchar(255) NULL,
-  [type] varchar(255) NULL,
-  [active] bit NOT NULL DEFAULT 1,
-  PRIMARY KEY CLUSTERED ([id])
-)
