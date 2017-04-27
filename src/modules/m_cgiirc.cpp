78b691e113e7e48fca0eb918591a17437fa0b96d
Peter Powell <petpow@saberuk.com>
2017-04-27 02:52:51 +0100

    Fix WEBIRC not rejecting invalid IP addresses.

--- src/modules/m_cgiirc.cpp
+++ src/modules/m_cgiirc.cpp
@@ -73,6 +73,7 @@ class CommandWebirc : public Command
 		  realhost("cgiirc_realhost", Creator), realip("cgiirc_realip", Creator),
 		  webirc_hostname("cgiirc_webirc_hostname", Creator), webirc_ip("cgiirc_webirc_ip", Creator)
 		{
+			allow_empty_last_param = false;
 			works_before_reg = true;
 			this->syntax = "password client hostname ip";
 		}
@@ -80,6 +81,13 @@ class CommandWebirc : public Command
 		{
 			if(user->registered == REG_ALL)
 				return CMD_FAILURE;
+
+			irc::sockets::sockaddrs ipaddr;
+			if (!irc::sockets::aptosa(parameters[3], 0, ipaddr))
+			{
+				ServerInstance->SNO->WriteGlobalSno('a', "Connecting user %s tried to use WEBIRC but gave an invalid IP address.", user->GetFullRealHost().c_str());
+				return CMD_FAILURE;
+			}
 
 			for(CGIHostlist::iterator iter = Hosts.begin(); iter != Hosts.end(); iter++)
 			{
