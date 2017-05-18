a92b6b7af463135ed5f84f0ed640b9416db684ef
Adam <Adam@anope.org>
2017-05-17 20:10:51 -0400

    m_override: fix parentheses compiler warning

--- src/modules/m_override.cpp
+++ src/modules/m_override.cpp
@@ -102,7 +102,7 @@ class ModuleOverride : public Module
 		{
 			// If the kicker's status is less than the target's,			or	the kicker's status is less than or equal to voice
 			if ((memb->chan->GetPrefixValue(source) < memb->getRank()) || (memb->chan->GetPrefixValue(source) <= VOICE_VALUE) ||
-			    (memb->chan->GetPrefixValue(source) == HALFOP_VALUE) && (memb->getRank() == HALFOP_VALUE))
+			    (memb->chan->GetPrefixValue(source) == HALFOP_VALUE && memb->getRank() == HALFOP_VALUE))
 			{
 				ServerInstance->SNO->WriteGlobalSno('v',source->nick+" used oper override to kick "+memb->user->nick+" on "+memb->chan->name+" ("+reason+")");
 				return MOD_RES_ALLOW;
