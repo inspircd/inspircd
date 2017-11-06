inspircd/inspircd

commit c202dea024542b9c6c6b771bb9a3a081d9eacdc5

Replace OnRehash() with ReadConfig() that is called on boot, on module load and on rehash
This eliminates the need for calling OnRehash() in init()

src/configreader.cpp
@@ -837,7 +837,12 @@ void ConfigReaderThread::Finish()
 		static_cast<ListModeBase*>(*ban)->DoRehash();
 		Config->ApplyDisabledCommands(Config->DisabledCommands);
 		User* user = ServerInstance->FindNick(TheUserUID);
-		FOREACH_MOD(OnRehash, (user));
+
+		ConfigStatus status(user);
+		const ModuleManager::ModuleMap& mods = ServerInstance->Modules->GetModules();
+		for (ModuleManager::ModuleMap::const_iterator i = mods.begin(); i != mods.end(); ++i)
+			i->second->ReadConfig(status);
+
 		ServerInstance->ISupport.Build();
 
 		ServerInstance->Logs->CloseLogs();

commit 2bce37fc7dc10ad719cddd5094845e57c93ff095

Fix an unhandled exception crash when rehashing modules.
Previously we used FOREACH_MOD to call OnRehash which handled any
thrown exceptions. When we switched to ReadConfig this stopped
being the case.

This bug was introduced in c202dea024.

src/configreader.cpp
@@ -820,7 +820,17 @@ void ConfigReaderThread::Finish()
 		ConfigStatus status(user);
 		const ModuleManager::ModuleMap& mods = ServerInstance->Modules->GetModules();
 		for (ModuleManager::ModuleMap::const_iterator i = mods.begin(); i != mods.end(); ++i)
-			i->second->ReadConfig(status);
+		{
+			try
+			{
+				ServerInstance->Logs->Log("MODULE", LOG_DEBUG, "Rehashing " + i->first);
+				i->second->ReadConfig(status);
+			}
+			catch (CoreException& modex)
+			{
+				ServerInstance->Logs->Log("MODULE", LOG_DEFAULT, "Exception caught: " + modex.GetReason());
+			}
+		}
 
 		// The description of this server may have changed - update it for WHOIS etc.
 		ServerInstance->FakeClient->server->description = Config->ServerDesc;