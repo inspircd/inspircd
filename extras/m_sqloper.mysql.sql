4ec475ba10b65785277ed9dab5f99775289165b3
Dylan Frank <b00mx0r@aureus.pw>
2018-04-06 10:34:15 -0700

    Refactor m_sqloper to be a full opers.conf replacement (#983).
    m_sqloper now supports dynamic fields, works with m_sslinfo, and works with /stats o.

--- extras/m_sqloper.mysql.sql
+++ extras/m_sqloper.mysql.sql
@@ -1,9 +1,12 @@
 CREATE TABLE ircd_opers (
   id bigint(20) NOT NULL auto_increment,
-  username text,
-  password text,
-  hostname text,
-  type text,
+  name text NOT NULL,
+  password text NOT NULL,
+  hash text,
+  host text NOT NULL,
+  type text NOT NULL,
+  fingerprint text,
+  autologin tinyint(1) NOT NULL DEFAULT 0,
   active tinyint(1) NOT NULL DEFAULT 1,
   PRIMARY KEY  (id)
 ) ENGINE=MyISAM;
