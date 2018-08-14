16fa4621a06328a51103caba4af84149df80d697
A_D <A_D@snoonet.org>
2018-08-14 16:02:29 +0200

    made m_kicknorejoin respect /invite

--- src/modules/m_kicknorejoin.cpp
+++ src/modules/m_kicknorejoin.cpp
@@ -24,6 +24,7 @@
 
 
 #include "inspircd.h"
+#include "modules/invite.h"
 
 class KickRejoinData
 {
@@ -121,10 +122,12 @@ class KickRejoin : public ParamMode<KickRejoin, SimpleExtItem<KickRejoinData> >
 class ModuleKickNoRejoin : public Module
 {
 	KickRejoin kr;
+	Invite::API invapi;
 
 public:
 	ModuleKickNoRejoin()
 		: kr(this)
+		, invapi(this)
 	{
 	}
 
@@ -133,7 +136,7 @@ public:
 		if (chan)
 		{
 			const KickRejoinData* data = kr.ext.get(chan);
-			if ((data) && (!data->canjoin(user)))
+			if ((data) && !invapi->IsInvited(chan, user) && (!data->canjoin(user)))
 			{
 				user->WriteNumeric(ERR_DELAYREJOIN, chan, InspIRCd::Format("You must wait %u seconds after being kicked to rejoin (+J)", data->delay));
 				return MOD_RES_DENY;
