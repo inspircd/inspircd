inspircd/inspircd

commit 97a1d6429a735eb279496df010d04e3f42aa4e22

Make more config stuff case insensitive.

src/modules/extra/m_mysql.cpp
@@ -427,7 +427,7 @@ void ModuleSQL::ReadConfig(ConfigStatus& status)
 	ConfigTagList tags = ServerInstance->Config->ConfTags("database");
 	for(ConfigIter i = tags.first; i != tags.second; i++)
 	{
-		if (i->second->getString("module", "mysql") != "mysql")
+		if (!stdalgo::string::equalsci(i->second->getString("provider"), "mysql"))
 			continue;
 		std::string id = i->second->getString("id");
 		ConnMap::iterator curr = connections.find(id);

src/modules/extra/m_pgsql.cpp
@@ -546,7 +546,7 @@ class ModulePgSQL : public Module
 		ConfigTagList tags = ServerInstance->Config->ConfTags("database");
 		for(ConfigIter i = tags.first; i != tags.second; i++)
 		{
-			if (i->second->getString("module", "pgsql") != "pgsql")
+			if (!stdalgo::string::equalsci(i->second->getString("provider"), "pgsql"))
 				continue;
 			std::string id = i->second->getString("id");
 			ConnMap::iterator curr = connections.find(id);

src/modules/extra/m_sqlite3.cpp
@@ -254,7 +254,7 @@ class ModuleSQLite3 : public Module
 		ConfigTagList tags = ServerInstance->Config->ConfTags("database");
 		for(ConfigIter i = tags.first; i != tags.second; i++)
 		{
-			if (i->second->getString("module", "sqlite") != "sqlite")
+			if (!stdalgo::string::equalsci(i->second->getString("provider"), "sqlite"))
 				continue;
 			SQLConn* conn = new SQLConn(this, i->second);
 			conns.insert(std::make_pair(i->second->getString("id"), conn));

src/configparser.cpp
@@ -213,25 +213,25 @@ struct Parser
 			mandatory_tag.clear();
 		}
 
-		if (name == "include")
+		if (stdalgo::string::equalsci(name, "include"))
 		{
 			stack.DoInclude(tag, flags);
 		}
-		else if (name == "files")
+		else if (stdalgo::string::equalsci(name, "files"))
 		{
 			for(ConfigItems::iterator i = items->begin(); i != items->end(); i++)
 			{
 				stack.DoReadFile(i->first, i->second, flags, false);
 			}
 		}
-		else if (name == "execfiles")
+		else if (stdalgo::string::equalsci(name, "execfiles"))
 		{
 			for(ConfigItems::iterator i = items->begin(); i != items->end(); i++)
 			{
 				stack.DoReadFile(i->first, i->second, flags, true);
 			}
 		}
-		else if (name == "define")
+		else if (stdalgo::string::equalsci(name, "define"))
 		{
 			if (flags & FLAG_USE_COMPAT)
 				throw CoreException("<define> tags may only be used in XML-style config (add <config format=\"xml\">)");
@@ -241,12 +241,12 @@ struct Parser
 				throw CoreException("Variable definition must include variable name");
 			stack.vars[varname] = value;
 		}
-		else if (name == "config")
+		else if (stdalgo::string::equalsci(name, "config"))
 		{
 			std::string format = tag->getString("format");
-			if (format == "xml")
+			if (stdalgo::string::equalsci(format, "xml"))
 				flags &= ~FLAG_USE_COMPAT;
-			else if (format == "compat")
+			else if (stdalgo::string::equalsci(format, "compat"))
 				flags |= FLAG_USE_COMPAT;
 			else if (!format.empty())
 				throw CoreException("Unknown configuration format " + format);

src/listensocket.cpp
@@ -183,7 +183,7 @@ void ListenSocket::OnEventHandlerRead()
 	if (res == MOD_RES_PASSTHRU)
 	{
 		std::string type = bind_tag->getString("type", "clients");
-		if (type == "clients")
+		if (stdalgo::string::equalsci(type, "clients"))
 		{
 			ServerInstance->Users->AddUser(incomingSockfd, this, &client, &server);
 			res = MOD_RES_ALLOW;

src/logger.cpp
@@ -77,35 +77,35 @@ void LogManager::OpenFileLogs()
 	{
 		ConfigTag* tag = i->second;
 		std::string method = tag->getString("method");
-		if (method != "file")
+		if (!stdalgo::string::equalsci(method, "file"))
 		{
 			continue;
 		}
 		std::string type = tag->getString("type");
 		std::string level = tag->getString("level");
 		LogLevel loglevel = LOG_DEFAULT;
-		if (level == "rawio")
+		if (stdalgo::string::equalsci(level, "rawio"))
 		{
 			loglevel = LOG_RAWIO;
 			ServerInstance->Config->RawLog = true;
 		}
-		else if (level == "debug")
+		else if (stdalgo::string::equalsci(level, "debug"))
 		{
 			loglevel = LOG_DEBUG;
 		}
-		else if (level == "verbose")
+		else if (stdalgo::string::equalsci(level, "verbose"))
 		{
 			loglevel = LOG_VERBOSE;
 		}
-		else if (level == "default")
+		else if (stdalgo::string::equalsci(level, "default"))
 		{
 			loglevel = LOG_DEFAULT;
 		}
-		else if (level == "sparse")
+		else if (stdalgo::string::equalsci(level, "sparse"))
 		{
 			loglevel = LOG_SPARSE;
 		}
-		else if (level == "none")
+		else if (stdalgo::string::equalsci(level, "none"))
 		{
 			loglevel = LOG_NONE;
 		}

src/modules/extra/m_ldap.cpp
@@ -258,9 +258,9 @@ class LDAPService : public LDAPProvider, public SocketThread
 		, con(NULL), config(tag), last_connect(0)
 	{
 		std::string scope = config->getString("searchscope");
-		if (scope == "base")
+		if (stdalgo::string::equalsci(scope, "base"))
 			searchscope = LDAP_SCOPE_BASE;
-		else if (scope == "onelevel")
+		else if (stdalgo::string::equalsci(scope, "onelevel"))
 			searchscope = LDAP_SCOPE_ONELEVEL;
 		else
 			searchscope = LDAP_SCOPE_SUBTREE;
@@ -533,7 +533,7 @@ class ModuleLDAP : public Module
 		{
 			const reference<ConfigTag>& tag = i->second;
 
-			if (tag->getString("module") != "ldap")
+			if (!stdalgo::string::equalsci(tag->getString("module"), "ldap"))
 				continue;
 
 			std::string id = tag->getString("id");

src/modules/extra/m_regex_stdlib.cpp
@@ -74,19 +74,19 @@ public:
 		ConfigTag* Conf = ServerInstance->Config->ConfValue("stdregex");
 		std::string regextype = Conf->getString("type", "ecmascript");
 
-		if(regextype == "bre")
+		if (stdalgo::string::equalsci(regextype, "bre"))
 			ref.regextype = std::regex::basic;
-		else if(regextype == "ere")
+		else if (stdalgo::string::equalsci(regextype, "ere"))
 			ref.regextype = std::regex::extended;
-		else if(regextype == "awk")
+		else if (stdalgo::string::equalsci(regextype, "awk"))
 			ref.regextype = std::regex::awk;
-		else if(regextype == "grep")
+		else if (stdalgo::string::equalsci(regextype, "grep"))
 			ref.regextype = std::regex::grep;
-		else if(regextype == "egrep")
+		else if (stdalgo::string::equalsci(regextype, "egrep"))
 			ref.regextype = std::regex::egrep;
 		else
 		{
-			if(regextype != "ecmascript")
+			if (!stdalgo::string::equalsci(regextype, "ecmascript"))
 				ServerInstance->SNO->WriteToSnoMask('a', "WARNING: Non-existent regex engine '%s' specified. Falling back to ECMAScript.", regextype.c_str());
 			ref.regextype = std::regex::ECMAScript;
 		}

commit 36e701f4abfa6d8fc4f096be023a681a6b7cec2e

Fix SQL modules using "provider" in <database> instead of "module".
This fixes a regression introduced in 97a1d642.

src/modules/extra/m_mysql.cpp
@@ -427,7 +427,7 @@ void ModuleSQL::ReadConfig(ConfigStatus& status)
 	ConfigTagList tags = ServerInstance->Config->ConfTags("database");
 	for(ConfigIter i = tags.first; i != tags.second; i++)
 	{
-		if (!stdalgo::string::equalsci(i->second->getString("provider"), "mysql"))
+		if (!stdalgo::string::equalsci(i->second->getString("module"), "mysql"))
 			continue;
 		std::string id = i->second->getString("id");
 		ConnMap::iterator curr = connections.find(id);

src/modules/extra/m_pgsql.cpp
@@ -546,7 +546,7 @@ class ModulePgSQL : public Module
 		ConfigTagList tags = ServerInstance->Config->ConfTags("database");
 		for(ConfigIter i = tags.first; i != tags.second; i++)
 		{
-			if (!stdalgo::string::equalsci(i->second->getString("provider"), "pgsql"))
+			if (!stdalgo::string::equalsci(i->second->getString("module"), "pgsql"))
 				continue;
 			std::string id = i->second->getString("id");
 			ConnMap::iterator curr = connections.find(id);

src/modules/extra/m_sqlite3.cpp
@@ -254,7 +254,7 @@ class ModuleSQLite3 : public Module
 		ConfigTagList tags = ServerInstance->Config->ConfTags("database");
 		for(ConfigIter i = tags.first; i != tags.second; i++)
 		{
-			if (!stdalgo::string::equalsci(i->second->getString("provider"), "sqlite"))
+			if (!stdalgo::string::equalsci(i->second->getString("module"), "sqlite"))
 				continue;
 			SQLConn* conn = new SQLConn(this, i->second);
 			conns.insert(std::make_pair(i->second->getString("id"), conn));