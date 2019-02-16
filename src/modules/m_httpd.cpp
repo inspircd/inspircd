c5b57fc79ef9b4d9d2953b471d3cf30bd8edcd52
Peter Powell <petpow@saberuk.com>
2019-02-16 09:10:05 +0000

    Remove a "commas at end of enumerator lists on C++03" warning.

--- src/modules/m_httpd.cpp
+++ src/modules/m_httpd.cpp
@@ -28,17 +28,6 @@
 #include "iohook.h"
 #include "modules/httpd.h"
 
-// Fix warnings about the use of commas at end of enumerator lists on C++03.
-#if defined __clang__
-# pragma clang diagnostic ignored "-Wc++11-extensions"
-#elif defined __GNUC__
-# if (__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 8))
-#  pragma GCC diagnostic ignored "-Wpedantic"
-# else
-#  pragma GCC diagnostic ignored "-pedantic"
-# endif
-#endif
-
 // Fix warnings about shadowing in http_parser.
 #pragma GCC diagnostic ignored "-Wshadow"
 
