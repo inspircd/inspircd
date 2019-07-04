inspircd/inspircd

commit ca161e8637384af9ba7e437c1e45611560517fdf

CoreExport for CommandSave. fixes #1635

src/modules/m_spanningtree/commands.h
@@ -320,7 +320,7 @@ class SpanningTree::CommandPong : public ServerOnlyServerCommand<SpanningTree::C
 	RouteDescriptor GetRouting(User* user, const Params& parameters) CXX11_OVERRIDE { return ROUTE_UNICAST(parameters[0]); }
 };
 
-class CommandSave : public ServerCommand
+class CoreExport CommandSave : public ServerCommand
 {
  public:
 	/** Timestamp of the uuid nick of all users who collided and got their nick changed to uuid

commit 0d33974a75b0764864b316a481cfaf75937d3f57

Fix building on Windows.
This regression was introduced by #1659. This distinction is only
relevant on Windows so this should not affect non-Windows platforms.

src/modules/m_spanningtree/commands.h
@@ -320,7 +320,7 @@ class SpanningTree::CommandPong : public ServerOnlyServerCommand<SpanningTree::C
 	RouteDescriptor GetRouting(User* user, const Params& parameters) CXX11_OVERRIDE { return ROUTE_UNICAST(parameters[0]); }
 };
 
-class CoreExport CommandSave : public ServerCommand
+class DllExport CommandSave : public ServerCommand
 {
  public:
 	/** Timestamp of the uuid nick of all users who collided and got their nick changed to uuid