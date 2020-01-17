2e91d9b2e19a5dee8e5e0959bc9face260c22daf
Sadie Powell <sadie@witchery.services>
2020-01-17 12:50:42 +0000

    Migrate from Travis CI to GitHub Actions.
    This brings shiny new GitHub integrations as well as allowing us to
    bring back the macOS builds which were disabled because of the very
    broken Travis CI macOS infrastructure.

--- tools/travis-ci.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ev
-sudo apt-get update --assume-yes
-sudo apt-get install --assume-yes --no-install-recommends libgnutls-dev libldap2-dev libmaxminddb-dev libmbedtls-dev libmysqlclient-dev libpcre3-dev libpq-dev libre2-dev libsqlite3-dev libssl-dev libtre-dev
-TEST_BUILD_MODULES="m_geo_maxmind.cpp,m_ldap.cpp,m_mysql.cpp,m_pgsql.cpp,m_regex_pcre.cpp,m_regex_posix.cpp,m_regex_re2.cpp,m_regex_stdlib.cpp,m_regex_tre.cpp,m_sqlite3.cpp,m_ssl_gnutls.cpp,m_ssl_mbedtls.cpp,m_ssl_openssl.cpp,m_sslrehashsignal.cpp"
-./tools/test-build $CXX
