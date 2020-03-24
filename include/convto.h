d6549b03eaa0a4db48065d5ef5774c757dae41a4
Matt Schatz <genius3000@g3k.solutions>
2020-03-24 11:50:14 -0600

    Fix the signed-ness within ConvToNum char overloads.
    It should be signed int with signed char and vice-versa.
    Currently, anything over 127 as unsigned char would return 0.

--- include/convto.h
+++ include/convto.h
@@ -104,14 +104,14 @@ template<> inline char ConvToNum<char>(const std::string& in)
 {
 	// We specialise ConvToNum for char to avoid istringstream treating
 	// the input as a character literal.
-	uint16_t num = ConvToNum<uint16_t>(in);
-	return num <= UINT8_MAX ? num : 0;
+	int16_t num = ConvToNum<int16_t>(in);
+	return num >= INT8_MIN && num <= INT8_MAX ? num : 0;
 }
 
 template<> inline unsigned char ConvToNum<unsigned char>(const std::string& in)
 {
 	// We specialise ConvToNum for unsigned char to avoid istringstream
 	// treating the input as a character literal.
-	int16_t num = ConvToNum<int16_t>(in);
-	return num >= INT8_MIN && num <= INT8_MAX ? num : 0;
+	uint16_t num = ConvToNum<uint16_t>(in);
+	return num <= UINT8_MAX ? num : 0;
 }
