25052ccef3c47cc43af9b900a658e5c09e8bec2e
Sadie Powell <sadie@witchery.services>
2020-03-30 23:38:42 +0100

    Make showing the channel modes in /LIST configurable.

--- src/coremods/core_list.cpp
+++ src/coremods/core_list.cpp
@@ -47,8 +47,9 @@ class CommandList : public Command
 	}
 
  public:
-	/** Constructor for list.
-	 */
+	// Whether to show modes in the LIST response.
+	bool showmodes;
+
 	CommandList(Module* parent)
 		: Command(parent,"LIST", 0, 0)
 		, secretmode(creator, "secret")
@@ -185,10 +186,15 @@ CmdResult CommandList::Handle(User* user, const Params& parameters)
 				// Channel is private (+p) and user is outside/not privileged
 				user->WriteNumeric(RPL_LIST, '*', users, "");
 			}
+			else if (showmodes)
+			{
+				// Show the list response with the modes and topic.
+				user->WriteNumeric(RPL_LIST, chan->name, users, InspIRCd::Format("[+%s] %s", chan->ChanModes(n), chan->topic.c_str()));
+			}
 			else
 			{
-				/* User is in the channel/privileged, channel is not +s */
-				user->WriteNumeric(RPL_LIST, chan->name, users, InspIRCd::Format("[+%s] %s", chan->ChanModes(n), chan->topic.c_str()));
+				// Show the list response with just the modes.
+				user->WriteNumeric(RPL_LIST, chan->name, users, chan->topic);
 			}
 		}
 	}
@@ -206,6 +212,12 @@ class CoreModList : public Module
 	CoreModList()
 		: cmd(this)
 	{
+	}
+
+	void ReadConfig(ConfigStatus& status) CXX11_OVERRIDE
+	{
+		ConfigTag* tag = ServerInstance->Config->ConfValue("options");
+		cmd.showmodes = tag->getBool("modesinlist", true);
 	}
 
 	void On005Numeric(std::map<std::string, std::string>& tokens) CXX11_OVERRIDE
