e278879cfcd8fae9282c3a7d6d97e534a7ee3823
Matt Schatz <genius3000@g3k.solutions>
2020-04-12 22:59:22 -0600

    Allow 'tls' to be used in /REHASH.

--- src/modules/extra/m_ssl_gnutls.cpp
+++ src/modules/extra/m_ssl_gnutls.cpp
@@ -1369,7 +1369,7 @@ class ModuleSSLGnuTLS : public Module
 
 	void OnModuleRehash(User* user, const std::string &param) CXX11_OVERRIDE
 	{
-		if (!irc::equals(param, "ssl"))
+		if (!irc::equals(param, "tls") && !irc::equals(param, "ssl"))
 			return;
 
 		try
