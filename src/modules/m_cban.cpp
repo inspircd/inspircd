e73d011e68c577df72c2f0eae235c32cd4261870
InspIRCd Robot <noreply@inspircd.org>
2020-10-30 11:56:02 +0000

    Update copyright headers.

--- src/modules/m_cban.cpp
+++ src/modules/m_cban.cpp
@@ -1,6 +1,7 @@
 /*
  * InspIRCd -- Internet Relay Chat Daemon
  *
+ *   Copyright (C) 2020 Michael <michaelhazell@hotmail.com>
  *   Copyright (C) 2019 Matt Schatz <genius3000@g3k.solutions>
  *   Copyright (C) 2018 linuxdaemon <linuxdaemon.irc@gmail.com>
  *   Copyright (C) 2013, 2017-2018, 2020 Sadie Powell <sadie@witchery.services>
