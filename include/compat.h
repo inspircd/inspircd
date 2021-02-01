5b21a60a9d96827d6822c56b2dfdf08dbce5867f
Sadie Powell <sadie@witchery.services>
2021-02-01 17:00:19 +0000

    Fix using the TR1 headers on compilers that support C++17.

--- include/compat.h
+++ include/compat.h
@@ -24,7 +24,7 @@
  * Some implementations of the C++11 standard library are incomplete so we use
  * the implementation of the same types from C++ Technical Report 1 instead.
  */
-#if defined _LIBCPP_VERSION || defined _WIN32
+#if defined _LIBCPP_VERSION || defined _WIN32 || __cplusplus >= 201103L
 # define TR1NS std
 # include <array>
 # include <functional>
