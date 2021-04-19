a30729c571e2794ceeae78af06c420c81cf896fb
Sadie Powell <sadie@witchery.services>
2021-04-19 17:52:38 +0100

    Improve the message sent when a mode does not exist.

--- src/modules/m_disable.cpp
+++ src/modules/m_disable.cpp
@@ -171,8 +171,9 @@ class ModuleDisable : public Module
 		{
 			// The server administrator has specified that disabled modes should be
 			// treated as if they do not exist.
-			user->WriteNumeric(mh->GetModeType() == MODETYPE_CHANNEL ? ERR_UNKNOWNMODE : ERR_UNKNOWNSNOMASK,
-				mh->GetModeChar(), "is an unknown mode character");
+			int numeric = (mh->GetModeType() == MODETYPE_CHANNEL ? ERR_UNKNOWNMODE : ERR_UNKNOWNSNOMASK);
+			const char* typestr = (mh->GetModeType() == MODETYPE_CHANNEL ? "channel" : "user");
+			user->WriteNumeric(numeric, mh->GetModeChar(), InspIRCd::Format("is not a recognised %s mode.", typestr));
 			return MOD_RES_DENY;
 		}
 
