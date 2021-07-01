inspircd/inspircd

commit ba3dd9cedcca2f3cc6781f09410e3cf2cf696e43

Convert FOREACH_MOD_CUSTOM to a variadic function.

include/event.h
@@ -83,6 +83,10 @@ class Events::ModuleEventProvider
 		OnUnsubscribe(subscriber);
 	}
 
+	/** Run the given hook provided by a module. */
+	template<typename Class, typename... FunArgs, typename... FwdArgs>
+	inline void Call(void (Class::*function)(FunArgs...), FwdArgs&&... args) const;
+
 	/**
 	 * Run the given hook provided by a module until some module returns MOD_RES_ALLOW or MOD_RES_DENY.
 	 * If no module does that, result is set to MOD_RES_PASSTHRU.
@@ -184,27 +188,25 @@ inline bool Events::ModuleEventProvider::ElementComp::operator()(Events::ModuleE
 	return std::less<ModuleEventListener*>()(lhs, rhs);
 }
 
-/**
- * Run the given hook provided by a module
- *
- * FOREACH_MOD_CUSTOM(accountevprov, AccountEventListener, OnAccountChange, MOD_RESULT, (user, newaccount))
- */
-#define FOREACH_MOD_CUSTOM(prov, listenerclass, func, params) do { \
-	const ::Events::ModuleEventProvider::SubscriberList& _handlers = (prov).GetSubscribers(); \
-	for (::Events::ModuleEventProvider::SubscriberList::const_iterator _i = _handlers.begin(); _i != _handlers.end(); ++_i) \
-	{ \
-		listenerclass* _t = static_cast<listenerclass*>(*_i); \
-		const Module* _m = _t->GetModule(); \
-		if (_m && !_m->dying) \
-			_t->func params ; \
-	} \
-} while (0);
+template<typename Class, typename... FunArgs, typename... FwdArgs>
+inline void Events::ModuleEventProvider::Call(void (Class::*function)(FunArgs...), FwdArgs&&... args) const
+{
+	for (const auto& subscriber : subscribers)
+	{
+		const Module* mod = subscriber->GetModule();
+		if (!mod || mod->dying)
+			continue;
+
+		Class* klass = static_cast<Class*>(subscriber);
+		(klass->*function)(std::forward<FwdArgs>(args)...);
+	}
+}
 
 template<typename Class, typename... FunArgs, typename... FwdArgs>
 inline ModResult Events::ModuleEventProvider::FirstResult(ModResult (Class::*function)(FunArgs...), FwdArgs&&... args) const
 {
 	ModResult result;
-	for (ModuleEventListener* subscriber : subscribers)
+	for (const auto& subscriber : subscribers)
 	{
 		const Module* mod = subscriber->GetModule();
 		if (!mod || mod->dying)

include/modules/server.h
@@ -91,14 +91,14 @@ class ServerProtocol::MessageEventListener
 	 * @param name The name of the command which was sent.
 	 * @param tags The tags which will be sent with the message.
 	 */
-	virtual void OnBuildMessage(User* source, const char* name, ClientProtocol::TagMap& tags) { }
+	virtual void OnBuildUserMessage(User* source, const char* name, ClientProtocol::TagMap& tags) { }
 
 	/** Fired when a server message is being sent by a server.
 	 * @param source The server who sent the message.
 	 * @param name The name of the command which was sent.
 	 * @param tags The tags which will be sent with the message.
 	 */
-	virtual void OnBuildMessage(Server* source, const char* name, ClientProtocol::TagMap& tags) { }
+	virtual void OnBuildServerMessage(Server* source, const char* name, ClientProtocol::TagMap& tags) { }
 };
 
 class ServerProtocol::SyncEventListener

src/clientprotocol.cpp
@@ -64,7 +64,7 @@ const ClientProtocol::SerializedMessage& ClientProtocol::Serializer::SerializeFo
 	if (!msg.msginit_done)
 	{
 		msg.msginit_done = true;
-		FOREACH_MOD_CUSTOM(evprov, MessageTagProvider, OnPopulateTags, (msg));
+		evprov.Call(&MessageTagProvider::OnPopulateTags, msg);
 	}
 	return msg.GetSerialized(Message::SerializedInfo(this, MakeTagWhitelist(user, msg.GetTags())));
 }
@@ -89,7 +89,7 @@ void ClientProtocol::Event::GetMessagesForUser(LocalUser* user, MessageList& mes
 	if (!eventinit_done)
 	{
 		eventinit_done = true;
-		FOREACH_MOD_CUSTOM(*event, EventHook, OnEventInit, (*this));
+		event->Call(&EventHook::OnEventInit, *this);
 	}
 
 	// Most of the time there's only a single message but in rare cases there are more

src/coremods/core_info/isupport.cpp
@@ -90,7 +90,7 @@ void ISupportManager::Build()
 		{ "TOPICLEN",    ConvToStr(ServerInstance->Config->Limits.MaxTopic) },
 		{ "USERLEN",     ConvToStr(ServerInstance->Config->Limits.IdentMax) },
 	};
-	FOREACH_MOD_CUSTOM(isupportevprov, ISupport::EventListener, OnBuildISupport, (tokens));
+	isupportevprov.Call(&ISupport::EventListener::OnBuildISupport, tokens);
 
 	// Transform the map into a list of numerics ready to be sent to clients.
 	std::vector<Numeric::Numeric> numerics;

src/coremods/core_reloadmodule.cpp
@@ -518,7 +518,7 @@ void DataKeeper::Save(Module* currmod)
 	CreateModeList(MODETYPE_CHANNEL);
 	DoSaveChans();
 
-	FOREACH_MOD_CUSTOM(*reloadevprov, ReloadModule::EventListener, OnReloadModuleSave, (mod, this->moddata));
+	reloadevprov->Call(&ReloadModule::EventListener::OnReloadModuleSave, mod, this->moddata);
 
 	ServerInstance->Logs.Log(MODNAME, LOG_DEBUG, "Saved data about %lu users %lu chans %lu modules", (unsigned long)userdatalist.size(), (unsigned long)chandatalist.size(), (unsigned long)moddata.list.size());
 }

src/coremods/core_user/cmd_away.cpp
@@ -60,7 +60,7 @@ CmdResult CommandAway::Handle(User* user, const Params& parameters)
 		user->awaytime = ServerInstance->Time();
 		user->awaymsg.assign(message, 0, ServerInstance->Config->Limits.MaxAway);
 		user->WriteNumeric(RPL_NOWAWAY, "You have been marked as being away");
-		FOREACH_MOD_CUSTOM(awayevprov, Away::EventListener, OnUserAway, (user));
+		awayevprov.Call(&Away::EventListener::OnUserAway, user);
 	}
 	else
 	{
@@ -74,7 +74,7 @@ CmdResult CommandAway::Handle(User* user, const Params& parameters)
 		user->awaytime = 0;
 		user->awaymsg.clear();
 		user->WriteNumeric(RPL_UNAWAY, "You are no longer marked as being away");
-		FOREACH_MOD_CUSTOM(awayevprov, Away::EventListener, OnUserBack, (user));
+		awayevprov.Call(&Away::EventListener::OnUserBack, user);
 	}
 
 	return CmdResult::SUCCESS;

src/coremods/core_whois.cpp
@@ -265,7 +265,7 @@ void CommandWhois::DoWhois(LocalUser* user, User* dest, time_t signon, unsigned 
 		}
 	}
 
-	FOREACH_MOD_CUSTOM(evprov, Whois::EventListener, OnWhois, (whois));
+	evprov.Call(&Whois::EventListener::OnWhois, whois);
 
 	/*
 	 * We only send these if we've been provided them. That is, if hideserver is turned off, and user is local, or

src/modules/m_cap.cpp
@@ -180,7 +180,7 @@ class Cap::ManagerImpl : public Cap::Manager, public ReloadModule::EventListener
 		caps.insert(std::make_pair(cap->GetName(), cap));
 		ServerInstance->Modules.AddReferent("cap/" + cap->GetName(), cap);
 
-		FOREACH_MOD_CUSTOM(evprov, Cap::EventListener, OnCapAddDel, (cap, true));
+		evprov.Call(&Cap::EventListener::OnCapAddDel, cap, true);
 	}
 
 	void DelCap(Cap::Capability* cap) override
@@ -192,7 +192,7 @@ class Cap::ManagerImpl : public Cap::Manager, public ReloadModule::EventListener
 		ServerInstance->Logs.Log(MODNAME, LOG_DEBUG, "Unregistering cap %s", cap->GetName().c_str());
 
 		// Fire the event first so modules can still see who is using the cap which is being unregistered
-		FOREACH_MOD_CUSTOM(evprov, Cap::EventListener, OnCapAddDel, (cap, false));
+		evprov.Call(&Cap::EventListener::OnCapAddDel, cap, false);
 
 		// Turn off the cap for all users
 		const UserManager::LocalList& list = ServerInstance->Users.GetLocalUsers();
@@ -218,7 +218,7 @@ class Cap::ManagerImpl : public Cap::Manager, public ReloadModule::EventListener
 	void NotifyValueChange(Capability* cap) override
 	{
 		ServerInstance->Logs.Log(MODNAME, LOG_DEBUG, "Cap %s changed value", cap->GetName().c_str());
-		FOREACH_MOD_CUSTOM(evprov, Cap::EventListener, OnCapValueChange, (cap));
+		evprov.Call(&Cap::EventListener::OnCapValueChange, cap);
 	}
 
 	Protocol GetProtocol(LocalUser* user) const

commit d21e969e4d8c166e3179460e7aa8881087853f2b

Fix module events not being fired correctly.
Regression introduced in commit ba3dd9ce.

Closes #1912.

include/event.h
@@ -199,7 +199,7 @@ inline void Events::ModuleEventProvider::Call(void (Class::*function)(FunArgs...
 	if (GetModule() && GetModule()->dying)
 		return;
 
-	for (const auto& subscriber : subscribers)
+	for (const auto& subscriber : GetSubscribers())
 	{
 		const Module* mod = subscriber->GetModule();
 		if (!mod || mod->dying)
@@ -217,7 +217,7 @@ inline ModResult Events::ModuleEventProvider::FirstResult(ModResult (Class::*fun
 		return MOD_RES_PASSTHRU;
 
 	ModResult result;
-	for (const auto& subscriber : subscribers)
+	for (const auto& subscriber : GetSubscribers())
 	{
 		const Module* mod = subscriber->GetModule();
 		if (!mod || mod->dying)