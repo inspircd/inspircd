inspircd/inspircd

commit 7d84e4900fa8f4ef96e8cf4bb67b76be7902e840

Fix a ton of pedantic compiler warnings.

src/coremods/core_whois.cpp
@@ -315,7 +315,7 @@ CmdResult CommandWhois::HandleLocal(LocalUser* user, const Params& parameters)
 		LocalUser* localuser = IS_LOCAL(dest);
 		if (localuser && (ServerInstance->Config->HideServer.empty() || parameters.size() > 1))
 		{
-			idle = labs((long)((localuser->idle_lastmsg)-ServerInstance->Time()));
+			idle = std::max<unsigned long>(localuser->idle_lastmsg - ServerInstance->Time(), 0);
 			signon = dest->signon;
 		}
 

commit 87057baf24aea92896b27d2a43df42a177c879c0

Fix local idle times being incorrect.
Regression introduced in commit 7d84e490.

src/coremods/core_whois.cpp
@@ -305,7 +305,7 @@ CmdResult CommandWhois::HandleLocal(LocalUser* user, const Params& parameters)
 		LocalUser* localuser = IS_LOCAL(dest);
 		if (localuser && (ServerInstance->Config->HideServer.empty() || parameters.size() > 1))
 		{
-			idle = std::max<unsigned long>(localuser->idle_lastmsg - ServerInstance->Time(), 0);
+			idle = ServerInstance->Time() - localuser->idle_lastmsg;
 			signon = dest->signon;
 		}
 