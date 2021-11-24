80004246536f9a96fb184a41757d89bb0aacdaf2
Sadie Powell <sadie@witchery.services>
2021-11-24 12:28:36 +0000

    Squash a long form loop that was merged in from the insp3 branch.

--- src/modules/m_spanningtree/utils.cpp
+++ src/modules/m_spanningtree/utils.cpp
@@ -386,12 +386,8 @@ std::string SpanningTreeUtilities::BuildLinkString(uint16_t proto, Module* mod)
 void SpanningTreeUtilities::SendListLimits(Channel* chan, TreeSocket* sock)
 {
 	std::stringstream buffer;
-	const ModeParser::ListModeList& listmodes = ServerInstance->Modes.GetListModes();
-	for (ModeParser::ListModeList::const_iterator i = listmodes.begin(); i != listmodes.end(); ++i)
-	{
-		ListModeBase* lm = *i;
+	for (const auto& lm : ServerInstance->Modes.GetListModes())
 		buffer << lm->GetModeChar() << " " << lm->GetLimit(chan) << " ";
-	}
 
 	std::string bufferstr = buffer.str();
 	if (bufferstr.empty())
