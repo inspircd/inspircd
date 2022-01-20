1373c45ccac3128633d3c6e6567693087080c0a7
Sadie Powell <sadie@witchery.services>
2022-01-20 14:51:22 +0000

    Fix parsing a LIST request with both a match and inverted match.
    e.g. /LIST *foo* !*bar*

--- src/coremods/core_list.cpp
+++ src/coremods/core_list.cpp
@@ -79,10 +79,10 @@ CmdResult CommandList::Handle(User* user, const Params& parameters)
 	time_t maxcreationtime = 0;
 
 	// M: Searching based on mask.
+	std::string match;
+
 	// N: Searching based on !mask.
-	bool match_name_topic = false;
-	bool match_inverted = false;
-	const char* match = NULL;
+	std::string notmatch;
 
 	// T: Searching based on topic time, via the "T<val" and "T>val" modifiers to
 	// search for a topic time that is lower or higher than val respectively.
@@ -124,19 +124,15 @@ CmdResult CommandList::Handle(User* user, const Params& parameters)
 			{
 				maxtopictime = ParseMinutes(constraint);
 			}
+			else if (constraint[0] == '!')
+			{
+				// Ensure that the user didn't just run "LIST !".
+				if (constraint.length() > 2)
+					notmatch = constraint.substr(1);
+			}
 			else
 			{
-				// If the glob is prefixed with ! it is inverted.
-				match = constraint.c_str();
-				if (match[0] == '!')
-				{
-					match_inverted = true;
-					match += 1;
-				}
-
-				// Ensure that the user didn't just run "LIST !".
-				if (match[0])
-					match_name_topic = true;
+				match = constraint;
 			}
 		}
 	}
@@ -165,18 +161,12 @@ CmdResult CommandList::Handle(User* user, const Params& parameters)
 			continue;
 
 		// Attempt to match a glob pattern.
-		if (match_name_topic)
-		{
-			bool matches = InspIRCd::Match(chan->name, match) || InspIRCd::Match(chan->topic, match);
+		if (!match.empty() && !InspIRCd::Match(chan->name, match) && !InspIRCd::Match(chan->topic, match))
+			continue;
 
-			// The user specified an match that we did not match.
-			if (!matches && !match_inverted)
-				continue;
-
-			// The user specified an inverted match that we did match.
-			if (matches && match_inverted)
-				continue;
-		}
+		// Attempt to match an inverted glob pattern.
+		if (!notmatch.empty() && (InspIRCd::Match(chan->name, notmatch) || InspIRCd::Match(chan->topic, notmatch)))
+			continue;
 
 		// if the channel is not private/secret, OR the user is on the channel anyway
 		bool n = (has_privs || chan->HasUser(user));
