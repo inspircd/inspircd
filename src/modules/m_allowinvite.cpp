c48c5f374b428b845849b28608bb6cc637636066
Sadie Powell <sadie@witchery.services>
2022-04-12 00:59:11 +0100

    Use ERR_RESTRICTED when an action can't be done even with privileges.

--- src/modules/m_allowinvite.cpp
+++ src/modules/m_allowinvite.cpp
@@ -48,7 +48,7 @@ class ModuleAllowInvite : public Module
 			if (res == MOD_RES_DENY)
 			{
 				// Matching extban, explicitly deny /invite
-				user->WriteNumeric(ERR_CHANOPRIVSNEEDED, channel->name, "You are banned from using INVITE");
+				user->WriteNumeric(ERR_RESTRICTED, channel->name, "You are banned from using INVITE");
 				return res;
 			}
 			if (channel->IsModeSet(ni) || res == MOD_RES_ALLOW)
