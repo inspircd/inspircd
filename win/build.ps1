222a2d8bb84faf340303504530472d3c45c2a009
Sadie Powell <sadie@witchery.services>
2023-04-27 10:42:37 +0100

    When building the PACKAGE project we don't need to specify a target.

--- win/build.ps1
+++ win/build.ps1
@@ -1,4 +1,4 @@
 cd $PSScriptRoot/build
 conan install --build=missing ..
 cmake .. -A x64 -D CMAKE_BUILD_TYPE=Release
-msbuild PACKAGE.vcxproj /P:Configuration=Release /P:Platform=x64 /T:PACKAGE /VERBOSITY:MINIMAL
+msbuild PACKAGE.vcxproj /P:Configuration=Release /P:Platform=x64 /VERBOSITY:MINIMAL
