inspircd/inspircd

commit eba5cb9dc973062e68c2629a5fbcb1adab19fa3e

Fix cert expiration for dates after year 2049

src/modules/extra/m_ssl_openssl.cpp
@@ -704,11 +704,11 @@ class OpenSSLIOHook : public SSLIOHook
 		certinfo->activation = GetTime(X509_getm_notBefore(cert));
 		certinfo->expiration = GetTime(X509_getm_notAfter(cert));
 
-		int activated = ASN1_UTCTIME_cmp_time_t(X509_getm_notBefore(cert), ServerInstance->Time());
+		int activated = ASN1_TIME_cmp_time_t(X509_getm_notBefore(cert), ServerInstance->Time());
 		if (activated != -1 && activated != 0)
 			certinfo->error = "Certificate not activated";
 
-		int expired = ASN1_UTCTIME_cmp_time_t(X509_getm_notAfter(cert), ServerInstance->Time());
+		int expired = ASN1_TIME_cmp_time_t(X509_getm_notAfter(cert), ServerInstance->Time());
 		if (expired != 0 && expired != 1)
 			certinfo->error = "Certificate has expired";
 

commit e607175dfa8e53d35964e5f650babb597c1c72b5

Fix ssl_openssl on OpenSSL versions older than 1.1.1.
Fixes a regression introduced in commit eba5cb9dc9.

src/modules/extra/m_ssl_openssl.cpp
@@ -704,6 +704,10 @@ class OpenSSLIOHook : public SSLIOHook
 		certinfo->activation = GetTime(X509_getm_notBefore(cert));
 		certinfo->expiration = GetTime(X509_getm_notAfter(cert));
 
+#if OPENSSL_VERSION_NUMBER < 0x10101000L
+# define ASN1_TIME_cmp_time_t ASN1_UTCTIME_cmp_time_t
+#endif
+
 		int activated = ASN1_TIME_cmp_time_t(X509_getm_notBefore(cert), ServerInstance->Time());
 		if (activated != -1 && activated != 0)
 			certinfo->error = "Certificate not activated";