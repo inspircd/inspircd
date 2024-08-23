448c3342d92652328ec6c34489406ae5b390054a
Sadie Powell <sadie@witchery.services>
2024-08-23 18:38:03 +0100

    Allow building log_json against yyjson.
    This is the default if available. RapidJSON has not had a release
    in years and has known security vulnerabilities so we should really
    not be using it.

--- src/modules/extra/m_log_json.cpp
+++ src/modules/extra/m_log_json.cpp
@@ -16,20 +16,32 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/// $CompilerFlags: find_compiler_flags("RapidJSON")
+/// $CompilerFlags: require_library("yyjson") find_compiler_flags("yyjson") -DHAS_YYJSON
+/// $CompilerFlags: require_library("!yyjson") find_compiler_flags("RapidJSON") warning("Building log_json against RapidJSON is deprecated. Please build against yyjson instead.")
 
-/// $PackageInfo: require_system("alpine") rapidjson-dev pkgconf
-/// $PackageInfo: require_system("arch") rapidjson pkgconf
-/// $PackageInfo: require_system("darwin") rapidjson pkg-config
+/// $LinkerFlags: require_library("yyjson") find_linker_flags("yyjson")
+
+/// $PackageInfo: require_system("alpine") pkgconf yyjson-dev
+/// $PackageInfo: require_system("arch") pkgconf yyjson
+/// $PackageInfo: require_system("darwin") pkg-config yyjson
 /// $PackageInfo: require_system("debian~") rapidjson-dev pkg-config
 
+#ifdef _WIN32
+# define HAS_YYJSON
+# pragma comment(lib, "yyjson.lib")
+#endif
 
-#include <rapidjson/ostreamwrapper.h>
-#include <rapidjson/writer.h>
+#ifdef HAS_YYJSON
+# include <yyjson.h>
+#else
+# include <rapidjson/ostreamwrapper.h>
+# include <rapidjson/writer.h>
+#endif
 
 #include "inspircd.h"
 #include "timeutils.h"
 
+#include <iostream>
 class JSONMethod final
 	: public Log::Method
 	, public Timer
@@ -71,8 +83,10 @@ public:
 			fclose(file);
 	}
 
+#ifndef HAS_YYJSON
 	// RapidJSON API: We implement our own flushing in OnLog.
 	void Flush() { }
+#endif
 
 	void OnLog(time_t time, Log::Level level, const std::string& type, const std::string& message) override
 	{
@@ -84,6 +98,28 @@ public:
 			timestr = Time::ToString(prevtime, "%Y-%m-%dT%H:%M:%S%z");
 		}
 
+		const auto* levelstr = Log::LevelToString(level);
+
+#ifdef HAS_YYJSON
+		auto* doc = yyjson_mut_doc_new(nullptr);
+
+		auto* root = yyjson_mut_obj(doc);
+		yyjson_mut_doc_set_root(doc, root);
+
+		auto error = false;
+		error |= yyjson_mut_obj_add_strn(doc, root, "time", timestr.c_str(), timestr.length());
+		error |= yyjson_mut_obj_add_strn(doc, root, "type", type.c_str(), type.length());
+		error |= yyjson_mut_obj_add_strn(doc, root, "level", levelstr, strlen(levelstr));
+		error |= yyjson_mut_obj_add_strn(doc, root, "message", message.c_str(), message.length());
+
+		yyjson_write_err errmsg;
+		error |= yyjson_mut_write_fp(file, doc, YYJSON_WRITE_ALLOW_INVALID_UNICODE | YYJSON_WRITE_NEWLINE_AT_END, nullptr, &errmsg);
+
+		yyjson_mut_doc_free(doc);
+
+		if (error)
+			throw CoreException(INSP_FORMAT("Unable to generate JSON for {}: {}", name, errmsg.msg ? errmsg.msg : "unknown error"));
+#else
 		rapidjson::Writer writer(*this);
 		writer.StartObject();
 		{
@@ -94,18 +130,18 @@ public:
 			writer.String(type.c_str(), static_cast<rapidjson::SizeType>(type.size()));
 
 			writer.Key("level", 5);
-			const std::string levelstr = Log::LevelToString(level);
-			writer.String(levelstr.c_str(), static_cast<rapidjson::SizeType>(levelstr.size()));
+			writer.String(levelstr, static_cast<rapidjson::SizeType>(strlen(levelstr)));
 
 			writer.Key("message", 7);
 			writer.Key(message.c_str(), static_cast<rapidjson::SizeType>(message.size()));
 		}
 		writer.EndObject();
 
-#ifdef _WIN32
-	fputs("\r\n", file);
-#else
-	fputs("\n", file);
+# ifdef _WIN32
+		fputs("\r\n", file);
+# else
+		fputs("\n", file);
+# endif
 #endif
 
 		if (!(++lines % flush))
@@ -115,11 +151,13 @@ public:
 			throw CoreException(INSP_FORMAT("Unable to write to {}: {}", name, strerror(errno)));
 	}
 
+#ifndef HAS_YYJSON
 	// RapidJSON API: Write a character to the file.
 	void Put(Ch c)
 	{
 		fputc(c, file);
 	}
+#endif
 
 	bool Tick() override
 	{
@@ -194,8 +232,14 @@ public:
 
 	void init() override
 	{
+#ifdef HAS_YYJSON
+		const auto yyversion = yyjson_version();
+		ServerInstance->Logs.Normal(MODNAME, "Module was compiled against yyjson version {} and is running against version {}.{}.{}",
+			YYJSON_VERSION_STRING, (yyversion >> 16) & 0xFF, (yyversion >> 8) & 0xFF, yyversion & 0xFF);
+#else
 		ServerInstance->Logs.Normal(MODNAME, "Module was compiled against RapidJSON version {}",
 			RAPIDJSON_VERSION_STRING);
+#endif
 	}
 };
 
