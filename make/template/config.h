95ebf4044302e647c08c37f49c14efc8073b2fe8
Sadie Powell <sadie@witchery.services>
2026-04-08 08:13:16 +0100

    Use line comments in the config header.
    This isn't parsed by Doxygen so there's no point using /** */
    comments.

--- make/template/config.h
+++ make/template/config.h
@@ -19,52 +19,52 @@
 
 #pragma once
 
-/** The branch version that is shown to unprivileged users. */
+// The branch version that is shown to unprivileged users.
 #define INSPIRCD_BRANCH "InspIRCd-@VERSION_MAJOR@"
 
-/** The full version that is shown to privileged users. */
+// The full version that is shown to privileged users.
 #define INSPIRCD_VERSION "InspIRCd-@VERSION_FULL@"
 
-/** Determines whether this version of InspIRCd is older than the requested version. */
+// Determines whether this version of InspIRCd is older than the requested version.
 #define INSPIRCD_VERSION_BEFORE(MAJOR, MINOR) (((@VERSION_MAJOR@ << 8) | @VERSION_MINOR@) < ((MAJOR << 8) | (MINOR)))
 
-/** Determines whether this version of InspIRCd is equal to or newer than the requested version. */
+// Determines whether this version of InspIRCd is equal to or newer than the requested version.
 #define INSPIRCD_VERSION_SINCE(MAJOR, MINOR) (((@VERSION_MAJOR@ << 8) | @VERSION_MINOR@) >= ((MAJOR << 8) | (MINOR)))
 
-/** The default location that config files are stored in. */
+// The default location that config files are stored in.
 #define INSPIRCD_CONFIG_PATH "@CONFIG_DIR@"
 
-/** The default location that example config files are stored in. */
+// The default location that example config files are stored in.
 #define INSPIRCD_EXAMPLE_PATH "@EXAMPLE_DIR@"
 
-/** The default location that data files are stored in. */
+// The default location that data files are stored in.
 #define INSPIRCD_DATA_PATH "@DATA_DIR@"
 
-/** The default location that log files are stored in. */
+// The default location that log files are stored in.
 #define INSPIRCD_LOG_PATH "@LOG_DIR@"
 
-/** The default location that module files are stored in. */
+// The default location that module files are stored in.
 #define INSPIRCD_MODULE_PATH "@MODULE_DIR@"
 
-/** The default location that runtime files are stored in. */
+// The default location that runtime files are stored in.
 #define INSPIRCD_RUNTIME_PATH "@RUNTIME_DIR@"
 
-/** The URL of the InspIRCd docs site. */
+// The URL of the InspIRCd docs site.
 #define INSPIRCD_DOCS "https://docs.inspircd.org/@VERSION_MAJOR@/"
 
 #ifndef _WIN32
  %target include/config.h
 
- /** Whether the arc4random_buf() function was available at compile time. */
- %define HAS_ARC4RANDOM_BUF
+// Whether the arc4random_buf() function was available at compile time.
+ %define HAVE_ARC4RANDOM_BUF
 
- /** Whether the clock_gettime() function was available at compile time. */
- %define HAS_CLOCK_GETTIME
+// Whether the clock_gettime() function was available at compile time.
+ %define HAVE_CLOCK_GETTIME
 
- /** Whether the getentropy() function was available at compile time. */
- %define HAS_GETENTROPY
+// Whether the getentropy() function was available at compile time.
+ %define HAVE_GETENTROPY
 
-/** Whether the sigabbrev_np() function was available at compile time. */
- %define HAS_SIGABBREV_NP
+// Whether the sigabbrev_np() function was available at compile time.
+ %define HAVE_SIGABBREV_NP
 
 #endif
