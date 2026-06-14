1adc29e9b68bbf113c67f479e5b82997cd59da88
Sadie Powell <sadie@sadiepowell.dev>
2026-06-14 18:27:13 +0100

    Fix building ssl_openssl on OpenSSL 4.

--- src/modules/extra/m_ssl_openssl.cpp
+++ src/modules/extra/m_ssl_openssl.cpp
@@ -685,7 +685,7 @@ private:
 		X509_free(cert);
 	}
 
-	static void GetDNString(X509_NAME* x509name, std::string& out)
+	static void GetDNString(const X509_NAME* x509name, std::string& out)
 	{
 		char buf[512];
 		X509_NAME_oneline(x509name, buf, sizeof(buf));
@@ -695,7 +695,7 @@ private:
 			out[pos] = ' ';
 	}
 
-	static time_t GetTime(ASN1_TIME* x509time)
+	static time_t GetTime(const ASN1_TIME* x509time)
 	{
 		if (!x509time)
 			return 0;
