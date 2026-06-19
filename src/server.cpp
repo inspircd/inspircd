c5e7d4d8451a7c8a74f885e429ea02e7ce8713a7
Sadie Powell <sadie@sadiepowell.dev>
2026-06-19 13:08:38 +0100

    There's no need to print an error message when we exit cleanly.

--- src/server.cpp
+++ src/server.cpp
@@ -59,7 +59,7 @@ void InspIRCd::Exit(int status)
 	this->Cleanup();
 	ServerInstance = nullptr;
 	delete this;
-	if (isatty(fileno(stdout)))
+	if (status && isatty(fileno(stdout)))
 	{
 		fmt::println("");
 		fmt::println("Exiting with code {}.", status);
